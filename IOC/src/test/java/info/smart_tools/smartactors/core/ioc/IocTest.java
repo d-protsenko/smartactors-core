008b5f269f2e177344c4eba2f270a0de46471c67
Andrey Kutalev <akutalev@gmail.com>
2016-04-28 18:42:11 +0600

    IOC has been covered by tests.

--- IOC/src/test/java/info/smart_tools/smartactors/core/ioc/IocTest.java
+++ IOC/src/test/java/info/smart_tools/smartactors/core/ioc/IocTest.java
@@ -1,9 +1,20 @@
 package info.smart_tools.smartactors.core.ioc;
 
+import info.smart_tools.smartactors.core.iresolve_dependency_strategy.IResolveDependencyStrategy;
 import org.junit.Test;
-//import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-//import static org.mockito.Mockito.*;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 /**
@@ -12,7 +23,48 @@ import org.junit.Test;
 public class IocTest {
 
     @Test
-    public void checkResolution() {
-        //IContainer container = mock()
+    public void checkResolution()
+            throws Exception {
+        Integer value = 1;
+        Object[] args = new Object[]{};
+        IContainer container = mock(IContainer.class);
+        IKey<Integer> key = mock(IKey.class);
+        when(container.resolve(key, args)).thenReturn(value);
+
+        Field field = IOC.class.getDeclaredField("container");
+        field.setAccessible(true);
+        field.set(null, container);
+        field.setAccessible(false);
+
+        Integer result = IOC.resolve(key, args);
+        verify(container, times(1)).resolve(key, args);
+        assertEquals(result, value);
+    }
+
+    @Test
+    public void checkRegistration()
+            throws Exception {
+        final Map<IKey, IResolveDependencyStrategy> testMap = new HashMap<IKey, IResolveDependencyStrategy>();
+        IResolveDependencyStrategy strategy = mock(IResolveDependencyStrategy.class);
+        IContainer container = mock(IContainer.class);
+        IKey<Integer> key = mock(IKey.class);
+        doAnswer(new Answer() {
+            private Map<IKey, IResolveDependencyStrategy> map = new HashMap<IKey, IResolveDependencyStrategy>();
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                IKey key = (IKey)invocationOnMock.getArguments()[0];
+                IResolveDependencyStrategy strategy = (IResolveDependencyStrategy)invocationOnMock.getArguments()[1];
+                testMap.put(key, strategy);
+                return null;
+            }
+        }).when(container).register(key, strategy);
+
+        Field field = IOC.class.getDeclaredField("container");
+        field.setAccessible(true);
+        field.set(null, container);
+        field.setAccessible(false);
+
+        IOC.register(key, strategy);
+        verify(container, times(1)).register(key, strategy);
+        assertEquals(testMap.get(key), strategy);
     }
 }
--- IocContainer/src/test/java/info/smart_tools/smartactors/core/ioc_container/ContainerTest.java
+++ IocContainer/src/test/java/info/smart_tools/smartactors/core/ioc_container/ContainerTest.java
@@ -20,7 +20,9 @@ import java.lang.reflect.Field;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for IOC Container
