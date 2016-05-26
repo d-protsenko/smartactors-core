fa552392ec1a6c766fd80dbf6dbb794fc9b8bc17
Andrey Kutalev <akutalev@gmail.com>
2016-05-26 13:47:12 +0600

    Added NamedKeyStorage service locator.

--- ServerWithIoc/src/main/java/info/smart_tools/smartactors/core/server_with_ioc/Server.java
+++ ServerWithIoc/src/main/java/info/smart_tools/smartactors/core/server_with_ioc/Server.java
@@ -10,6 +10,7 @@ import info.smart_tools.smartactors.core.iscope.IScope;
 import info.smart_tools.smartactors.core.iserver.exception.ServerExecutionException;
 import info.smart_tools.smartactors.core.iserver.exception.ServerInitializeException;
 import info.smart_tools.smartactors.core.iserver.IServer;
+import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import info.smart_tools.smartactors.core.resolve_by_name_ioc_with_lambda_strategy.ResolveByNameIocStrategy;
 import info.smart_tools.smartactors.core.scope_provider.ScopeProvider;
 import info.smart_tools.smartactors.core.singleton_strategy.SingletonStrategy;
@@ -49,14 +50,16 @@ public class Server implements IServer {
             String result1 = IOC.resolve(key1);
 
             /** Example of registration the ResolveByNameIoc strategy */
-            IOC.register(IOC.getKeyForKeyStorage(), new ResolveByNameIocStrategy(
-                    (a) -> {
-                        try {
-                            return new Key<IKey>((String) a[0]);
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    })
+            IOC.register(
+                    IOC.getKeyForKeyStorage(),
+                    new ResolveByNameIocStrategy(
+                        (a) -> {
+                            try {
+                                return new Key<IKey>((String) a[0]);
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
             );
             /** Example of usage the ResolveByNameIoc strategy */
             IKey key2 = IOC.resolve(IOC.getKeyForKeyStorage(), "key2");
@@ -65,7 +68,9 @@ public class Server implements IServer {
 
             /** Example of registration the CreateNewInstance strategy */
             IKey<IObject> key5 = new Key<IObject>("create_new");
-            IOC.register(key5, new CreateNewInstanceStrategy(
+            IOC.register(
+                    key5,
+                    new CreateNewInstanceStrategy(
                             (a) -> new IObjectImpl())
             );
             /** Example of usage the CreateNewInstance strategy */
@@ -75,11 +80,25 @@ public class Server implements IServer {
             /** Example of registration and usage CreateNewInstance strategy by named key */
             Object param = new Object();
             IKey key6 = IOC.resolve(IOC.getKeyForKeyStorage(), "key6");
-            IOC.register(key6, new CreateNewInstanceStrategy(
+            IOC.register(
+                    key6,
+                    new CreateNewInstanceStrategy(
                             (a) -> new IObjectImpl())
             );
             IObject obj = IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), "key6"), param);
             System.out.print("Did it.");
+
+
+            /** Example of registration and usage Keys service locator */
+            IKey<Integer> key7 = Keys.getOrAdd("key7");
+            IOC.register(
+                    key7,
+                    new CreateNewInstanceStrategy(
+                            (a) -> {
+                                return 2;
+                            })
+            );
+            IOC.resolve(Keys.getOrAdd("key7"));
 
         } catch (Exception | Error e) {
             System.out.println(e);
