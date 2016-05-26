068654c08da2c07205ddc1a4b1da35fe318af764
Andrey Kutalev <akutalev@gmail.com>
2016-05-26 20:01:49 +0600

    Broken commit. Added topoligical sort to the implementation of IBootstrap.start().

--- Bootstrap/src/main/java/info/smart_tools/smartactors/core/bootstrap/Bootstrap.java
+++ Bootstrap/src/main/java/info/smart_tools/smartactors/core/bootstrap/Bootstrap.java
@@ -6,28 +6,31 @@ import info.smart_tools.smartactors.core.ibootstrap.exception.RevertProcessExecu
 import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Map;
+import java.util.Stack;
 
 /**
  * Implementation of {@link IBootstrap}
  */
-public class Bootstrap implements IBootstrap<IBootstrapItem> {
+public class Bootstrap implements IBootstrap<IBootstrapItem<String>> {
 
-    private List<IBootstrapItem> itemStorage = new ArrayList<>();
+    //private List<IBootstrapItem> itemStorage = new ArrayList<>();
+    private Map<String, IBootstrapItem<String>> itemStorage = new HashMap<>();
 
     @Override
-    public void add(final IBootstrapItem bootstrapItem) {
-        itemStorage.add(bootstrapItem);
+    public void add(final IBootstrapItem<String> bootstrapItem) {
+        itemStorage.put(bootstrapItem.getItemName(), bootstrapItem);
     }
 
     @Override
     public void start()
             throws ProcessExecutionException {
         try {
-            for (IBootstrapItem item : itemStorage) {
-                item.executeProcess();
-            }
+            TopologicalSort ts = new TopologicalSort(itemStorage);
+            String[] orderedItems = ts.getOrderedList();
+
         } catch (Throwable e) {
             throw  new ProcessExecutionException("Could not execute plugin process.", e);
         }
@@ -36,13 +39,82 @@ public class Bootstrap implements IBootstrap<IBootstrapItem> {
     @Override
     public void revert()
             throws RevertProcessExecutionException {
-        try {
-            ListIterator iterator = itemStorage.listIterator(itemStorage.size());
-            while (iterator.hasPrevious()) {
-                ((IBootstrapItem) iterator.previous()).executeRevertProcess();
+//        try {
+//
+//        } catch (Throwable e) {
+//            throw new RevertProcessExecutionException("Could not execute plugin revert process.", e);
+//        }
+    }
+
+    /**
+     * Sort given list of {@link IBootstrapItem} using topological graph sorting
+     * @return sorted list of {@link IBootstrapItem} names
+     */
+
+
+    private boolean dfs(int v) {
+        if ()
+    }
+
+}
+
+class TopologicalSort {
+    private boolean cycle;
+    private int[] colors;
+    private Stack stack = new Stack();
+    private String[] orderedList;
+    private int size;
+    private List<String>[] edges;
+    private Map<String, IBootstrapItem<String>> items;
+
+    public TopologicalSort(final Map<String, IBootstrapItem<String>> items) {
+        this.items = items;
+        this.size = items.size();
+        this.edges = new ArrayList[this.size];
+        this.orderedList = new String[this.size];
+        this.colors = new int[this.size];
+        sortItems();
+    }
+
+    private void sortItems() {
+
+        // fill lists of edges
+        int count = 0;
+        for (Map.Entry<String, IBootstrapItem<String>> pair : items.entrySet()) {
+            String name = pair.getValue().getItemName();
+
+            // fill 'after' dependencies
+            edges[count] = pair.getValue().getAfterItems();
+
+            // loop for find dependencies from other items
+            for (Map.Entry<String, IBootstrapItem<String>> innerPair : items.entrySet()) {
+                for (String before : innerPair.getValue().getBeforeItems()) {
+                    if (before.equals(name)) {
+                        edges[count].add(before);
+                    }
+                }
             }
-        } catch (Throwable e) {
-            throw new RevertProcessExecutionException("Could not execute plugin revert process.", e);
+            ++count;
         }
+    }
+
+    private boolean dfs(int v) {
+        if (1 == colors[v]) {
+            return true;
+        }
+        if (2 == colors[v]) {
+            return false;
+        }
+        colors[v] = 1;
+        for (int i = 0; i < this.edges[v].size(); ++i) {
+            if (dfs(this.edges[v].get(i))) {
+
+            }
+        }
+
+    }
+
+    public String[] getOrderedList() {
+        return this.orderedList;
     }
 }
