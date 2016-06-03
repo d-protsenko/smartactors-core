1dafdf777e1f3f0324abfb1e28a19c7670251021
aslesarev <aslesarev@hwdtech.ru>
2016-06-03 15:29:59 +0600

    add package-info

---
+++ DataBaseStorage/src/main/java/info/smart_tools/smartactors/core/db_storage/exceptions/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contain exceptions for {@link info.smart_tools.smartactors.core.db_storage.DataBaseStorage}
+ */
+package info.smart_tools.smartactors.core.db_storage.exceptions;
---
+++ DataBaseStorage/src/main/java/info/smart_tools/smartactors/core/db_storage/utils/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contain utils for {@link info.smart_tools.smartactors.core.db_storage.DataBaseStorage}
+ */
+package info.smart_tools.smartactors.core.db_storage.utils;
