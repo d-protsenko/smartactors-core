85d5ec636ab29de80e49fca681da2207149f6add
aslesarev <aslesarev@hwdtech.ru>
2016-06-09 14:31:03 +0600

    add setConnection method

--- IDatabaseTask/src/main/java/info/smart_tools/smartactors/core/idatabase_task/IDatabaseTask.java
+++ IDatabaseTask/src/main/java/info/smart_tools/smartactors/core/idatabase_task/IDatabaseTask.java
@@ -1,7 +1,9 @@
 package info.smart_tools.smartactors.core.idatabase_task;
 
 
+import info.smart_tools.smartactors.core.db_storage.interfaces.StorageConnection;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
+import info.smart_tools.smartactors.core.idatabase_task.exception.TaskSetConnectionException;
 import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.itask.ITask;
 
@@ -17,4 +19,6 @@ public interface IDatabaseTask extends ITask{
      * @throws TaskPrepareException if error occurs in process of query preparing
      */
     void prepare(IObject query) throws TaskPrepareException;
+
+    void setConnection(StorageConnection connection) throws TaskSetConnectionException;
 }
