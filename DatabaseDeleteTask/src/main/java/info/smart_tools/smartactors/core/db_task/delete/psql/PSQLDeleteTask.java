5c86103ce75988e4827d0fed7be15604ef1dc0c9
Maminsibirac <turkish_13@mail.ru>
2016-06-09 15:39:48 +0600

    Added java docs.

--- DatabaseDeleteTask/src/main/java/info/smart_tools/smartactors/core/db_task/delete/psql/PSQLDeleteTask.java
+++ DatabaseDeleteTask/src/main/java/info/smart_tools/smartactors/core/db_task/delete/psql/PSQLDeleteTask.java
@@ -4,6 +4,7 @@ import info.smart_tools.smartactors.core.db_storage.exceptions.QueryBuildExcepti
 import info.smart_tools.smartactors.core.db_storage.utils.CollectionName;
 import info.smart_tools.smartactors.core.db_storage.utils.ConnectionPool;
 import info.smart_tools.smartactors.core.db_task.delete.DBDeleteTask;
+import info.smart_tools.smartactors.core.db_task.delete.DeleteTaskException;
 import info.smart_tools.smartactors.core.db_task.delete.wrappers.DeletionQuery;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
@@ -16,64 +17,89 @@ import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.io.Writer;
 
+/**
+ * Task for deletion documents from database.
+ */
 public class PSQLDeleteTask extends DBDeleteTask {
     private QueryStatement query;
     private DeletionQuery message;
 
-    // ToDo : Create common to all tasks exception.
-    private PSQLDeleteTask(ConnectionPool connectionPool, CollectionName collectionName) {
+    /**
+     * A single constructor for creation {@link PSQLDeleteTask}
+     *
+     * @param connectionPool container for all connections to database.
+     * @param collectionName the name of collection to which a query is sent.
+     *
+     * @throws DeleteTaskException when error writing deletion query statement.
+     */
+    private PSQLDeleteTask(ConnectionPool connectionPool, CollectionName collectionName) throws DeleteTaskException {
         super(connectionPool);
         try {
             query = new QueryStatement();
             query.getBodyWriter().write(String.format("DELETE FROM %s WHERE %s IN (",
                     collectionName.toString(), "id"));
         } catch (IOException e) {
-            // ToDo : handle exception.
-            //("Error while writing deletion query statement.", e);
+            throw new DeleteTaskException("Error while writing deletion query statement.", e);
         }
     }
 
-    // ToDo : Create common to all tasks exception.
-    public static PSQLDeleteTask create(@Nonnull ConnectionPool connectionPool, @Nonnull String collectionName) {
+    /**
+     * Factory method for creation new instance of {@link PSQLDeleteTask}.
+     *
+     * @param connectionPool container for all connections to database.
+     * @param collectionName the name of collection to which a query is sent.
+     *
+     * @return a new {@link PSQLDeleteTask} object.
+     *
+     * @throws DeleteTaskException when invalid collection name.
+     */
+    public static PSQLDeleteTask create(@Nonnull ConnectionPool connectionPool, @Nonnull String collectionName)
+            throws DeleteTaskException {
         try {
             CollectionName cName = CollectionName.fromString(collectionName);
             return new PSQLDeleteTask(connectionPool, cName);
         } catch (QueryBuildException e) {
-            // ToDo : handle exception.
-            //(e.getMessage(), e);
-            return null;
+            throw new DeleteTaskException(e.getMessage(), e);
         }
     }
 
+    /**
+     * {@see IDatabaseTask} {@link info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask}
+     * Prepare query for deletion documents from database by ids.
+     *
+     * @param message contains parameters for deletion query.
+     *                Should contains list of documents ids deleted objects.
+     *
+     * @throws TaskPrepareException when:
+     *                1. IOC resolution error;
+     *                2. List of documents ids is null or empty;
+     *                3. Error writing deletion query statement.
+     */
     @Override
     public void prepare(@Nonnull IObject message) throws TaskPrepareException {
-        DeletionQuery taskMessage;
         try {
-            taskMessage = IOC.resolve(
+            this.message = IOC.resolve(
                     IOC.resolve(IOC.getKeyForKeyStorage(), DeletionQuery.class),
                     message
             );
-            this.message = taskMessage;
-
-            query = new QueryStatement();
-
         } catch (ResolutionException e) {
             throw new TaskPrepareException(e.getMessage(), e);
         }
 
-        if(taskMessage.countDocumentIds() == 0) {
+        if(this.message.countDocumentIds() == 0) {
             throw new TaskPrepareException("List of id's to delete should not be empty.");
         }
 
         try {
+            int documentsIdsSize = this.message.countDocumentIds();
             Writer writer = query.getBodyWriter();
-            for (int i = taskMessage.countDocumentIds(); i > 0; --i) {
+            for (int i = documentsIdsSize; i > 0; --i) {
                 writer.write("?" + ((i == 1) ? "" : ","));
             }
             writer.write(")");
             query.pushParameterSetter((statement, index) -> {
-                for (int i = 0; i < taskMessage.countDocumentIds(); ++i)
-                    statement.setLong(index++, taskMessage.getDocumentIds(i));
+                for (int i = 0; i < documentsIdsSize; ++i)
+                    statement.setLong(index++, this.message.getDocumentIds(i));
 
                 return index;
             });
@@ -82,6 +108,12 @@ public class PSQLDeleteTask extends DBDeleteTask {
         }
     }
 
+    /**
+     * {@see ITask} {@link info.smart_tools.smartactors.core.itask.ITask}
+     * Executes deletion documents by ids from database.
+     *
+     * @throws TaskExecutionException when error in execution process.
+     */
     @Override
     public void execute() throws TaskExecutionException {
         super.execute(query, message);
