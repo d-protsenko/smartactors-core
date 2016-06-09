e544b9a4c7db114d0317db0732fdbd1213376866
Maminsibirac <turkish_13@mail.ru>
2016-06-09 17:22:27 +0600

    DataBaseStorage replace by DBTaskExecutor

--- DataBaseStorage/src/main/java/info/smart_tools/smartactors/core/db_storage/interfaces/package-info.java
+++ DataBaseStorage/src/main/java/info/smart_tools/smartactors/core/db_storage/interfaces/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Contain interfaces for {@link info.smart_tools.smartactors.core.db_storage.DataBaseStorage}
+ * Contain interfaces for {@link info.smart_tools.smartactors.core.db_storage.DBTaskExecutor}
  */
 package info.smart_tools.smartactors.core.db_storage.interfaces;
