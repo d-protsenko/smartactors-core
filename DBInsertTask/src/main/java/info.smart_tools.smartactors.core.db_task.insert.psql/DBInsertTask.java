1b3da7f2f86eb5c7e67b7647082ffa61508daa9f
Vitaly <stels95@list.ru>
2016-06-10 18:45:13 +0600

    fix dependencies

--- DBInsertTask/src/main/java/info.smart_tools.smartactors.core.db_task.insert.psql/DBInsertTask.java
+++ DBInsertTask/src/main/java/info.smart_tools.smartactors.core.db_task.insert.psql/DBInsertTask.java
@@ -1,13 +1,20 @@
 package info.smart_tools.smartactors.core.db_task.insert.psql;
 
+import info.smart_tools.smartactors.core.db_storage.interfaces.StorageConnection;
 import info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
+import info.smart_tools.smartactors.core.idatabase_task.exception.TaskSetConnectionException;
 import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
 
 public class DBInsertTask implements IDatabaseTask {
     @Override
     public void prepare(IObject query) throws TaskPrepareException {
+
+    }
+
+    @Override
+    public void setConnection(StorageConnection connection) throws TaskSetConnectionException {
 
     }
 
