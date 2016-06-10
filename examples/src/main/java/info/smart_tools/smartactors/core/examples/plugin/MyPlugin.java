b77e0e5aee376d8ee070bfc9e04caaf8453961b4
Denis Nelubin <dnelubin@gmail.com>
2016-06-10 18:39:23 +0600

    Implemented the system of two plugins, for example.

--- examples/src/main/java/info/smart_tools/smartactors/core/examples/plugin/MyPlugin.java
+++ examples/src/main/java/info/smart_tools/smartactors/core/examples/plugin/MyPlugin.java
@@ -1,8 +1,12 @@
 package info.smart_tools.smartactors.core.examples.plugin;
 
 import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
+import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
+import info.smart_tools.smartactors.core.examples.MyClass;
 import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
 import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
+import info.smart_tools.smartactors.core.ikey.IKey;
+import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.iplugin.IPlugin;
 import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
 
@@ -20,14 +24,21 @@ public class MyPlugin implements IPlugin {
     @Override
     public void load() throws PluginException {
         try {
-            // TODO: add dependency from IOC
             IBootstrapItem<String> item = new BootstrapItem("MyPlugin");
+            item.after("IOC");
             item.process(() -> {
+                try {
+                    IKey<MyClass>key = IOC.resolve(IOC.getKeyForKeyStorage(), "new MyClass");
+                    IOC.register(key, new CreateNewInstanceStrategy(
+                            (args) -> new MyClass((String) args[0])));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
                 System.out.println("MyPlugin initialized");
             });
             bootstrap.add(item);
         } catch (Exception e) {
-            throw new PluginException("Could not load plugin", e);
+            throw new PluginException("Could not load MyPlugin", e);
         }
         System.out.println("MyPlugin loaded");
     }
