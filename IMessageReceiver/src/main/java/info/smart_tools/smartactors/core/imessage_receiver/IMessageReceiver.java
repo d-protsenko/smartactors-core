3511fe07ae9750259298e727cda3b021dc441085
AlexeyBond <alexey.bond.94.55@gmail.com>
2016-06-14 17:14:30 +0600

    Update IMessageReceiver interface.

--- IMessageReceiver/src/main/java/info/smart_tools/smartactors/core/imessage_receiver/IMessageReceiver.java
+++ IMessageReceiver/src/main/java/info/smart_tools/smartactors/core/imessage_receiver/IMessageReceiver.java
@@ -1,5 +1,6 @@
 package info.smart_tools.smartactors.core.imessage_receiver;
 
+import info.smart_tools.smartactors.core.iaction.IAction;
 import info.smart_tools.smartactors.core.imessage.IMessage;
 import info.smart_tools.smartactors.core.imessage_receiver.exception.MessageReceiveException;
 
@@ -8,10 +9,13 @@ import info.smart_tools.smartactors.core.imessage_receiver.exception.MessageRece
  */
 public interface IMessageReceiver {
     /**
-     * Receive the message.
+     * Receive the message and execute given action when done.
+     *
+     * Passes exception to the action if any error occurs while processing the message.
      *
      * @param message the message
+     * @param onEnd the action that should be executed when message processing by this receiver is completed
      * @throws MessageReceiveException if error occurs receiving the message
      */
-    void receive(IMessage message) throws MessageReceiveException;
+    void receive(IMessage message, IAction<Throwable> onEnd) throws MessageReceiveException;
 }
