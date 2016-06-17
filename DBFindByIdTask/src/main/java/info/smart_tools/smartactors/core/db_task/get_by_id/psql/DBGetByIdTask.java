352b521f6cc80c5cbdd7af9d33fbf240a0770474
aslesarev <aslesarev@hwdtech.ru>
2016-06-17 18:17:59 +0600

    add tests

--- DBFindByIdTask/src/main/java/info/smart_tools/smartactors/core/db_task/get_by_id/psql/DBGetByIdTask.java
+++ DBFindByIdTask/src/main/java/info/smart_tools/smartactors/core/db_task/get_by_id/psql/DBGetByIdTask.java
@@ -1,9 +1,10 @@
 package info.smart_tools.smartactors.core.db_task.get_by_id.psql;
 
+import info.smart_tools.smartactors.core.db_storage.exceptions.QueryBuildException;
 import info.smart_tools.smartactors.core.db_storage.exceptions.StorageException;
 import info.smart_tools.smartactors.core.db_storage.interfaces.CompiledQuery;
 import info.smart_tools.smartactors.core.db_storage.interfaces.StorageConnection;
-import info.smart_tools.smartactors.core.db_task.get_by_id.exception.DBGetByIdTaskException;
+import info.smart_tools.smartactors.core.db_storage.utils.CollectionName;
 import info.smart_tools.smartactors.core.db_task.get_by_id.psql.wrapper.SearchByIdQuery;
 import info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
@@ -13,10 +14,14 @@ import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
+import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import info.smart_tools.smartactors.core.sql_commons.JDBCCompiledQuery;
 import info.smart_tools.smartactors.core.sql_commons.QueryStatement;
+import info.smart_tools.smartactors.core.sql_commons.QueryStatementFactory;
+import info.smart_tools.smartactors.core.sql_commons.exception.QueryStatementFactoryException;
 
 import java.io.IOException;
+import java.io.Writer;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -25,39 +30,45 @@ import java.sql.SQLException;
  */
 public class DBGetByIdTask implements IDatabaseTask {
     private StorageConnection connection;
-    private SearchByIdQuery message;
     private CompiledQuery compiledQuery;
-    private QueryStatement searchQueryStatement;
+    private SearchByIdQuery message;
 
     /**
      * Constructor for DBGetByIdTask
-     * @throws DBGetByIdTaskException
      */
-    public DBGetByIdTask() throws DBGetByIdTaskException {
-        try {
-            this.searchQueryStatement = IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), QueryStatement.class.toString()));
-        } catch (ResolutionException e) {
-            throw new DBGetByIdTaskException("Error while resolving query statement.", e);
-        }
-    }
+    public DBGetByIdTask() {}
 
     @Override
     public void prepare(final IObject object) throws TaskPrepareException {
         try {
-            this.message = IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), SearchByIdQuery.class.toString()), object);
-            initQuery();
+            QueryStatementFactory factory = () -> {
+                QueryStatement preparedQuery = new QueryStatement();
+                Writer writer = preparedQuery.getBodyWriter();
+                try {
+                    message = IOC.resolve(Keys.getOrAdd(SearchByIdQuery.class.toString()), object);
+                    CollectionName collectionName = CollectionName.fromString(message.getCollectionName());
+                    writer.write(String.format("SELECT * FROM %s WHERE ", collectionName.toString()));
+                    writer.write(String.format("token = \'%s\'", message.getId()));
+                } catch (IOException | ResolutionException | ChangeValueException | ReadValueException e) {
+                    throw new QueryStatementFactoryException("Error while initialize update query.", e);
+                } catch (QueryBuildException e) {
+                    e.printStackTrace();
+                }
+                return preparedQuery;
+            };
 
-            this.compiledQuery = connection.compileQuery(searchQueryStatement);
-        } catch (DBGetByIdTaskException | StorageException | ResolutionException e) {
-            throw new TaskPrepareException("Error while writing search query statement.", e);
+            this.compiledQuery = IOC.resolve(Keys.getOrAdd(CompiledQuery.class.toString()), connection, factory);
+        } catch (ResolutionException e) {
+            throw new TaskPrepareException("Error while writing collection creation statement.", e);
         }
+
     }
 
     @Override
     public void execute() throws TaskExecutionException {
         try {
             ResultSet resultSet = ((JDBCCompiledQuery) compiledQuery).getPreparedStatement().executeQuery();
-            if (resultSet.first()) {
+            if (resultSet != null && resultSet.first()) {
                 try {
                     message.setSearchResult((IObject) resultSet.getObject(0));
                 } catch (ChangeValueException e) {
@@ -74,14 +85,5 @@ public class DBGetByIdTask implements IDatabaseTask {
     @Override
     public void setConnection(final StorageConnection connection) {
         this.connection = connection;
-    }
-
-    private void initQuery() throws DBGetByIdTaskException {
-        try {
-        searchQueryStatement.getBodyWriter().write(String.format("SELECT * FROM %s WHERE ", message.getCollectionName()));
-        searchQueryStatement.getBodyWriter().write(String.format("token = \'%s\'", message.getId()));
-        } catch (IOException | ChangeValueException | ReadValueException e) {
-            throw new DBGetByIdTaskException("Error while initialize search query.", e);
-        }
     }
 }
