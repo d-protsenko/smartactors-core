ea22fac3a59a679c65dfc3b8c4f8bb60ec8c8a52
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-06-23 18:36:02 +0600

    added tests for tasks

--- CachedCollection/src/test/java/info/smart_tools/smartactors/core/cached_collection/task/DeleteFromCachedCollectionTaskTest.java
+++ CachedCollection/src/test/java/info/smart_tools/smartactors/core/cached_collection/task/DeleteFromCachedCollectionTaskTest.java
@@ -14,13 +14,17 @@ import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import info.smart_tools.smartactors.core.string_ioc_key.Key;
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
@@ -73,6 +77,43 @@ public class DeleteFromCachedCollectionTaskTest {
         when(IOC.resolve(deleteFromCachedCollectionQueryKey, srcQuery)).thenThrow(new ResolutionException(""));
 
         task.prepare(srcQuery);
+    }
+
+    @Test(expected = TaskPrepareException.class)
+    public void ShouldInCorrectPrepareObjectForDeletingWhenMessageThrowException() throws ResolutionException, TaskPrepareException, ReadValueException, ChangeValueException {
+        Key deleteFromCachedCollectionQueryKey = mock(Key.class);
+        when(Keys.getOrAdd(DeleteFromCachedCollectionQuery.class.toString())).thenReturn(deleteFromCachedCollectionQueryKey);
+
+        IObject srcQuery = mock(IObject.class);
+
+        DeleteFromCachedCollectionQuery query = mock (DeleteFromCachedCollectionQuery.class);
+        when(IOC.resolve(deleteFromCachedCollectionQueryKey, srcQuery)).thenReturn(query);
+        when(query.getDeleteItem()).thenThrow(new ReadValueException());
+
+        task.prepare(srcQuery);
+    }
+
+    @Test
+    public void ShouldInCorrectPrepareObjectForDeletingWhenDeleteItemThrowException() throws ResolutionException, TaskPrepareException, ReadValueException, ChangeValueException {
+        Key deleteFromCachedCollectionQueryKey = mock(Key.class);
+        when(Keys.getOrAdd(DeleteFromCachedCollectionQuery.class.toString())).thenReturn(deleteFromCachedCollectionQueryKey);
+
+        IObject srcQuery = mock(IObject.class);
+
+        DeleteFromCachedCollectionQuery query = mock (DeleteFromCachedCollectionQuery.class);
+        when(IOC.resolve(deleteFromCachedCollectionQueryKey, srcQuery)).thenReturn(query);
+        when(query.wrapped()).thenReturn(srcQuery);
+        DeleteItem deleteItem = mock(DeleteItem.class);
+        when(query.getDeleteItem()).thenReturn(deleteItem);
+        doThrow(new ChangeValueException()).when(deleteItem).setIsActive(any());
+
+        try {
+            task.prepare(srcQuery);
+        } catch (TaskPrepareException e) {
+            verify(query).getDeleteItem();
+            return;
+        }
+        assertTrue("Must throw new exception", false);
     }
 
     @Test
