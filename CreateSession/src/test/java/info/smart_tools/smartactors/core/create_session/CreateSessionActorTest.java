e498a70ad6b41c90b2b139b0b54500af8aed4b83
Vitaly <stels95@list.ru>
2016-06-28 19:17:56 +0600

    add tests add dependencies for tests

--- CreateSession/src/test/java/info/smart_tools/smartactors/core/create_session/CreateSessionActorTest.java
+++ CreateSession/src/test/java/info/smart_tools/smartactors/core/create_session/CreateSessionActorTest.java
@@ -1,11 +1,55 @@
 package info.smart_tools.smartactors.core.create_session;
 
-import static org.junit.Assert.*;
+import info.smart_tools.smartactors.core.create_session.wrapper.CreateSessionMessage;
+import info.smart_tools.smartactors.core.create_session.wrapper.Session;
+import info.smart_tools.smartactors.core.ikey.IKey;
+import info.smart_tools.smartactors.core.iobject.IObject;
+import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
+import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
+import info.smart_tools.smartactors.core.ioc.IOC;
+import info.smart_tools.smartactors.core.string_ioc_key.Key;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(IOC.class)
 public class CreateSessionActorTest {
+    private Session session;
+    private CreateSessionMessage inputMessage;
+    private IObject authInfo;
+    CreateSessionActor actor;
 
     @org.junit.Before
     public void setUp() throws Exception {
+        inputMessage = mock(CreateSessionMessage.class);
+        session = mock(Session.class);
+        authInfo = mock(IObject.class);
+        when(inputMessage.getAuthInfo()).thenReturn(authInfo);
 
+        mockStatic(IOC.class);
+        IKey key = mock(IKey.class);
+        IKey sessionKey = mock(IKey.class);
+        when(IOC.getKeyForKeyStorage()).thenReturn(key);
+        when(IOC.resolve(eq(key), eq("interface info.smart_tools.smartactors.core.create_session.wrapper.Session"))).thenReturn(sessionKey);
+        when(IOC.resolve(eq(sessionKey))).thenReturn(session);
+
+        actor = new CreateSessionActor(mock(IObject.class));
+    }
+
+    @Test
+    public void Should_insertNewSessionInMessage_When_SessionIsNull() throws ChangeValueException, ReadValueException {
+        when(inputMessage.getSession()).thenReturn(null);
+        actor.createSession(inputMessage);
+        verify(session).setAuthInfo(eq(authInfo));
+        verify(inputMessage).setSession(eq(session));
     }
 }
