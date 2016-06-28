50fbf122c8f079a6afc5fd653e79a79885e7cdf2
Vitaly <stels95@list.ru>
2016-06-28 18:44:13 +0600

    create tests add dependencies for tests: core.create_new_instance_strategy, core.singleton_strategy, core.strategy_container, core.resolve_by_name_ioc_with_lambda, junit 4.12

---
+++ CreateSession/src/test/java/info/smart_tools/smartactors/core/create_session/CreateSessionActorTest.java
@@ -0,0 +1,11 @@
+package info.smart_tools.smartactors.core.create_session;
+
+import static org.junit.Assert.*;
+
+public class CreateSessionActorTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+}
