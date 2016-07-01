137107ec9036ca1ac7f50e66b5cf1d1c09860bb1
Ivan Ivashchenko <ivan.ivashchenko@7bits.it>
2016-07-01 12:06:23 +0600

    add some comments

--- CreateAsyncOperation/src/main/java/info/smart_tools/smartactors/actor/create_async_operation/CreateAsyncOperationActor.java
+++ CreateAsyncOperation/src/main/java/info/smart_tools/smartactors/actor/create_async_operation/CreateAsyncOperationActor.java
@@ -3,6 +3,7 @@ package info.smart_tools.smartactors.actor.create_async_operation;
 import info.smart_tools.smartactors.actor.create_async_operation.wrapper.AsyncOperation;
 import info.smart_tools.smartactors.actor.create_async_operation.wrapper.AuthOperationData;
 import info.smart_tools.smartactors.actor.create_async_operation.wrapper.CreateAsyncOperationMessage;
+import info.smart_tools.smartactors.core.async_operation_collection.IAsyncOperationCollection;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
 import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
@@ -19,17 +20,18 @@ public class CreateAsyncOperationActor {
     public void create(final CreateAsyncOperationMessage message) {
 
         try {
+            //TODO:: generate token??
             String token = message.getAsyncOperationToken();
             IAsyncOperationCollection collection = IOC.resolve(Keys.getOrAdd(IAsyncOperationCollection.class.toString()));
             AsyncOperation asyncOperation = IOC.resolve(Keys.getOrAdd(AsyncOperation.class.toString()));
             asyncOperation.setToken(token);
+            //TODO:: how we define expired time??
             asyncOperation.setExpiredTime(LocalDateTime.now());
-            asyncOperation.setIsDone(false);
             AuthOperationData authOperationData = IOC.resolve(Keys.getOrAdd(AuthOperationData.class.toString()));
             authOperationData.setSessionId(message.getSessionId());
             //TODO:: Think about it, maybe IObject will be better and then move AsyncOperation interafce to AsyncCollection module?
             asyncOperation.setOperationData(authOperationData);
-            collection.insert(asyncOperation);
+//            collection.createAsyncOperation(asyncOperation, token);
 
             message.setAsyncOperationSessionToken(token);
             message.setAsyncOperationToken(token);
