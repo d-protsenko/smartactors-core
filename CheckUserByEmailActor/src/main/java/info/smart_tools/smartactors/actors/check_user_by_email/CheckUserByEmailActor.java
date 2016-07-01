9656df0681a1052bfd26cb7638ddf2b282b4535e
aslesarev <aslesarev@hwdtech.ru>
2016-07-01 18:05:10 +0600

    start actor

---
+++ CheckUserByEmailActor/src/main/java/info/smart_tools/smartactors/actors/check_user_by_email/CheckUserByEmailActor.java
@@ -0,0 +1,13 @@
+package info.smart_tools.smartactors.actors.check_user_by_email;
+
+import info.smart_tools.smartactors.actors.check_user_by_email.wrapper.MessageWrapper;
+import info.smart_tools.smartactors.core.iobject.IObject;
+
+public class CheckUserByEmailActor {
+    public CheckUserByEmailActor(IObject Params) {
+    }
+
+    public void checkUser(MessageWrapper message) {
+
+    }
+}
