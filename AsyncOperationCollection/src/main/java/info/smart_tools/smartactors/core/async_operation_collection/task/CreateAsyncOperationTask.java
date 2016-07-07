ea476d43e1af8340f7abc9b870ec5ad9f730a409
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-07-07 16:04:55 +0600

    fix code of CreateAsyncOperationTask

--- AsyncOperationCollection/src/main/java/info/smart_tools/smartactors/core/async_operation_collection/task/CreateAsyncOperationTask.java
+++ AsyncOperationCollection/src/main/java/info/smart_tools/smartactors/core/async_operation_collection/task/CreateAsyncOperationTask.java
@@ -7,12 +7,15 @@ import info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskSetConnectionException;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.core.iobject.IFieldName;
 import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
+import info.smart_tools.smartactors.core.wrapper_generator.Field;
 
 /**
  * DB task for creating operations in db
@@ -21,12 +24,23 @@ public class CreateAsyncOperationTask implements IDatabaseTask {
 
     private IDatabaseTask task;
 
+    private Field<IObject> asyncDataField;
+    private Field<Boolean> doneFlagField;
+    private Field<String> tokenField;
+    private Field<String> expiredTimeField;
+    private Field<IObject> documentField;
     /**
      * Constructor
      * @param task the insert DB task
      */
-    public CreateAsyncOperationTask(final IDatabaseTask task) {
+    public CreateAsyncOperationTask(final IDatabaseTask task) throws ResolutionException, InvalidArgumentException {
         this.task = task;
+
+        asyncDataField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "asyncData"));
+        doneFlagField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "done"));
+        tokenField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "token"));
+        expiredTimeField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "expiredTime"));
+        documentField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "document"));
     }
 
     /**
@@ -42,19 +56,20 @@ public class CreateAsyncOperationTask implements IDatabaseTask {
     @Override
     public void prepare(final IObject query) throws TaskPrepareException {
         try {
-            CreateOperationQuery srcQueryObject = IOC.resolve(Keys.getOrAdd(CreateOperationQuery.class.toString()), query);
-            AsyncDocument document = IOC.resolve(Keys.getOrAdd(AsyncDocument.class.toString()));
-            document.setAsyncData(srcQueryObject.getAsyncData());
-            document.setDoneFlag(false);
-            document.setToken(srcQueryObject.getToken());
-            document.setExpiredTime(srcQueryObject.getExpiredTime());
-            srcQueryObject.setDocument(document.getIObject());
-            //TODO:: mb pass to prepare new object only with needed fields?
-            task.prepare(srcQueryObject.getIObject());
+
+            IObject document = IOC.resolve(Keys.getOrAdd(IObject.class.toString()));
+
+            asyncDataField.in(document, asyncDataField.out(query));
+            doneFlagField.in(document, false);
+            tokenField.in(document, tokenField.out(query));
+            expiredTimeField.in(document, expiredTimeField.out(query));
+            documentField.in(query, document);
+
+            task.prepare(query);
         } catch (ResolutionException e) {
             throw new TaskPrepareException("Can't resolve objects during prepare create async operation", e);
-        } catch (ReadValueException | ChangeValueException e) {
-            throw new TaskPrepareException("Can't prepare query for create async operation", e);
+        } catch (ReadValueException | ChangeValueException | InvalidArgumentException e) {
+            throw new TaskPrepareException("Can't prepare query for create async operation cause one of Field.out operation down", e);
         }
     }
 
