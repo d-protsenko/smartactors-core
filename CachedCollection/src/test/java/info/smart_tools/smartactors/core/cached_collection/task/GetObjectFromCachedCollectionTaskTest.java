e59ab3299c7583b3898430bdcabed32c39c7106e
Ivan Ivashchenko <ivan.ivashchenko@7bits.it>
2016-07-08 16:36:31 +0600

    get item task tests have been fixed

--- CachedCollection/src/test/java/info/smart_tools/smartactors/core/cached_collection/task/GetObjectFromCachedCollectionTaskTest.java
+++ CachedCollection/src/test/java/info/smart_tools/smartactors/core/cached_collection/task/GetObjectFromCachedCollectionTaskTest.java
@@ -1,21 +1,18 @@
 package info.smart_tools.smartactors.core.cached_collection.task;
 
-import info.smart_tools.smartactors.core.cached_collection.wrapper.CriteriaCachedCollectionQuery;
-import info.smart_tools.smartactors.core.cached_collection.wrapper.GetObjectFromCachedCollectionQuery;
-import info.smart_tools.smartactors.core.cached_collection.wrapper.get_item.DateToMessage;
-import info.smart_tools.smartactors.core.cached_collection.wrapper.get_item.EQMessage;
 import info.smart_tools.smartactors.core.db_storage.interfaces.StorageConnection;
 import info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskSetConnectionException;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.core.ikey.IKey;
 import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
-import info.smart_tools.smartactors.core.string_ioc_key.Key;
+import info.smart_tools.smartactors.core.wrapper_generator.Field;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,10 +22,10 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import java.time.LocalDateTime;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.powermock.api.mockito.PowerMockito.doThrow;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
@@ -40,7 +37,13 @@ public class GetObjectFromCachedCollectionTaskTest {
     private GetObjectFromCachedCollectionTask testTask;
     private IDatabaseTask targetTask;
 
-    private String key = "key";
+    private Field<Integer> pageSizeField;
+    private Field<Integer> pageNumberField;
+    private Field<String> keyNameField;
+    private Field<String> keyValueField;
+    private Field<Boolean> criteriaEqualsIsActiveField;
+    private Field<String> criteriaDateToStartDateTimeField;
+    private IKey mockKeyField = mock(IKey.class);
 
     @Before
     public void prepare () throws Exception {
@@ -48,107 +51,59 @@ public class GetObjectFromCachedCollectionTaskTest {
         mockStatic(Keys.class);
         mockStatic(LocalDateTime.class);
 
+        Field<String> collectionNameField = mock(Field.class);
+        keyNameField = mock(Field.class);
+        keyValueField = mock(Field.class);
+        pageSizeField = mock(Field.class);
+        pageNumberField = mock(Field.class);
+        criteriaEqualsIsActiveField = mock(Field.class);
+        criteriaDateToStartDateTimeField = mock(Field.class);
+
+
+        when(Keys.getOrAdd("Field")).thenReturn(mockKeyField);
+        when(IOC.resolve(mockKeyField, "collectionName")).thenReturn(collectionNameField);
+        when(IOC.resolve(mockKeyField, "keyName")).thenReturn(keyNameField);
+        when(IOC.resolve(mockKeyField, "keyValue")).thenReturn(keyValueField);
+        when(IOC.resolve(mockKeyField, "pageSize")).thenReturn(pageSizeField);
+        when(IOC.resolve(mockKeyField, "pageNumber")).thenReturn(pageNumberField);
+        when(IOC.resolve(mockKeyField, "criteria/isActive/$eq")).thenReturn(criteriaEqualsIsActiveField);
+        when(IOC.resolve(mockKeyField, "criteria/startDateTime/$date-to")).thenReturn(criteriaDateToStartDateTimeField);
+
         targetTask = mock(IDatabaseTask.class);
         testTask = new GetObjectFromCachedCollectionTask(targetTask);
     }
 
     @Test
-    public void MustCorrectPrepareQueryForSelecting() throws ResolutionException, TaskPrepareException, ReadValueException, ChangeValueException {
+    public void MustCorrectPrepareQueryForSelecting() throws Exception {
 
-        Key iobjectKey = mock(Key.class);
-        IObject testIObject = mock(IObject.class);
-        when(IOC.resolve(IOC.getKeyForKeyStorage(), IObject.class.toString())).thenReturn(iobjectKey);
-        when(IOC.resolve(iobjectKey)).thenReturn(testIObject);
-
-        Key searchCachedCollectionKey = mock(Key.class);
-        CriteriaCachedCollectionQuery criteriaQuery = mock(CriteriaCachedCollectionQuery.class);
-
-        when(Keys.getOrAdd(CriteriaCachedCollectionQuery.class.toString())).thenReturn(searchCachedCollectionKey);
-        when(IOC.resolve(searchCachedCollectionKey, testIObject)).thenReturn(criteriaQuery);
-
-        EQMessage eqMessage = mock(EQMessage.class);
-        Key eqKey = mock(Key.class);
-        when(Keys.getOrAdd(EQMessage.class.toString())).thenReturn(eqKey);
-        when(IOC.resolve(eqKey, testIObject)).thenReturn(eqMessage);
-
-        DateToMessage dateToMessage = mock(DateToMessage.class);
-        Key dateToKey = mock(Key.class);
-        when(Keys.getOrAdd(DateToMessage.class.toString())).thenReturn(dateToKey);
-        when(IOC.resolve(dateToKey, testIObject)).thenReturn(dateToMessage);
-
-        GetObjectFromCachedCollectionQuery srcQueryObject = mock(GetObjectFromCachedCollectionQuery.class);
         IObject query = mock(IObject.class);
-        when(srcQueryObject.getKey()).thenReturn(key);
-        when(srcQueryObject.wrapped()).thenReturn(query);
+        when(keyNameField.out(query)).thenReturn("keyName");
+        when(keyValueField.out(query)).thenReturn("keyValue");
 
-        Key getObjectFromCachedGalleryQueryKey = mock(Key.class);
+        IObject queryForNestedTask = mock(IObject.class);
+        IKey keyIObject = mock(IKey.class);
+        when(Keys.getOrAdd(IObject.class.toString())).thenReturn(keyIObject);
+        when(IOC.resolve(keyIObject)).thenReturn(queryForNestedTask);
 
-        when(Keys.getOrAdd(GetObjectFromCachedCollectionQuery.class.toString())).thenReturn(getObjectFromCachedGalleryQueryKey);
-        when(IOC.resolve(getObjectFromCachedGalleryQueryKey, query)).thenReturn(srcQueryObject);
+        Field<String> criteriaEqualsKeyField = mock(Field.class);
+        when(IOC.resolve(mockKeyField, "keyName/$eq/keyValue")).thenReturn(criteriaEqualsKeyField);
 
         testTask.prepare(query);
 
-        verify(srcQueryObject).getKey();
-        verify(eqMessage).setEq(key);
-        verify(criteriaQuery).setKey(eqMessage);
-
-        verify(eqMessage).setEq(Boolean.toString(true));
-        verify(criteriaQuery).setIsActive(eqMessage);
-
-        verify(dateToMessage).setDateTo(any());// FIXME: 6/23/16 must test time
-        verify(criteriaQuery).setStartDateTime(dateToMessage);
-
-        verify(srcQueryObject).setPageNumber(0);
-        verify(srcQueryObject).setPageSize(100);// FIXME: 6/23/16 must test time with @GetObjectFromCachedCollectionTask
-        verify(srcQueryObject).setCriteria(criteriaQuery);
-
-        verify(targetTask).prepare(query);
+        verify(targetTask).prepare(queryForNestedTask);
+        verify(criteriaEqualsKeyField).out(queryForNestedTask);
+        verify(pageSizeField).in(queryForNestedTask, 100);
+        verify(pageNumberField).in(queryForNestedTask, 1);
+        verify(criteriaEqualsIsActiveField).in(queryForNestedTask, true);
+        verify(criteriaDateToStartDateTimeField).in(eq(queryForNestedTask), anyString());
     }
 
     @Test(expected = TaskPrepareException.class)
     public void MustInCorrectPrepareQueryForSelectingWhenIOCThrowException() throws ResolutionException, TaskPrepareException, ReadValueException, ChangeValueException {
 
-        IObject query = mock(IObject.class);
+        when(IOC.resolve(any())).thenThrow(new ResolutionException(""));
 
-        Key getObjectFromCachedGalleryQueryKey = mock(Key.class);
-        when(Keys.getOrAdd(GetObjectFromCachedCollectionQuery.class.toString())).thenReturn(getObjectFromCachedGalleryQueryKey);
-        when(IOC.resolve(getObjectFromCachedGalleryQueryKey, query)).thenThrow(new ResolutionException(""));
-
-        testTask.prepare(query);
-    }
-
-    @Test(expected = TaskPrepareException.class)
-    public void MustInCorrectPrepareQueryForSelectingWhenWrapperThrowException() throws ResolutionException, TaskPrepareException, ReadValueException, ChangeValueException {
-
-        Key iobjectKey = mock(Key.class);
-        IObject testIObject = mock(IObject.class);
-        when(IOC.resolve(IOC.getKeyForKeyStorage(), IObject.class.toString())).thenReturn(iobjectKey);
-        when(IOC.resolve(iobjectKey)).thenReturn(testIObject);
-
-        Key searchCachedCollectionKey = mock(Key.class);
-        CriteriaCachedCollectionQuery criteriaQuery = mock(CriteriaCachedCollectionQuery.class);
-
-        when(Keys.getOrAdd(CriteriaCachedCollectionQuery.class.toString())).thenReturn(searchCachedCollectionKey);
-        when(IOC.resolve(searchCachedCollectionKey, testIObject)).thenReturn(criteriaQuery);
-
-        EQMessage eqMessage = mock(EQMessage.class);
-        Key eqKey = mock(Key.class);
-        when(Keys.getOrAdd(EQMessage.class.toString())).thenReturn(eqKey);
-        when(IOC.resolve(eqKey, testIObject)).thenReturn(eqMessage);
-
-        doThrow(new ChangeValueException()).when(eqMessage).setEq(any());
-
-        GetObjectFromCachedCollectionQuery srcQueryObject = mock(GetObjectFromCachedCollectionQuery.class);
-        IObject query = mock(IObject.class);
-        when(srcQueryObject.getKey()).thenReturn(key);
-        when(srcQueryObject.wrapped()).thenReturn(query);
-
-        Key getObjectFromCachedGalleryQueryKey = mock(Key.class);
-
-        when(Keys.getOrAdd(GetObjectFromCachedCollectionQuery.class.toString())).thenReturn(getObjectFromCachedGalleryQueryKey);
-        when(IOC.resolve(getObjectFromCachedGalleryQueryKey, query)).thenReturn(srcQueryObject);
-
-        testTask.prepare(query);
+        testTask.prepare(mock(IObject.class));
     }
 
     @Test
