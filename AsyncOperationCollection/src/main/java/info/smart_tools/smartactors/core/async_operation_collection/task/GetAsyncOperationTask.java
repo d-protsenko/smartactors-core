6327a68c068cd26071cc0aed0b6e18d7fd0f6de4
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-07-11 17:28:02 +0600

    fix GetAsyncOperationTask using fields and constructor

--- AsyncOperationCollection/src/main/java/info/smart_tools/smartactors/core/async_operation_collection/task/GetAsyncOperationTask.java
+++ AsyncOperationCollection/src/main/java/info/smart_tools/smartactors/core/async_operation_collection/task/GetAsyncOperationTask.java
@@ -1,22 +1,19 @@
 package info.smart_tools.smartactors.core.async_operation_collection.task;
 
-import info.smart_tools.smartactors.core.async_operation_collection.wrapper.get_item.AsyncOperationTaskQuery;
-import info.smart_tools.smartactors.core.async_operation_collection.wrapper.get_item.EQMessage;
-import info.smart_tools.smartactors.core.async_operation_collection.wrapper.get_item.GetAsyncOperationQuery;
+import info.smart_tools.smartactors.core.async_operation_collection.exception.GetAsyncOperationException;
 import info.smart_tools.smartactors.core.db_storage.interfaces.StorageConnection;
 import info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskSetConnectionException;
+import info.smart_tools.smartactors.core.ifield.IField;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
 import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
-import info.smart_tools.smartactors.core.iobject.IFieldName;
 import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
-import info.smart_tools.smartactors.core.wrapper_generator.Field;
 
 /**
  * Task-facade for read task for async operations collection
@@ -25,24 +22,29 @@ public class GetAsyncOperationTask implements IDatabaseTask {
 
     private IDatabaseTask getItemTask;
 
-    private Field<Integer> pageNumberField;
-    private Field<Integer> pageSizeField;
-    private Field<IObject> queryField;
-    private Field<String> eqField;
-    private Field<String> tokenField;
+    private IField pageNumberField;
+    private IField pageSizeField;
+    private IField queryField;
+    private IField eqField;
+    private IField tokenField;
 
     /**
      * Constructor
      * @param getItemTask nested task for read object
+     * @throws GetAsyncOperationException
      */
-    public GetAsyncOperationTask(final IDatabaseTask getItemTask) throws ResolutionException, InvalidArgumentException {
+    public GetAsyncOperationTask(final IDatabaseTask getItemTask) throws GetAsyncOperationException {
         this.getItemTask = getItemTask;
 
-        pageNumberField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "pageNumber"));
-        pageSizeField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "pageSize"));
-        queryField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "query"));
-        eqField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "$eq"));
-        tokenField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "token"));
+        try {
+            pageNumberField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "pageNumber");
+            pageSizeField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "pageSize");
+            queryField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "query");
+            eqField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "$eq");
+            tokenField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "token");
+        } catch (ResolutionException e) {
+            throw new GetAsyncOperationException("Can't resolve one of fields", e);
+        }
     }
 
     /**
@@ -59,11 +61,11 @@ public class GetAsyncOperationTask implements IDatabaseTask {
 
             IObject criteriaIObject = IOC.resolve(Keys.getOrAdd(IObject.class.toString()));
 
-            eqField.in(criteriaIObject, tokenField.out(query));
+            eqField.out(criteriaIObject, tokenField.in(query));
 
-            pageNumberField.in(query, 1);
-            pageSizeField.in(query, 1);
-            queryField.in(query, criteriaIObject);
+            pageNumberField.out(query, 1);
+            pageSizeField.out(query, 1);
+            queryField.out(query, criteriaIObject);
 
             getItemTask.prepare(query);
         } catch (ResolutionException e) {
