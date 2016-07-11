0da9fdd5d50945dc6eefe4e3e929bbf118ff6049
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-07-11 17:22:39 +0600

    fix CreateAsyncOperationTask using fields

--- AsyncOperationCollection/src/main/java/info/smart_tools/smartactors/core/async_operation_collection/task/CreateAsyncOperationTask.java
+++ AsyncOperationCollection/src/main/java/info/smart_tools/smartactors/core/async_operation_collection/task/CreateAsyncOperationTask.java
@@ -1,21 +1,18 @@
 package info.smart_tools.smartactors.core.async_operation_collection.task;
 
-import info.smart_tools.smartactors.core.async_operation_collection.wrapper.create_item.AsyncDocument;
-import info.smart_tools.smartactors.core.async_operation_collection.wrapper.create_item.CreateOperationQuery;
 import info.smart_tools.smartactors.core.db_storage.interfaces.StorageConnection;
 import info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskSetConnectionException;
+import info.smart_tools.smartactors.core.ifield.IField;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
 import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
-import info.smart_tools.smartactors.core.iobject.IFieldName;
 import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
-import info.smart_tools.smartactors.core.wrapper_generator.Field;
 
 /**
  * DB task for creating operations in db
@@ -24,11 +21,11 @@ public class CreateAsyncOperationTask implements IDatabaseTask {
 
     private IDatabaseTask task;
 
-    private Field<IObject> asyncDataField;
-    private Field<Boolean> doneFlagField;
-    private Field<String> tokenField;
-    private Field<String> expiredTimeField;
-    private Field<IObject> documentField;
+    private IField asyncDataField;
+    private IField doneFlagField;
+    private IField tokenField;
+    private IField expiredTimeField;
+    private IField documentField;
     /**
      * Constructor
      * @param task the insert DB task
@@ -36,11 +33,11 @@ public class CreateAsyncOperationTask implements IDatabaseTask {
     public CreateAsyncOperationTask(final IDatabaseTask task) throws ResolutionException, InvalidArgumentException {
         this.task = task;
 
-        asyncDataField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "asyncData"));
-        doneFlagField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "done"));
-        tokenField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "token"));
-        expiredTimeField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "expiredTime"));
-        documentField = new Field<>(IOC.resolve(Keys.getOrAdd(IFieldName.class.toString()), "document"));
+        asyncDataField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "asyncData");
+        doneFlagField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "done");
+        tokenField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "token");
+        expiredTimeField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "expiredTime");
+        documentField = IOC.resolve(Keys.getOrAdd(IField.class.toString()), "document");
     }
 
     /**
@@ -59,17 +56,17 @@ public class CreateAsyncOperationTask implements IDatabaseTask {
 
             IObject document = IOC.resolve(Keys.getOrAdd(IObject.class.toString()));
 
-            asyncDataField.in(document, asyncDataField.out(query));
-            doneFlagField.in(document, false);
-            tokenField.in(document, tokenField.out(query));
-            expiredTimeField.in(document, expiredTimeField.out(query));
-            documentField.in(query, document);
+            asyncDataField.out(document, asyncDataField.in(query));
+            doneFlagField.out(document, false);
+            tokenField.out(document, tokenField.in(query));
+            expiredTimeField.out(document, expiredTimeField.in(query));
+            documentField.out(query, document);
 
             task.prepare(query);
         } catch (ResolutionException e) {
             throw new TaskPrepareException("Can't resolve objects during prepare create async operation", e);
         } catch (ReadValueException | ChangeValueException | InvalidArgumentException e) {
-            throw new TaskPrepareException("Can't prepare query for create async operation cause one of Field.out operation down", e);
+            throw new TaskPrepareException("Can't prepare query for create async operation cause one of IField.out operation down", e);
         }
     }
 
