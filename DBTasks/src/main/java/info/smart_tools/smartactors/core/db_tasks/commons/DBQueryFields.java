3d0c86b76a88cdccc1d6958556a19db62f194cdd
Maminsibirac <turkish_13@mail.ru>
2016-07-11 09:11:50 +0600

    Update tasks.

---
+++ DBTasks/src/main/java/info/smart_tools/smartactors/core/db_tasks/commons/DBQueryFields.java
@@ -0,0 +1,7 @@
+package info.smart_tools.smartactors.core.db_tasks.commons;
+
+/**
+ * Created by Maminsibirac on 09.07.2016.
+ */
+public class DBQueryFields {
+}
--- SqlCommons/src/main/java/info/smart_tools/smartactors/core/sql_commons/IQueryStatementFactory.java
+++ SqlCommons/src/main/java/info/smart_tools/smartactors/core/sql_commons/IQueryStatementFactory.java
@@ -3,7 +3,7 @@ package info.smart_tools.smartactors.core.sql_commons;
 import info.smart_tools.smartactors.core.sql_commons.exception.QueryStatementFactoryException;
 
 @FunctionalInterface
-public interface QueryStatementFactory {
+public interface IQueryStatementFactory {
 
     QueryStatement create() throws QueryStatementFactoryException;
 }
