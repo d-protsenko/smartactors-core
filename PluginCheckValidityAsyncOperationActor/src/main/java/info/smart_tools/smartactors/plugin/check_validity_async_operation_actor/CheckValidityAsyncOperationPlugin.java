cc441cfd402962be74b63c2b590567c0b9b689ff
Vitaly <stels95@list.ru>
2016-07-12 17:13:52 +0600

    create test

--- PluginCheckValidityAsyncOperationActor/src/main/java/info/smart_tools/smartactors/plugin/check_validity_async_operation_actor/CheckValidityAsyncOperationPlugin.java
+++ PluginCheckValidityAsyncOperationActor/src/main/java/info/smart_tools/smartactors/plugin/check_validity_async_operation_actor/CheckValidityAsyncOperationPlugin.java
@@ -1,9 +1,56 @@
 package info.smart_tools.smartactors.plugin.check_validity_async_operation_actor;
 
+import info.smart_tools.smartactors.core.actors.check_validity_async_operation.CheckValidityAsyncOperationActor;
+import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
+import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
+import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
+import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
+import info.smart_tools.smartactors.core.iioccontainer.exception.RegistrationException;
+import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.core.ikey.IKey;
+import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.core.iobject.IObject;
+import info.smart_tools.smartactors.core.ioc.IOC;
+import info.smart_tools.smartactors.core.iplugin.IPlugin;
+import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
+import info.smart_tools.smartactors.core.named_keys_storage.Keys;
+
 /**
  * Plugin for load IOC-strategy for check validity asynchronous operation actor
  */
-public class CheckValidityAsyncOperationPlugin {
+public class CheckValidityAsyncOperationPlugin implements IPlugin {
 
-    private final
+    private final IBootstrap<IBootstrapItem<String>> bootstrap;
+
+    /**
+     * Constructor for create instance of CheckValidityAsyncOperationPlugin
+     * @param bootstrap the bootstrap
+     */
+    public CheckValidityAsyncOperationPlugin(final IBootstrap<IBootstrapItem<String>> bootstrap) {
+        this.bootstrap = bootstrap;
+    }
+
+    @Override
+    public void load() throws PluginException {
+        try {
+            IKey operationKey = Keys.getOrAdd(CheckValidityAsyncOperationActor.class.toString());
+            IBootstrapItem<String> item = new BootstrapItem("CreateCheckValidityAsyncOperationActor");
+
+            item.process(() -> {
+                try {
+                    IOC.register(operationKey, new CreateNewInstanceStrategy(
+                            (args) -> {
+                                return new CheckValidityAsyncOperationActor((IObject) args[0]);
+                        }
+                    ));
+                } catch (RegistrationException | InvalidArgumentException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            bootstrap.add(item);
+        } catch (ResolutionException | InvalidArgumentException e) {
+            throw new PluginException("Can't load CheckValidityAsyncOperation plugin", e);
+        }
+
+    }
 }
---
+++ PluginCheckValidityAsyncOperationActor/src/test/java/info/smart_tools/smartactors/plugin/check_validity_async_operation_actor/CheckValidityAsyncOperationPluginTest.java
@@ -0,0 +1,13 @@
+package info.smart_tools.smartactors.plugin.check_validity_async_operation_actor;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CheckValidityAsyncOperationPluginTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
