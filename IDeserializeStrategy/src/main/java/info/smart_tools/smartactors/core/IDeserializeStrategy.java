de44010eecf170de662a33f26caa7ab329b3976c
ValchukDmitry <dart.fk712@gmail.com>
2016-07-12 16:49:10 +0600

    add interface for deserialization strategies

--- IDeserializeStrategy/src/main/java/info/smart_tools/smartactors/core/IDeserializeStrategy.java
+++ IDeserializeStrategy/src/main/java/info/smart_tools/smartactors/core/IDeserializeStrategy.java
@@ -3,11 +3,12 @@ package info.smart_tools.smartactors.core;
 
 import info.smart_tools.smartactors.core.exceptions.DeserializationException;
 import info.smart_tools.smartactors.core.imessage.IMessage;
+import info.smart_tools.smartactors.core.iobject.IObject;
 import io.netty.handler.codec.http.FullHttpRequest;
 
 /**
  * Strategy for different deserialization logic of http request to message
  */
 public interface IDeserializeStrategy {
-    IMessage deserialize(FullHttpRequest request) throws DeserializationException;
+    IObject deserialize(FullHttpRequest request) throws DeserializationException;
 }
