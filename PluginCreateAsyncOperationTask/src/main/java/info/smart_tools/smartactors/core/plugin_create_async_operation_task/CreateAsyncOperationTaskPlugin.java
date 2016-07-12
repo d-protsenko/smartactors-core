013602ec196e62497cf47e49b8e2231520e751e5
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-07-12 18:15:45 +0600

    added registration of other tasks

--- PluginCreateAsyncOperationTask/src/main/java/info/smart_tools/smartactors/core/plugin_create_async_operation_task/CreateAsyncOperationTaskPlugin.java
+++ PluginCreateAsyncOperationTask/src/main/java/info/smart_tools/smartactors/core/plugin_create_async_operation_task/CreateAsyncOperationTaskPlugin.java
@@ -1,7 +1,13 @@
 package info.smart_tools.smartactors.core.plugin_create_async_operation_task;
 
 import info.smart_tools.smartactors.core.async_operation_collection.exception.CreateAsyncOperationException;
+import info.smart_tools.smartactors.core.async_operation_collection.exception.DeleteAsyncOperationException;
+import info.smart_tools.smartactors.core.async_operation_collection.exception.GetAsyncOperationException;
+import info.smart_tools.smartactors.core.async_operation_collection.exception.UpdateAsyncOperationException;
 import info.smart_tools.smartactors.core.async_operation_collection.task.CreateAsyncOperationTask;
+import info.smart_tools.smartactors.core.async_operation_collection.task.DeleteAsyncOperationTask;
+import info.smart_tools.smartactors.core.async_operation_collection.task.GetAsyncOperationTask;
+import info.smart_tools.smartactors.core.async_operation_collection.task.UpdateAsyncOperationTask;
 import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
 import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
 import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
@@ -15,6 +21,7 @@ import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.iplugin.IPlugin;
 import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
+import info.smart_tools.smartactors.core.singleton_strategy.SingletonStrategy;
 
 import java.util.function.Function;
 
@@ -32,24 +39,44 @@ public class CreateAsyncOperationTaskPlugin implements IPlugin {
     @Override
     public void load() throws PluginException {
         try {
-            IKey createAsyncOperationTaskKey = Keys.getOrAdd(CreateAsyncOperationTask.class.toString());
             IBootstrapItem<String> item = new BootstrapItem("CreateAsyncOperationTaskPlugin");
 
             item.process(() -> {
                 try {
-                    IOC.register(createAsyncOperationTaskKey, new CreateNewInstanceStrategy((args) -> {
+                    IKey createOperationTasksKey = Keys.getOrAdd(CreateAsyncOperationTask.class.toString());
+                    IOC.register(createOperationTasksKey, new CreateNewInstanceStrategy((args) -> {
                         try {
                             return new CreateAsyncOperationTask((IDatabaseTask) args[0]);
                         } catch (CreateAsyncOperationException e) {
                             throw new RuntimeException("Target argument must be IDatabaseTask, but was " + args[0].getClass().toString(), e);
                         }
                     }));
-                } catch (RegistrationException | InvalidArgumentException e) {
+                    IKey getOperationTasksKey = Keys.getOrAdd(GetAsyncOperationTask.class.toString());
+                    IOC.register(getOperationTasksKey, new CreateNewInstanceStrategy((args) -> {
+                        try {
+                            return new GetAsyncOperationTask((IDatabaseTask) args[0]);
+                        } catch (GetAsyncOperationException e) {
+                            throw new RuntimeException("Target argument must be IDatabaseTask, but was " + args[0].getClass().toString(), e);
+                        }
+                    }));
+                    IKey updateOperationTasksKey = Keys.getOrAdd(UpdateAsyncOperationTask.class.toString());
+                    IOC.register(updateOperationTasksKey, new CreateNewInstanceStrategy((args) -> {
+                        try {
+                            return new UpdateAsyncOperationTask((IDatabaseTask) args[0]);
+                        } catch (UpdateAsyncOperationException e) {
+                            throw new RuntimeException("Target argument must be IDatabaseTask, but was " + args[0].getClass().toString(), e);
+                        }
+                    }));
+                    IKey deleteOperationTasksKey = Keys.getOrAdd(DeleteAsyncOperationTask.class.toString());
+                    IOC.register(deleteOperationTasksKey, new CreateNewInstanceStrategy((args) -> {
+                        return new DeleteAsyncOperationTask((IDatabaseTask) args[0]);
+                    }));
+                } catch (RegistrationException | InvalidArgumentException | ResolutionException e) {
                     throw new RuntimeException(e);
                 }
             });
             bootstrap.add(item);
-        } catch (ResolutionException | InvalidArgumentException e) {
+        } catch (InvalidArgumentException e) {
             throw new PluginException("Can't load CreateAsyncOperationTask plugin", e);
         }
     }
