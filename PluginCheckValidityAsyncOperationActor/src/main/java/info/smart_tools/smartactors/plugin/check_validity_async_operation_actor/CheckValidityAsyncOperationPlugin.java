77ada4c7eb84530b2613a6e0901527899a6f1c1c
Vitaly <stels95@list.ru>
2016-07-12 16:31:30 +0600

    add pom file

---
+++ PluginCheckValidityAsyncOperationActor/src/main/java/info/smart_tools/smartactors/plugin/check_validity_async_operation_actor/CheckValidityAsyncOperationPlugin.java
@@ -0,0 +1,9 @@
+package info.smart_tools.smartactors.plugin.check_validity_async_operation_actor;
+
+/**
+ * Plugin for load IOC-strategy for check validity asynchronous operation actor
+ */
+public class CheckValidityAsyncOperationPlugin {
+
+    private final
+}
