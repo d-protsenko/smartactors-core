4899c2350486531d14b4d15540dfa9e0f8a4fd68
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-07-13 13:50:15 +0600

    add tests

--- PluginCompileQuery/src/test/java/info/smart_tools/smartactors/plugin/compile_query/CompileQueryPluginTest.java
+++ PluginCompileQuery/src/test/java/info/smart_tools/smartactors/plugin/compile_query/CompileQueryPluginTest.java
@@ -6,9 +6,11 @@ import info.smart_tools.smartactors.core.db_storage.interfaces.CompiledQuery;
 import info.smart_tools.smartactors.core.db_storage.interfaces.StorageConnection;
 import info.smart_tools.smartactors.core.iaction.IPoorAction;
 import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
+import info.smart_tools.smartactors.core.iioccontainer.exception.RegistrationException;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
 import info.smart_tools.smartactors.core.ikey.IKey;
 import info.smart_tools.smartactors.core.ioc.IOC;
+import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import info.smart_tools.smartactors.core.sql_commons.QueryStatement;
 import info.smart_tools.smartactors.core.sql_commons.QueryStatementFactory;
@@ -22,6 +24,8 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import java.util.HashMap;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -51,7 +55,7 @@ public class CompileQueryPluginTest {
     }
 
     @Test
-    public void ShouldAddNewItemDuringLoad() throws Exception {
+    public void MustCorrectLoadPlugin() throws Exception {
 
         IKey compiledQueryKey = mock(IKey.class);
         when(Keys.getOrAdd(CompiledQuery.class.toString())).thenReturn(compiledQueryKey);
@@ -103,5 +107,115 @@ public class CompileQueryPluginTest {
         verify(queryMap).put(queryKey, query);
 
         verify(bootstrap).add(eq(bootstrapItem));
+    }
+
+    @Test
+    public void MustIncorrectLoadPluginWhenKeysThrowException() throws Exception {
+
+        when(Keys.getOrAdd(CompiledQuery.class.toString())).thenThrow(new ResolutionException(""));
+
+        try {
+            plugin.load();
+        } catch (PluginException e) {
+
+            verifyStatic();
+            Keys.getOrAdd(CompiledQuery.class.toString());
+            return;
+        }
+        assertTrue("Must throw exception", false);
+    }
+
+    @Test
+    public void MustInCorrectLoadPluginWhenKeysThrowException() throws Exception {
+
+        IKey compiledQueryKey = mock(IKey.class);
+        when(Keys.getOrAdd(CompiledQuery.class.toString())).thenReturn(compiledQueryKey);
+
+        HashMap<QueryKey, CompiledQuery> queryMap = mock(HashMap.class);
+        whenNew(HashMap.class).withNoArguments().thenReturn(queryMap);
+
+        BootstrapItem bootstrapItem = mock(BootstrapItem.class);
+        whenNew(BootstrapItem.class).withArguments("CompileQueryPlugin").thenReturn(bootstrapItem);
+        plugin.load();
+
+        verifyNew(BootstrapItem.class).withArguments("CompileQueryPlugin");
+
+        ArgumentCaptor<IPoorAction> iPoorActionArgumentCaptor = ArgumentCaptor.forClass(IPoorAction.class);
+        verify(bootstrapItem).process(iPoorActionArgumentCaptor.capture());
+
+        iPoorActionArgumentCaptor.getValue().execute();
+
+        ArgumentCaptor<CreateNewInstanceStrategy> createNewInstanceStrategyArgumentCaptor =
+                ArgumentCaptor.forClass(CreateNewInstanceStrategy.class);
+        verifyStatic();
+        IOC.register(eq(compiledQueryKey), createNewInstanceStrategyArgumentCaptor.capture());
+
+        StorageConnection connection = mock(StorageConnection.class);
+        String task = "task";
+        QueryStatementFactory factory = mock(QueryStatementFactory.class);
+
+        String id = "id";
+        when(connection.getId()).thenReturn(id);
+
+        QueryKey queryKey = mock(QueryKey.class);
+        when(QueryKey.create(task, id)).thenReturn(queryKey);
+
+        QueryStatement queryStatement = mock(QueryStatement.class);
+        when(factory.create()).thenReturn(queryStatement);
+
+        CompiledQuery query = mock(CompiledQuery.class);
+        when(connection.compileQuery(queryStatement)).thenReturn(query);
+
+        assertTrue("Must be equal", createNewInstanceStrategyArgumentCaptor.getValue().resolve(connection, task, factory) == query);
+
+        verify(connection).getId();
+        verifyStatic();
+        QueryKey.create(task, id);
+        verify(queryMap).get(queryKey);
+
+        verify(factory).create();
+        verify(connection).compileQuery(queryStatement);
+        verify(queryMap).put(queryKey, query);
+
+        verify(bootstrap).add(eq(bootstrapItem));
+    }
+
+    @Test
+    public void MustInCorrectExecuteInIPoorActionWhenThrowRegistrationException() throws Exception {
+
+        IKey cachedCollectionKey = mock(IKey.class);
+        when(Keys.getOrAdd(CompiledQuery.class.toString())).thenReturn(cachedCollectionKey);
+
+        BootstrapItem bootstrapItem = mock(BootstrapItem.class);
+        whenNew(BootstrapItem.class).withArguments("CompileQueryPlugin").thenReturn(bootstrapItem);
+        when(bootstrapItem.after(anyString())).thenReturn(bootstrapItem);
+
+        HashMap<String, CompiledQuery> collectionMap = mock(HashMap.class);
+        whenNew(HashMap.class).withNoArguments().thenReturn(collectionMap);
+
+        plugin.load();
+
+        verifyStatic();
+        Keys.getOrAdd(CompiledQuery.class.toString());
+
+        verifyNew(BootstrapItem.class).withArguments("CompileQueryPlugin");
+
+        ArgumentCaptor<IPoorAction> actionArgumentCaptor = ArgumentCaptor.forClass(IPoorAction.class);
+        verify(bootstrapItem).process(actionArgumentCaptor.capture());
+
+        doThrow(new RegistrationException("")).when(IOC.class);
+        IOC.register(eq(cachedCollectionKey), any());
+
+        try {
+            actionArgumentCaptor.getValue().execute();
+        } catch (RuntimeException e) {
+            verifyStatic();
+            IOC.register(eq(cachedCollectionKey), any());
+
+            verifyNew(HashMap.class).withNoArguments();
+            verify(bootstrap).add(bootstrapItem);
+            return;
+        }
+        assertTrue("Must throw exception", false);
     }
 }
