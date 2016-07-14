d3555a7b0c35a259a2be11a059ff3d5a8ec49fce
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-07-14 12:24:07 +0600

    write plugin without tests

---
+++ PluginIFieldName/src/main/java/info/smart_tools/smartactors/plugin/collection_name/CollectionNamePlugin.java/IFieldNamePlugin.java
@@ -0,0 +1,82 @@
+package info.smart_tools.smartactors.plugin.collection_name.CollectionNamePlugin.java;
+
+import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
+import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
+import info.smart_tools.smartactors.core.field_name.FieldName;
+import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
+import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
+import info.smart_tools.smartactors.core.ifield_name.IFieldName;
+import info.smart_tools.smartactors.core.iioccontainer.exception.RegistrationException;
+import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.core.ikey.IKey;
+import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.core.ioc.IOC;
+import info.smart_tools.smartactors.core.iplugin.IPlugin;
+import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
+import info.smart_tools.smartactors.core.named_keys_storage.Keys;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class IFieldNamePlugin implements IPlugin {
+
+    private final IBootstrap<IBootstrapItem<String>> bootstrap;
+
+    /**
+     * Constructor
+     * @param bootstrap bootstrap element
+     */
+    public IFieldNamePlugin(final IBootstrap<IBootstrapItem<String>> bootstrap) {
+        this.bootstrap = bootstrap;
+    }
+
+    @Override
+    public void load() throws PluginException {
+        try {
+            Map<String, IFieldName> fieldNamesMap = new ConcurrentHashMap<>();
+
+            IBootstrapItem<String> item = new BootstrapItem("IFieldNamePlugin");
+
+            item
+                    .after("IOC")
+                    .process(() -> {
+                        try {
+                            IKey iFieldNameKey = Keys.getOrAdd(IFieldName.class.toString());
+                            IOC.register(iFieldNameKey,
+                                    new CreateNewInstanceStrategy(
+                                            (args) -> {
+                                                try {
+                                                    String nameOfFieldName = (String) args[0];
+                                                    IFieldName result = fieldNamesMap.get(nameOfFieldName);
+
+                                                    if (result == null) {
+                                                        try {
+                                                            result = new FieldName(nameOfFieldName);
+                                                        } catch (InvalidArgumentException e) {
+                                                            throw new RuntimeException(
+                                                                    "Can't create FieldName with this name: "
+                                                                            + nameOfFieldName, e);
+                                                        }
+                                                        fieldNamesMap.put(nameOfFieldName, result);
+                                                    }
+
+                                                    return result;
+                                                } catch (ClassCastException e) {
+                                                    throw new RuntimeException("Can't cast object to String: " + args[0], e);
+                                                }
+                                        }
+                                    )
+                            );
+                        } catch (ResolutionException e) {
+                            throw new RuntimeException("Can't get IFieldName key");
+                        } catch (InvalidArgumentException e) {
+                            throw new RuntimeException("Can't get create strategy");
+                        } catch (RegistrationException e) {
+                            throw new RuntimeException("Can't get register new strategy");
+                        }
+                    });
+        } catch (InvalidArgumentException e) {
+            throw new PluginException("Can't get BootstrapItem from one of reason", e);
+        }
+    }
+}
