ac82d7bf357951d9c9ef3d841d066d8b17340aea
Andrey Kutalev <akutalev@gmail.com>
2016-07-14 21:21:43 +0600

    Change package-info text.

--- ClassGeneratorJavaCompileAPI/src/main/java/info/smart_tools/smartactors/core/class_generator_java_compile_api/class_builder/package-info.java
+++ ClassGeneratorJavaCompileAPI/src/main/java/info/smart_tools/smartactors/core/class_generator_java_compile_api/class_builder/package-info.java
@@ -1,4 +1,5 @@
 /**
- * Package contains {@link info.smart_tools.smartactors.core.class_generator_java_compile_api.class_builder.ClassBuilder}.
+ * Package contains special class for build class by given string parameters
+ * and other support classes
  */
 package info.smart_tools.smartactors.core.class_generator_java_compile_api.class_builder;
