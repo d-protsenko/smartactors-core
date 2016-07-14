603de8018eba5553d7db62d623939435ba0b7b65
aslesarev <aslesarev@hwdtech.ru>
2016-07-14 14:47:55 +0600

    add plugin for IObject simple impl

---
+++ PluginIObject/src/test/java/info/smart_tools/smartactors/plugin/iobject/IObjectPluginTest.java
@@ -0,0 +1,104 @@
+package info.smart_tools.smartactors.plugin.iobject;
+
+import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
+import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
+import info.smart_tools.smartactors.core.iaction.IPoorAction;
+import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
+import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.core.ikey.IKey;
+import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.core.iobject.IObject;
+import info.smart_tools.smartactors.core.ioc.IOC;
+import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
+import info.smart_tools.smartactors.core.named_keys_storage.Keys;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyNew;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+@PrepareForTest({IOC.class, Keys.class, IPoorAction.class, CreateNewInstanceStrategy.class, IObjectPlugin.class, IObject.class})
+@RunWith(PowerMockRunner.class)
+public class IObjectPluginTest {
+    private IObjectPlugin plugin;
+    private IBootstrap bootstrap;
+
+    @Before
+    public void setUp() throws Exception {
+
+        mockStatic(IOC.class);
+        mockStatic(Keys.class);
+        mockStatic(IObject.class);
+
+        IKey keyGeneral = mock(IKey.class);
+        IKey keyPlugin = mock(IKey.class);
+        when(IOC.getKeyForKeyStorage()).thenReturn(keyGeneral);
+        when(IOC.resolve(eq(keyGeneral), eq("IObjectPlugin"))).thenReturn(keyPlugin);
+
+        bootstrap = mock(IBootstrap.class);
+        plugin = new IObjectPlugin(bootstrap);
+    }
+
+    @Test
+    public void ShouldCorrectLoadPlugin() throws Exception {
+
+        IKey IObjectKey = mock(IKey.class);
+        when(Keys.getOrAdd(IObject.class.toString())).thenReturn(IObjectKey);
+
+        BootstrapItem bootstrapItem = mock(BootstrapItem.class);
+        whenNew(BootstrapItem.class).withArguments("IObjectPlugin").thenReturn(bootstrapItem);
+        when(bootstrapItem.after(anyString())).thenReturn(bootstrapItem);
+
+        plugin.load();
+
+        verifyNew(BootstrapItem.class).withArguments("IObjectPlugin");
+
+        ArgumentCaptor<IPoorAction> actionArgumentCaptor = ArgumentCaptor.forClass(IPoorAction.class);
+        verify(bootstrapItem).process(actionArgumentCaptor.capture());
+
+        ArgumentCaptor<CreateNewInstanceStrategy> createNewInstanceStrategyArgumentCaptor =
+                ArgumentCaptor.forClass(CreateNewInstanceStrategy.class);
+        actionArgumentCaptor.getValue().execute();
+
+        verifyStatic();
+        IOC.register(eq(IObjectKey), createNewInstanceStrategyArgumentCaptor.capture());
+
+        verify(bootstrap).add(bootstrapItem);
+    }
+
+    @Test(expected = PluginException.class)
+    public void ShouldThrowPluginException_When_BootstrapItemThrowsException() throws Exception {
+
+        whenNew(BootstrapItem.class).withArguments("IObjectPlugin").thenThrow(new InvalidArgumentException(""));
+        plugin.load();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void ShouldThrowRuntimeException_When_LambdaThrowsException() throws Exception {
+
+        when(Keys.getOrAdd(IObject.class.toString())).thenThrow(new ResolutionException(""));
+
+        BootstrapItem bootstrapItem = mock(BootstrapItem.class);
+        whenNew(BootstrapItem.class).withArguments("IObjectPlugin").thenReturn(bootstrapItem);
+        when(bootstrapItem.after(anyString())).thenReturn(bootstrapItem);
+
+        plugin.load();
+
+        verifyNew(BootstrapItem.class).withArguments("IObjectPlugin");
+
+        ArgumentCaptor<IPoorAction> actionArgumentCaptor = ArgumentCaptor.forClass(IPoorAction.class);
+        verify(bootstrapItem).process(actionArgumentCaptor.capture());
+        actionArgumentCaptor.getValue().execute();
+    }
+}
