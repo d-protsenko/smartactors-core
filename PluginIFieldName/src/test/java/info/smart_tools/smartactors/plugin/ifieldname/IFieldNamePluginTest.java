85500e0ef49b9c829a9b927af9b44ae3556422d3
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-07-14 13:06:28 +0600

    complete first test

--- PluginIFieldName/src/test/java/info/smart_tools/smartactors/plugin/ifieldname/IFieldNamePluginTest.java
+++ PluginIFieldName/src/test/java/info/smart_tools/smartactors/plugin/ifieldname/IFieldNamePluginTest.java
@@ -1,6 +1,9 @@
 package info.smart_tools.smartactors.plugin.ifieldname;
 
 import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
+import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
+import info.smart_tools.smartactors.core.field_name.FieldName;
+import info.smart_tools.smartactors.core.iaction.IPoorAction;
 import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
 import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
 import info.smart_tools.smartactors.core.ifield_name.IFieldName;
@@ -10,6 +13,7 @@ import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -18,9 +22,11 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.*;
 
-@PrepareForTest({IOC.class, Keys.class, IFieldNamePlugin.class, IFieldName.class})
+@PrepareForTest({IOC.class, Keys.class, CreateNewInstanceStrategy.class, IFieldNamePlugin.class, IFieldName.class})
 @RunWith(PowerMockRunner.class)
 public class IFieldNamePluginTest {
 
@@ -31,7 +37,7 @@ public class IFieldNamePluginTest {
     public void setUp() throws Exception {
 
         mockStatic(IOC.class);
-        mockStatic(IKey.class);
+        mockStatic(Keys.class);
 
         bootstrap = mock(IBootstrap.class);
         plugin = new IFieldNamePlugin(bootstrap);
@@ -44,9 +50,47 @@ public class IFieldNamePluginTest {
         whenNew(ConcurrentHashMap.class).withNoArguments().thenReturn(fieldNamesMap);
 
         BootstrapItem item = mock(BootstrapItem.class);
-        whenNew(BootstrapItem.class).withArguments(("IFieldNamePlugin")).thenReturn(item);
+        whenNew(BootstrapItem.class).withArguments("IFieldNamePlugin").thenReturn(item);
 
         when(item.after(anyString())).thenReturn(item);
+
+        plugin.load();
+
+        verifyNew(ConcurrentHashMap.class).withNoArguments();
+        verifyNew(BootstrapItem.class).withArguments("IFieldNamePlugin");
+
+        verify(item).after("IOC");
+
+        ArgumentCaptor<IPoorAction> iPoorActionArgumentCaptor = ArgumentCaptor.forClass(IPoorAction.class);
+        verify(item).process(iPoorActionArgumentCaptor.capture());
+
+        verify(bootstrap).add(item);
+
+        IKey iFieldNameKey = mock(IKey.class);
+        when(Keys.getOrAdd(IFieldName.class.toString())).thenReturn(iFieldNameKey);
+
+        ArgumentCaptor<CreateNewInstanceStrategy> createNewInstanceStrategyArgumentCaptor =
+                ArgumentCaptor.forClass(CreateNewInstanceStrategy.class);
+
+        iPoorActionArgumentCaptor.getValue().execute();
+
+        verifyStatic();
+        Keys.getOrAdd(IFieldName.class.toString());
+
+        verifyStatic();
+        IOC.register(eq(iFieldNameKey), createNewInstanceStrategyArgumentCaptor.capture());
+
+        String exampleFieldName = "exampleField";
+        FieldName newFieldName = mock(FieldName.class);
+        whenNew(FieldName.class).withArguments(exampleFieldName).thenReturn(newFieldName);
+
+        assertTrue("Must return correct value",
+                createNewInstanceStrategyArgumentCaptor.getValue().resolve(exampleFieldName) == newFieldName);
+
+        verify(fieldNamesMap).get(exampleFieldName);
+        verifyNew(FieldName.class).withArguments(exampleFieldName);
+        verify(fieldNamesMap).put(exampleFieldName, newFieldName);
+
     }
 
 }
