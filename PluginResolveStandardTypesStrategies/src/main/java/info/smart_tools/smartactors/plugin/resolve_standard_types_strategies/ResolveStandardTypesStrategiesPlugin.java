052e289f93d081646c64a04b472d8e4760fc7f39
aslesarev <aslesarev@hwdtech.ru>
2016-07-21 20:27:00 +0600

    start plugin for resolve types strategies

---
+++ PluginResolveStandardTypesStrategies/src/main/java/info/smart_tools/smartactors/plugin/resolve_standard_types_strategies/ResolveStandardTypesStrategiesPlugin.java
@@ -0,0 +1,16 @@
+package info.smart_tools.smartactors.plugin.resolve_standard_types_strategies;
+
+import info.smart_tools.smartactors.core.iplugin.IPlugin;
+import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
+
+/**
+ * Plugin.
+ * Implements {@link IPlugin}
+ * Load strategies for resolving standard types
+ */
+public class ResolveStandardTypesStrategiesPlugin implements IPlugin {
+    @Override
+    public void load() throws PluginException {
+
+    }
+}
