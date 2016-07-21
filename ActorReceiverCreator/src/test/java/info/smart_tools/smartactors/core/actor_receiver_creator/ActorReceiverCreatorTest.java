908816069cc716da40c2b38fd18fd9cace1a0a94
Andrey Kutalev <akutalev@gmail.com>
2016-07-21 15:24:15 +0600

    Test of ActorReceiverCreator has been updated.

--- ActorReceiverCreator/src/test/java/info/smart_tools/smartactors/core/actor_receiver_creator/ActorReceiverCreatorTest.java
+++ ActorReceiverCreator/src/test/java/info/smart_tools/smartactors/core/actor_receiver_creator/ActorReceiverCreatorTest.java
@@ -2,7 +2,6 @@ package info.smart_tools.smartactors.core.actor_receiver_creator;
 
 import info.smart_tools.smartactors.core.actor_receiver.ActorReceiver;
 import info.smart_tools.smartactors.core.field_name.FieldName;
-import info.smart_tools.smartactors.core.handler_routing_receiver.HandlerRoutingReceiver;
 import info.smart_tools.smartactors.core.ifield.IField;
 import info.smart_tools.smartactors.core.ifield_name.IFieldName;
 import info.smart_tools.smartactors.core.iobject.IObject;
@@ -13,6 +12,7 @@ import info.smart_tools.smartactors.core.iroutable_object_creator.exceptions.Obj
 import info.smart_tools.smartactors.core.irouter.IRouter;
 import info.smart_tools.smartactors.core.iscope.IScope;
 import info.smart_tools.smartactors.core.iwrapper_generator.IWrapperGenerator;
+import info.smart_tools.smartactors.core.message_processing.IMessageProcessingSequence;
 import info.smart_tools.smartactors.core.message_processing.IMessageProcessor;
 import info.smart_tools.smartactors.core.message_processing.IMessageReceiver;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
@@ -31,7 +31,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -58,26 +61,32 @@ public class ActorReceiverCreatorTest {
                             }
                         })
         );
-        IOC.register(
-                IOC.resolve(IOC.getKeyForKeyStorage(), IField.class.getCanonicalName()),
-                new ResolveByNameIocStrategy(
-                        (a) -> {
-                            return mock(IField.class);
-                        }
-                )
-        );
+
+        Queue queue = mock(Queue.class);
+        when(queue.isEmpty()).thenReturn(true);
         IOC.register(
                 IOC.resolve(IOC.getKeyForKeyStorage(), "actor_receiver_queue"),
-                new SingletonStrategy(mock(Queue.class))
+                new SingletonStrategy(queue)
         );
+
+        AtomicBoolean isBusy = new AtomicBoolean();
+        isBusy.set(false);
         IOC.register(
                 IOC.resolve(IOC.getKeyForKeyStorage(), "actor_receiver_busyness_flag"),
-                new SingletonStrategy(mock(AtomicBoolean.class))
+                new SingletonStrategy(isBusy)
         );
+
+        // register wrapper generator
+        IResolveDependencyStrategy wgs = mock(IResolveDependencyStrategy.class);
+        IWrapperGenerator wg = mock(IWrapperGenerator.class);
+        IOC.register(Keys.getOrAdd(IWrapperGenerator.class.getCanonicalName()), wgs);
+        when(wgs.resolve()).thenReturn(wg);
+        MethodWrapper mw = new MethodWrapper();
+        when(wg.generate(IMethodWrapper.class)).thenReturn(mw);
     }
 
     @Test
-    public void checkCreation()
+    public void checkCreationAndExecution()
             throws Exception {
         IOC.register(
                 IOC.resolve(IOC.getKeyForKeyStorage(), IFieldName.class.getCanonicalName()),
@@ -91,6 +100,23 @@ public class ActorReceiverCreatorTest {
                         }
                 )
         );
+        IField field = mock(IField.class);
+        IOC.register(
+                IOC.resolve(IOC.getKeyForKeyStorage(), IField.class.getCanonicalName()),
+                new ResolveByNameIocStrategy(
+                        (a) -> {
+                            return field;
+                        }
+                )
+        );
+        // register receiver generator
+        IResolveDependencyStrategy rgs = mock(IResolveDependencyStrategy.class);
+        IReceiverGenerator rg = mock(IReceiverGenerator.class);
+        IOC.register(Keys.getOrAdd(IReceiverGenerator.class.getCanonicalName()), rgs);
+        when(rgs.resolve()).thenReturn(rg);
+        IMessageReceiver mr = mock(IMessageReceiver.class);
+        when(rg.generate(any(CustomActor.class), any(IResolveDependencyStrategy.class), any(String.class))).thenReturn(mr);
+
         IObject objectSection = mock(IObject.class);
         when(objectSection.getValue(new FieldName("name"))).thenReturn("actorID");
         when(objectSection.getValue(new FieldName("dependency"))).thenReturn("createSampleActorStrategy");
@@ -103,27 +129,28 @@ public class ActorReceiverCreatorTest {
         when(createSampleActorStrategy.resolve(wrapper))
                 .thenReturn(a);
 
-        // register wrapper generator
-        IResolveDependencyStrategy wgs = mock(IResolveDependencyStrategy.class);
-        IWrapperGenerator wg = mock(IWrapperGenerator.class);
-        IOC.register(Keys.getOrAdd(IWrapperGenerator.class.getCanonicalName()), wgs);
-        when(wgs.resolve()).thenReturn(wg);
-        MethodWrapper mw = new MethodWrapper();
-        when(wg.generate(IMethodWrapper.class)).thenReturn(mw);
-        // register receiver generator
-        IResolveDependencyStrategy rgs = mock(IResolveDependencyStrategy.class);
-        IReceiverGenerator rg = mock(IReceiverGenerator.class);
-        IOC.register(Keys.getOrAdd(IReceiverGenerator.class.getCanonicalName()), rgs);
-        when(rgs.resolve()).thenReturn(rg);
-        IMessageReceiver mr = mock(IMessageReceiver.class);
-        when(rg.generate(any(CustomActor.class), any(IResolveDependencyStrategy.class), any(String.class))).thenReturn(mr);
-
         ActorReceiverCreator arc = new ActorReceiverCreator();
         IRouter router = new Router();
 
         arc.createObject(router, objectSection);
         assertEquals(((Router) router).map.size(), 1);
-        assertSame(router.route("actorID").getClass(), ActorReceiver.class);
+        IMessageReceiver actorReceiver = router.route("actorID");
+        assertSame(actorReceiver.getClass(), ActorReceiver.class);
+
+        // mock IMessageProcessor, IMessageProcessingSequence, IObject as current sequence
+        IMessageProcessor processor = mock(IMessageProcessor.class);
+        IMessageProcessingSequence sequence = mock(IMessageProcessingSequence.class);
+        IObject currentSequence = mock(IObject.class);
+        IObject env = mock(IObject.class);
+        when(processor.getEnvironment()).thenReturn(env);
+        when(processor.getSequence()).thenReturn(sequence);
+        when(sequence.getCurrentReceiverArguments()).thenReturn(currentSequence);
+        // configure mock of field
+        when(field.in(currentSequence)).thenReturn("getSomeValue");
+        // configure handler message receiver mock
+        doNothing().when(mr).receive(processor);
+        actorReceiver.receive(processor);
+        verify(mr, times(1)).receive(processor);
     }
 
     @Test (expected = ObjectCreationException.class)
