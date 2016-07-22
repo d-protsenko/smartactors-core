71bc246f508bcc20c029cb27f563b2ed9252adf1
ValchukDmitry <dart.fk712@gmail.com>
2016-07-22 15:20:20 +0600

    delete some unused modules

--- EndpointGenerator/src/main/java/info/smart_tools/smartactors/core/endpoint_generator/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package contains generator for endpoint
- */
-package info.smart_tools.smartactors.core.endpoint_generator;
--- IEndpointCreator/src/main/java/info/smart_tools/smartactors/core/iendpoint_creator/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains interface for an endpoint creator.
- */
-package info.smart_tools.smartactors.core.iendpoint_creator;
