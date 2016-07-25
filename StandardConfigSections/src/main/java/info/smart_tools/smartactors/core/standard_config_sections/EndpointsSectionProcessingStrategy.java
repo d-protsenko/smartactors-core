cf62ece31faecf4dadd85ef7f1b86f60bcf094ae
ValchukDmitry <dart.fk712@gmail.com>
2016-07-25 17:14:17 +0600

    add test for endpoint section processing strategy

--- StandardConfigSections/src/main/java/info/smart_tools/smartactors/core/standard_config_sections/EndpointsSectionProcessingStrategy.java
+++ StandardConfigSections/src/main/java/info/smart_tools/smartactors/core/standard_config_sections/EndpointsSectionProcessingStrategy.java
@@ -2,6 +2,7 @@ package info.smart_tools.smartactors.core.standard_config_sections;
 
 import info.smart_tools.smartactors.core.HttpEndpoint;
 import info.smart_tools.smartactors.core.environment_handler.EnvironmentHandler;
+import info.smart_tools.smartactors.core.iasync_service.IAsyncService;
 import info.smart_tools.smartactors.core.ichain_storage.IChainStorage;
 import info.smart_tools.smartactors.core.ichain_storage.exceptions.ChainNotFoundException;
 import info.smart_tools.smartactors.core.iconfiguration_manager.ISectionStrategy;
@@ -14,9 +15,8 @@ import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.iqueue.IQueue;
-import info.smart_tools.smartactors.core.iroutable_object_creator.IRoutedObjectCreator;
-import info.smart_tools.smartactors.core.iroutable_object_creator.exceptions.ObjectCreationException;
 import info.smart_tools.smartactors.core.iscope_provider_container.exception.ScopeProviderException;
+import info.smart_tools.smartactors.core.itask.ITask;
 import info.smart_tools.smartactors.core.message_processing.IReceiverChain;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import info.smart_tools.smartactors.core.scope_provider.ScopeProvider;
@@ -32,6 +32,7 @@ import java.util.List;
  *     {
  *         "endpoints": [
  *             {
+ *                 "name": "endpointName",
  *                 "type": "http",
  *                 "port": 8080,
  *                 "startChain": "mainChain",
@@ -53,6 +54,7 @@ public class EndpointsSectionProcessingStrategy implements ISectionStrategy {
     private final IFieldName startChainNameFieldName;
     private final IFieldName stackDepthFieldName;
     private final IFieldName maxContentLengthFieldName;
+    private final IFieldName endpointNameFieldName;
 
     /**
      * The constructor.
@@ -70,6 +72,8 @@ public class EndpointsSectionProcessingStrategy implements ISectionStrategy {
                 IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), IFieldName.class.getCanonicalName()), "stackDepth");
         this.maxContentLengthFieldName =
                 IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), IFieldName.class.getCanonicalName()), "maxContentLength");
+        this.endpointNameFieldName =
+                IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), IFieldName.class.getCanonicalName()), "endpointName");
     }
 
     @Override
@@ -78,20 +82,26 @@ public class EndpointsSectionProcessingStrategy implements ISectionStrategy {
             List<IObject> endpointObjects = (List<IObject>) config.getValue(name);
             IChainStorage chainStorage = IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(),
                     IChainStorage.class.getCanonicalName()));
-            IQueue queue = IOC.resolve(Keys.getOrAdd("task_queue"));
+            IQueue<ITask> queue = IOC.resolve(Keys.getOrAdd("task_queue"));
             for (IObject endpoint : endpointObjects) {
                 // TODO: 25.07.16 add endpoint type
                 // TODO: 25.07.16 remove stack depth from endpoint config
+                // TODO: 25.07.16 add endpoint name
+
+                String endpointName = (String) endpoint.getValue(endpointNameFieldName);
                 String type = (String) endpoint.getValue(typeFieldName);
                 Integer port = (Integer) endpoint.getValue(portFieldName);
                 String startChainName = (String) endpoint.getValue(startChainNameFieldName);
                 Integer stackDepth = (Integer) endpoint.getValue(stackDepthFieldName);
                 Integer maxContentLength = (Integer) endpoint.getValue(maxContentLengthFieldName);
+
                 Object mapId = IOC.resolve(Keys.getOrAdd("chain_id_from_map_name"), startChainName);
                 IReceiverChain chain = chainStorage.resolve(mapId);
                 IEnvironmentHandler environmentHandler = new EnvironmentHandler(queue, stackDepth);
-                IOC.resolve(Keys.getOrAdd(HttpEndpoint.class.getCanonicalName()), port, maxContentLength,
-                        ScopeProvider.getCurrentScope(), environmentHandler, chain);
+                IAsyncService endpointService =
+                        IOC.resolve(Keys.getOrAdd(HttpEndpoint.class.getCanonicalName()), port, maxContentLength,
+                                ScopeProvider.getCurrentScope(), environmentHandler, chain);
+                endpointService.start();
             }
         } catch (ReadValueException | InvalidArgumentException | ScopeProviderException e) {
             throw new ConfigurationProcessingException("Error occurred loading \"endpoint\" configuration section.", e);
