ebc4f859d4642eb7abe780d1ae65a7b78f57ffd0
Vitaly <stels95@list.ru>
2016-07-28 17:11:58 +0600

    fix PluginCheckUserByEmailActor

--- PluginCheckUserByEmailActor/src/main/java/info/smart_tools/smartactors/plugin/check_user_by_email_actor/CheckUserByEmailActorPlugin.java
+++ PluginCheckUserByEmailActor/src/main/java/info/smart_tools/smartactors/plugin/check_user_by_email_actor/CheckUserByEmailActorPlugin.java
@@ -68,11 +68,11 @@ public class CheckUserByEmailActorPlugin implements IPlugin {
                                     )
                             );
                         } catch (ResolutionException e) {
-                            throw new ActionExecuteException("Can't get CheckUserByEmailActor key");
+                            throw new ActionExecuteException("Can't get CheckUserByEmailActor key", e);
                         } catch (InvalidArgumentException e) {
-                            throw new ActionExecuteException("Can't get create strategy");
+                            throw new ActionExecuteException("Can't get create strategy", e);
                         } catch (RegistrationException e) {
-                            throw new ActionExecuteException("Can't get register new strategy");
+                            throw new ActionExecuteException("Can't get register new strategy", e);
                         }
                     });
             bootstrap.add(item);
