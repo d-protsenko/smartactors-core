4550090b667da2d07e1f4434fe3683404fec6436
Vitaly <stels95@list.ru>
2016-07-31 14:37:13 +0600

    remove unused import

--- CloseAsyncOperationActor/src/main/java/info/smart_tools/smartactors/actors/close_async_operation/CloseAsyncOperationActor.java
+++ CloseAsyncOperationActor/src/main/java/info/smart_tools/smartactors/actors/close_async_operation/CloseAsyncOperationActor.java
@@ -8,7 +8,6 @@ import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionExcep
 import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
-import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 
 /**
