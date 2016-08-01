1d168508c759736840c9f1b5fc0b709d0ef87ea1
Andrey Kutalev <akutalev@gmail.com>
2016-08-01 18:21:14 +0600

    Added ChainChoiceStrategy plugin. Added ChainCallReceiver plugin. ServerWithConfiguration has been updated.

---
+++ PluginChainChoiceStrategy/src/main/java/info/smart_tools/smartactors/plugin/chain_call_receiver_strategy/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package contains plugin for {@link info.smart_tools.smartactors.core.chain_call_receiver.IChainChoiceStrategy}.
+ * Implementation of {@link info.smart_tools.smartactors.core.iplugin.IPlugin}.
+ * Plugin creates and register into IOC {@link info.smart_tools.smartactors.core.chain_call_receiver.IChainChoiceStrategy}
+ * strategy.
+ */
+package info.smart_tools.smartactors.plugin.chain_call_receiver_strategy;
