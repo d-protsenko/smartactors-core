7d173c3ecb4e2c0d4985ade0f02ce2cd964ac27f
Ivan Ivashchenko <ivan.ivashchenko@7bits.it>
2016-08-03 16:44:37 +0600

    return needed code for start server with configuration

--- PluginConfigurationManager/src/main/java/info/smart_tools/smartactors/plugin/configuration_manager/PluginConfigurationManager.java
+++ PluginConfigurationManager/src/main/java/info/smart_tools/smartactors/plugin/configuration_manager/PluginConfigurationManager.java
@@ -61,6 +61,13 @@ public class PluginConfigurationManager implements IPlugin {
             configureItem
                     .after("configuration_manager")
                     .process(() -> {
+                        try {
+                            IConfigurationManager configurationManager = IOC.resolve(
+                                Keys.getOrAdd(IConfigurationManager.class.getCanonicalName()));
+                            configurationManager.configure();
+                        } catch (ResolutionException | InvalidStateException | ConfigurationProcessingException e) {
+                            throw new ActionExecuteException("ConfigurationManager plugin can't load", e);
+                        }
                     });
 
             bootstrap.add(configureItem);
