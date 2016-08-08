bc78b1cd0de2b67cde4b011991df23cfad315dfb
Denis Nelubin <dnelubin@gmail.com>
2016-08-08 14:22:13 +0600

    Updated tests to use plugins.

--- PostgresGetByIdTask/src/test/java/info/smart_tools/smartactors/core/postgres_getbyid_task/PostgresGetByIdTaskTest.java
+++ PostgresGetByIdTask/src/test/java/info/smart_tools/smartactors/core/postgres_getbyid_task/PostgresGetByIdTaskTest.java
@@ -1,11 +1,11 @@
 package info.smart_tools.smartactors.core.postgres_getbyid_task;
 
-import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
+import info.smart_tools.smartactors.core.bootstrap.Bootstrap;
 import info.smart_tools.smartactors.core.db_storage.exceptions.QueryBuildException;
 import info.smart_tools.smartactors.core.db_storage.utils.CollectionName;
-import info.smart_tools.smartactors.core.ds_object.DSObject;
 import info.smart_tools.smartactors.core.field_name.FieldName;
 import info.smart_tools.smartactors.core.iaction.IAction;
+import info.smart_tools.smartactors.core.ibootstrap.exception.ProcessExecutionException;
 import info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskSetConnectionException;
@@ -17,19 +17,18 @@ import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
-import info.smart_tools.smartactors.core.iscope.IScope;
-import info.smart_tools.smartactors.core.iscope_provider_container.exception.ScopeProviderException;
+import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
 import info.smart_tools.smartactors.core.istorage_connection.IStorageConnection;
 import info.smart_tools.smartactors.core.istorage_connection.exception.StorageException;
 import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import info.smart_tools.smartactors.core.postgres_connection.JDBCCompiledQuery;
 import info.smart_tools.smartactors.core.postgres_connection.QueryStatement;
-import info.smart_tools.smartactors.core.resolve_by_name_ioc_with_lambda_strategy.ResolveByNameIocStrategy;
-import info.smart_tools.smartactors.core.scope_provider.ScopeProvider;
 import info.smart_tools.smartactors.core.singleton_strategy.SingletonStrategy;
-import info.smart_tools.smartactors.core.strategy_container.StrategyContainer;
-import info.smart_tools.smartactors.core.string_ioc_key.Key;
+import info.smart_tools.smartactors.plugin.dsobject.PluginDSObject;
+import info.smart_tools.smartactors.plugin.ifieldname.IFieldNamePlugin;
+import info.smart_tools.smartactors.plugin.ioc_keys.PluginIOCKeys;
+import info.smart_tools.smartactors.plugin.ioc_simple_container.PluginIOCSimpleContainer;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -57,56 +56,13 @@ public class PostgresGetByIdTaskTest {
     private ResultSet resultSet;
 
     @BeforeClass
-    public static void prepareIOC() throws ScopeProviderException, InvalidArgumentException, RegistrationException, ResolutionException {
-        ScopeProvider.subscribeOnCreationNewScope(
-                scope -> {
-                    try {
-                        scope.setValue(IOC.getIocKey(), new StrategyContainer());
-                    } catch (Exception e) {
-                        throw new Error(e);
-                    }
-                }
-        );
-
-        Object keyOfMainScope = ScopeProvider.createScope(null);
-        IScope mainScope = ScopeProvider.getScope(keyOfMainScope);
-        ScopeProvider.setCurrentScope(mainScope);
-
-        IOC.register(
-                IOC.getKeyForKeyStorage(),
-                new ResolveByNameIocStrategy(
-                        (args) -> {
-                            try {
-                                return new Key((String) args[0]);
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                        })
-        );
-        IOC.register(
-                Keys.getOrAdd(IFieldName.class.getCanonicalName()),
-                new CreateNewInstanceStrategy(
-                        (args) -> {
-                            try {
-                                return new FieldName(String.valueOf(args[0]));
-                            } catch (InvalidArgumentException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                )
-        );
-        IOC.register(
-                Keys.getOrAdd(IObject.class.getCanonicalName()),
-                new CreateNewInstanceStrategy(
-                        (args) -> {
-                            try {
-                                return new DSObject(String.valueOf(args[0]));
-                            } catch (InvalidArgumentException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                )
-        );
+    public static void prepareIOC() throws PluginException, ProcessExecutionException {
+        Bootstrap bootstrap = new Bootstrap();
+        new PluginIOCSimpleContainer(bootstrap).load();
+        new PluginIOCKeys(bootstrap).load();
+        new IFieldNamePlugin(bootstrap).load();
+        new PluginDSObject(bootstrap).load();
+        bootstrap.start();
     }
 
     @Before
