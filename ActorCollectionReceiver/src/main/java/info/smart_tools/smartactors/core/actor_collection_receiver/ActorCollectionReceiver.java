6573032727b9d149646360b30eaed5fe24669662
Andrey Kutalev <akutalev@gmail.com>
2016-08-09 18:49:18 +0600

    Intermediate commit. Added ActorCollectionReceiver plugin.

--- ActorCollectionReceiver/src/main/java/info/smart_tools/smartactors/core/actor_collection_receiver/ActorCollectionReceiver.java
+++ ActorCollectionReceiver/src/main/java/info/smart_tools/smartactors/core/actor_collection_receiver/ActorCollectionReceiver.java
@@ -6,6 +6,10 @@ import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgum
 import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
+import info.smart_tools.smartactors.core.iroutable_object_creator.IRoutedObjectCreator;
+import info.smart_tools.smartactors.core.iroutable_object_creator.exceptions.ObjectCreationException;
+import info.smart_tools.smartactors.core.irouter.IRouter;
+import info.smart_tools.smartactors.core.irouter.exceptions.RouteNotFoundException;
 import info.smart_tools.smartactors.core.message_processing.IMessageProcessor;
 import info.smart_tools.smartactors.core.message_processing.IMessageReceiver;
 import info.smart_tools.smartactors.core.message_processing.exceptions.AsynchronousOperationException;
@@ -20,22 +24,20 @@ import java.util.Map;
  */
 public class ActorCollectionReceiver implements IMessageReceiver {
 
-    private Map<Object, IMessageReceiver> storage = new HashMap<>();
-    private IObject keyResolutionRule;
-    private IFieldName collectionItemKey;
-    private IRouter router;
+    private IFieldName keyFieldName;
+    private IFieldName wrapperFieldName;
+    private IRouter router = new ActorCollectionRouter();
 
     public ActorCollectionReceiver(final IObject configSection)
             throws InvalidArgumentException {
         try {
-            IFieldName wrapperFieldName = IOC.resolve(
+            this.wrapperFieldName = IOC.resolve(
                     IOC.resolve(IOC.getKeyForKeyStorage(), IFieldName.class.getCanonicalName()), "wrapper"
             );
-            this.collectionItemKey  = IOC.resolve(
-                    IOC.resolve(IOC.getKeyForKeyStorage(), IFieldName.class.getCanonicalName()), "getCollectionItemKey"
+            this.keyFieldName  = IOC.resolve(
+                    IOC.resolve(IOC.getKeyForKeyStorage(), IFieldName.class.getCanonicalName()), "key"
             );
-            this.keyResolutionRule = new WDSObject((IObject) configSection.getValue(wrapperFieldName));
-        } catch (ResolutionException | ReadValueException e) {
+        } catch (ResolutionException e) {
             throw new InvalidArgumentException("Could not create instance of ActorCollectionReceiver", e);
         }
     }
@@ -44,10 +46,16 @@ public class ActorCollectionReceiver implements IMessageReceiver {
     public void receive(final IMessageProcessor processor)
             throws MessageReceiveException, AsynchronousOperationException {
         try {
-            Object key = this.keyResolutionRule.getValue(this.collectionItemKey);
-            IMessageReceiver receiver = storage.get(key);
+            IObject mapSection = processor.getSequence().getCurrentReceiverArguments();
+            String keyName = (String) mapSection.getValue(this.keyFieldName);
+            IFieldName fieldNameForKeyName = IOC.resolve(
+                    IOC.resolve(IOC.getKeyForKeyStorage(), IFieldName.class.getCanonicalName()), keyName
+            );
+            Object key = ((IObject) processor.getEnvironment().getValue(this.wrapperFieldName))
+                    .getValue(fieldNameForKeyName);
+            IMessageReceiver receiver = router.route(key);
             if (null == receiver) {
-                IObject mapSection = processor.getSequence().getCurrentReceiverArguments();
+
                 IFieldName kindField = IOC.resolve(
                         IOC.resolve(IOC.getKeyForKeyStorage(), IFieldName.class.getCanonicalName()), "kind"
                 );
@@ -57,14 +65,29 @@ public class ActorCollectionReceiver implements IMessageReceiver {
                                 IRoutedObjectCreator.class.getCanonicalName() + "#" + String.valueOf(kindField)
                         )
                 );
-                objectCreator.createObject(router, objDesc);
-                IRouter router = IOC.resolve(
-                        IOC.resolve(IOC.getKeyForKeyStorage(), IRouter.class.getCanonicalName())
-                );
+                objectCreator.createObject(this.router, mapSection);
+                // TODO: bad solution, need refactoring. Change interface of IRoutedObjectCreator
+                receiver = this.router.route(key);
             }
             receiver.receive(processor);
-        } catch (InvalidArgumentException | ReadValueException | ResolutionException e) {
-            throw new MessageReceiveException("", e);
+        } catch (Exception e) {
+            throw new MessageReceiveException("Could not execute ActorCollectionReceiver.receive.", e);
         }
+    }
+}
+
+class ActorCollectionRouter implements IRouter {
+
+    private Map<Object, IMessageReceiver> storage = new HashMap<>();
+
+    @Override
+    public IMessageReceiver route(final Object targetId)
+            throws RouteNotFoundException {
+        return this.storage.get(targetId);
+    }
+
+    @Override
+    public void register(final Object targetId, final IMessageReceiver receiver) {
+        this.storage.put(targetId, receiver);
     }
 }
---
+++ PluginActorCollectionReceiver/src/main/java/info/smart_tools/smartactors/plugin/actor_collection_receiver/ActorCollectionReceiverPlugin.java
@@ -0,0 +1,83 @@
+package info.smart_tools.smartactors.plugin.actor_collection_receiver;
+
+import info.smart_tools.smartactors.core.actor_collection_receiver.ActorCollectionReceiver;
+import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
+import info.smart_tools.smartactors.core.iaction.exception.ActionExecuteException;
+import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
+import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
+import info.smart_tools.smartactors.core.iioccontainer.exception.RegistrationException;
+import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.core.iobject.IObject;
+import info.smart_tools.smartactors.core.ioc.IOC;
+import info.smart_tools.smartactors.core.iplugin.IPlugin;
+import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
+import info.smart_tools.smartactors.strategy.apply_function_to_arguments.ApplyFunctionToArgumentsStrategy;
+
+/**
+ * Implementation of {@link IPlugin}.
+ * Plugin registers into IOC strategy for creation new instance of {@link ActorCollectionReceiver}.
+ */
+public class ActorCollectionReceiverPlugin implements IPlugin {
+
+    /** Local storage for instance of {@link IBootstrap}*/
+    private IBootstrap<IBootstrapItem<String>> bootstrap;
+
+    /**
+     * @param bootstrap Target bootstrap for adding strategy
+     * @throws InvalidArgumentException if any errors occurred
+     */
+    public ActorCollectionReceiverPlugin(final IBootstrap<IBootstrapItem<String>> bootstrap)
+            throws InvalidArgumentException {
+        if (null == bootstrap) {
+            throw new InvalidArgumentException("Incoming argument should not be null.");
+        }
+        this.bootstrap = bootstrap;
+    }
+
+    @Override
+    public void load()
+            throws PluginException {
+        try {
+            IBootstrapItem<String> item = new BootstrapItem("ActorCollectionReceiver");
+            item
+                    .after("IOC")
+                    .before("configure")
+                    .after("IFieldPlugin")
+                    .after("IFieldNamePlugin")
+                    .process(
+                            () -> {
+                                try {
+                                    IOC.register(
+                                            IOC.resolve(
+                                                    IOC.getKeyForKeyStorage(),
+                                                    ActorCollectionReceiver.class.getCanonicalName()
+                                            ),
+                                            new ApplyFunctionToArgumentsStrategy(
+                                                    (args) -> {
+                                                        try {
+                                                            return new ActorCollectionReceiver((IObject) args[0]);
+                                                        } catch (Exception e) {
+                                                            throw new RuntimeException(
+                                                                    "Could not create new instance of ActorCollectionReceiver."
+                                                                    , e
+                                                            );
+                                                        }
+                                                    }
+                                            )
+                                    );
+                                } catch (ResolutionException e) {
+                                    throw new ActionExecuteException("ActorCollectionReceiver plugin can't load: can't get ActorCollectionReceiver key", e);
+                                } catch (InvalidArgumentException e) {
+                                    throw new ActionExecuteException("ActorCollectionReceiver plugin can't load: can't create strategy", e);
+                                } catch (RegistrationException e) {
+                                    throw new ActionExecuteException("ActorCollectionReceiver plugin can't load: can't register new strategy", e);
+                                }
+                            }
+                    );
+            this.bootstrap.add(item);
+        } catch (Throwable e) {
+            throw new PluginException("Could not load 'ActorReceiverCreator plugin'", e);
+        }
+    }
+}
