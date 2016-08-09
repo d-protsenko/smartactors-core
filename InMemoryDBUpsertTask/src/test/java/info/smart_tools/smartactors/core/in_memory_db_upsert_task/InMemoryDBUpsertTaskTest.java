de272d981edd37318da7dd0101474045d13987d9
ValchukDmitry <dart.fk712@gmail.com>
2016-08-09 20:15:07 +0600

    add upsert task for InMemoryDatabase

---
+++ InMemoryDBUpsertTask/src/test/java/info/smart_tools/smartactors/core/in_memory_db_upsert_task/InMemoryDBUpsertTaskTest.java
@@ -0,0 +1,97 @@
+package info.smart_tools.smartactors.core.in_memory_db_upsert_task;
+
+import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
+import info.smart_tools.smartactors.core.ds_object.DSObject;
+import info.smart_tools.smartactors.core.field_name.FieldName;
+import info.smart_tools.smartactors.core.idatabase.exception.IDataBaseException;
+import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
+import info.smart_tools.smartactors.core.ifield_name.IFieldName;
+import info.smart_tools.smartactors.core.iioccontainer.exception.RegistrationException;
+import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.core.in_memory_database.IConditionVerifier;
+import info.smart_tools.smartactors.core.in_memory_database.InMemoryDatabase;
+import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.core.iobject.IObject;
+import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
+import info.smart_tools.smartactors.core.ioc.IOC;
+import info.smart_tools.smartactors.core.iscope.IScope;
+import info.smart_tools.smartactors.core.iscope_provider_container.exception.ScopeProviderException;
+import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
+import info.smart_tools.smartactors.core.named_keys_storage.Keys;
+import info.smart_tools.smartactors.core.resolve_by_name_ioc_strategy.ResolveByNameIocStrategy;
+import info.smart_tools.smartactors.core.scope_provider.ScopeProvider;
+import info.smart_tools.smartactors.core.singleton_strategy.SingletonStrategy;
+import info.smart_tools.smartactors.core.strategy_container.StrategyContainer;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class InMemoryDBUpsertTaskTest {
+
+    InMemoryDatabase inMemoryDatabase;
+
+    @Before
+    public void setUp() throws ScopeProviderException, ResolutionException, RegistrationException, InvalidArgumentException {
+        inMemoryDatabase = mock(InMemoryDatabase.class);
+        ScopeProvider.subscribeOnCreationNewScope(
+                scope -> {
+                    try {
+                        scope.setValue(IOC.getIocKey(), new StrategyContainer());
+                    } catch (Exception e) {
+                        throw new Error(e);
+                    }
+                }
+        );
+
+        Object keyOfMainScope = ScopeProvider.createScope(null);
+        IScope mainScope = ScopeProvider.getScope(keyOfMainScope);
+        ScopeProvider.setCurrentScope(mainScope);
+        Map<String, IConditionVerifier> verifierMap = new HashMap<>();
+        IOC.register(
+                IOC.getKeyForKeyStorage(),
+                new ResolveByNameIocStrategy()
+        );
+        IOC.register(Keys.getOrAdd(IFieldName.class.getCanonicalName()),
+                new CreateNewInstanceStrategy(
+                        (args) -> {
+                            try {
+                                return new FieldName((String) args[0]);
+                            } catch (InvalidArgumentException ignored) {
+                            }
+                            return null;
+                        }
+                )
+        );
+
+        IOC.register(Keys.getOrAdd(InMemoryDatabase.class.getCanonicalName()), new SingletonStrategy(
+                        inMemoryDatabase
+                )
+        );
+    }
+
+    @Test
+    public void testPrepare() throws InvalidArgumentException, TaskPrepareException, TaskExecutionException, ChangeValueException, IDataBaseException {
+        InMemoryDBUpsertTask upsertTask = new InMemoryDBUpsertTask();
+        IObject query = new DSObject("{\"collectionName\": \"collection_name\"}");
+        IObject document = new DSObject("{\"hello\": \"world\"}");
+        IFieldName documentFieldName = new FieldName("document");
+        query.setValue(documentFieldName, document);
+        upsertTask.prepare(query);
+    }
+
+    public void testExecute() throws InvalidArgumentException, ChangeValueException, TaskPrepareException, TaskExecutionException, IDataBaseException {
+        InMemoryDBUpsertTask upsertTask = new InMemoryDBUpsertTask();
+        IObject query = new DSObject("{\"collectionName\": \"collection_name\"}");
+        IObject document = new DSObject("{\"hello\": \"world\"}");
+        IFieldName documentFieldName = new FieldName("document");
+        query.setValue(documentFieldName, document);
+        upsertTask.prepare(query);
+        upsertTask.execute();
+        verify(inMemoryDatabase).upsert(document, "collection_name");
+    }
+}
