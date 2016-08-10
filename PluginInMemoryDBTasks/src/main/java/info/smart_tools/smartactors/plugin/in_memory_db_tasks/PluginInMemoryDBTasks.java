f2dec380a99f2c6c0584722908b859a464484fd0
ValchukDmitry <dart.fk712@gmail.com>
2016-08-10 14:28:13 +0600

    add plugin for InMemoryDB tasks

---
+++ PluginInMemoryDBTasks/src/main/java/info/smart_tools/smartactors/plugin/in_memory_db_tasks/PluginInMemoryDBTasks.java
@@ -0,0 +1,168 @@
+package info.smart_tools.smartactors.plugin.in_memory_db_tasks;
+
+import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
+import info.smart_tools.smartactors.core.db_storage.utils.CollectionName;
+import info.smart_tools.smartactors.core.iaction.IAction;
+import info.smart_tools.smartactors.core.iaction.exception.ActionExecuteException;
+import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
+import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
+import info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask;
+import info.smart_tools.smartactors.core.ifield.IField;
+import info.smart_tools.smartactors.core.iioccontainer.exception.RegistrationException;
+import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.core.in_memory_database.InMemoryDatabase;
+import info.smart_tools.smartactors.core.in_memory_db_get_by_id_task.InMemoryGetByIdTask;
+import info.smart_tools.smartactors.core.in_memory_db_select_task.InMemoryDBSelectTask;
+import info.smart_tools.smartactors.core.in_memory_db_upsert_task.InMemoryDBUpsertTask;
+import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.core.iobject.IObject;
+import info.smart_tools.smartactors.core.ioc.IOC;
+import info.smart_tools.smartactors.core.iplugin.IPlugin;
+import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
+import info.smart_tools.smartactors.core.istorage_connection.IStorageConnection;
+import info.smart_tools.smartactors.core.named_keys_storage.Keys;
+import info.smart_tools.smartactors.strategy.apply_function_to_arguments.ApplyFunctionToArgumentsStrategy;
+
+/**
+ * Plugin for register tasks for {@link InMemoryDatabase}
+ */
+public class PluginInMemoryDBTasks implements IPlugin {
+
+    private final IBootstrap<IBootstrapItem<String>> bootstrap;
+
+    /**
+     * Constructor
+     *
+     * @param bootstrap bootstrap element
+     */
+    public PluginInMemoryDBTasks(final IBootstrap<IBootstrapItem<String>> bootstrap) {
+        this.bootstrap = bootstrap;
+    }
+
+    @Override
+    public void load() throws PluginException {
+
+        try {
+            BootstrapItem item = new BootstrapItem("PostgresDBTasksPlugin");
+            item
+                    .after("IOC")
+                    .after("IFieldPlugin")
+                    .after("InMemoryDatabase")
+                    .process(() -> {
+                        try {
+                            registerUpsertTask();
+                            registerGetByIdTask();
+                            registerSearchTask();
+                        } catch (ResolutionException e) {
+                            throw new ActionExecuteException("Can't resolve fields for db task.", e);
+                        } catch (InvalidArgumentException e) {
+                            throw new ActionExecuteException("Can't create strategy for db task.", e);
+                        } catch (RegistrationException e) {
+                            throw new ActionExecuteException("Can't register strategy for db task.", e);
+                        }
+                    });
+            bootstrap.add(item);
+        } catch (Exception e) {
+            throw new PluginException("Can't get BootstrapItem.", e);
+        }
+    }
+
+    public void registerUpsertTask() throws ResolutionException, InvalidArgumentException, RegistrationException {
+        IField collectionNameField = IOC.resolve(
+                Keys.getOrAdd(IField.class.getCanonicalName()), "collectionName");
+        IField documentField = IOC.resolve(
+                Keys.getOrAdd(IField.class.getCanonicalName()), "document");
+        IOC.register(
+                Keys.getOrAdd("db.collection.upsert"),
+                //TODO:: use smth like ResolveByNameStrategy, but this caching strategy should call prepare always
+                new ApplyFunctionToArgumentsStrategy(
+                        (args) -> {
+                            try {
+                                String collectionName = String.valueOf(args[1]);
+                                IObject document = (IObject) args[2];
+                                IDatabaseTask task = new InMemoryDBUpsertTask();
+
+                                IObject query = IOC.resolve(Keys.getOrAdd(IObject.class.getCanonicalName()));
+
+                                collectionNameField.out(query, collectionName);
+                                documentField.out(query, document);
+
+                                task.prepare(query);
+                                return task;
+                            } catch (Exception e) {
+                                throw new RuntimeException("Can't resolve upsert db task.", e);
+                            }
+                        }
+                )
+        );
+    }
+
+    public void registerGetByIdTask() throws ResolutionException, InvalidArgumentException, RegistrationException {
+        IField collectionNameField = IOC.resolve(
+                Keys.getOrAdd(IField.class.getCanonicalName()), "collectionName");
+        IField idField = IOC.resolve(
+                Keys.getOrAdd(IField.class.getCanonicalName()), "id");
+        IField callbackField = IOC.resolve(
+                Keys.getOrAdd(IField.class.getCanonicalName()), "callback");
+        IOC.register(
+                Keys.getOrAdd("db.collection.getbyid"),
+                //TODO:: use smth like ResolveByNameStrategy, but this caching strategy should call prepare always
+                new ApplyFunctionToArgumentsStrategy(
+                        (args) -> {
+                            try {
+                                CollectionName collectionName = CollectionName.fromString(String.valueOf(args[1]));
+                                Object id = args[2];
+                                IAction<IObject> callback = (IAction<IObject>) args[3];
+                                IDatabaseTask task = new InMemoryGetByIdTask();
+
+                                IObject query = IOC.resolve(Keys.getOrAdd(IObject.class.getCanonicalName()));
+
+                                collectionNameField.out(query, collectionName);
+                                idField.out(query, id);
+                                callbackField.out(query, callback);
+
+                                task.prepare(query);
+                                return task;
+                            } catch (Exception e) {
+                                throw new RuntimeException("Can't resolve getbyid db task.", e);
+                            }
+                        }
+                )
+        );
+    }
+
+    public void registerSearchTask() throws ResolutionException, InvalidArgumentException, RegistrationException {
+        IField collectionNameField = IOC.resolve(
+                Keys.getOrAdd(IField.class.getCanonicalName()), "collectionName");
+        IField criteriaField = IOC.resolve(
+                Keys.getOrAdd(IField.class.getCanonicalName()), "criteria");
+        IField callbackField = IOC.resolve(
+                Keys.getOrAdd(IField.class.getCanonicalName()), "callback");
+        IOC.register(
+                Keys.getOrAdd("db.collection.search"),
+                //TODO:: use smth like ResolveByNameStrategy, but this caching strategy should call prepare always
+                new ApplyFunctionToArgumentsStrategy(
+                        (args) -> {
+                            try {
+                                CollectionName collectionName = CollectionName.fromString(String.valueOf(args[1]));
+                                IObject criteria = (IObject) args[2];
+                                IAction<IObject[]> callback = (IAction<IObject[]>) args[3];
+                                IDatabaseTask task = new InMemoryDBSelectTask();
+
+                                IObject query = IOC.resolve(Keys.getOrAdd(IObject.class.getCanonicalName()));
+
+                                collectionNameField.out(query, collectionName);
+                                criteriaField.out(query, criteria);
+                                callbackField.out(query, callback);
+
+                                task.prepare(query);
+                                return task;
+                            } catch (Exception e) {
+                                throw new RuntimeException("Can't resolve search db task.", e);
+                            }
+                        }
+                )
+        );
+    }
+
+}
