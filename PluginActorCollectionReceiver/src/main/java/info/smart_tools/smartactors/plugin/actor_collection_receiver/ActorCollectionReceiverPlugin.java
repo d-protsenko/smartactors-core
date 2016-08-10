370855d4a276a903d610c2320cf3cca1f30bf932
Andrey Kutalev <akutalev@gmail.com>
2016-08-10 14:46:00 +0600

    Refactoring.

--- PluginActorCollectionReceiver/src/main/java/info/smart_tools/smartactors/plugin/actor_collection_receiver/ActorCollectionReceiverPlugin.java
+++ PluginActorCollectionReceiver/src/main/java/info/smart_tools/smartactors/plugin/actor_collection_receiver/ActorCollectionReceiverPlugin.java
@@ -9,7 +9,6 @@ import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
 import info.smart_tools.smartactors.core.iioccontainer.exception.RegistrationException;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
 import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
-import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.iplugin.IPlugin;
 import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
