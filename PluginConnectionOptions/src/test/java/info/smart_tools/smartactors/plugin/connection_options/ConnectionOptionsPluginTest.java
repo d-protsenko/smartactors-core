cae4e4f892e50be37220250eaf02e90e15037417
Ivan Ivashchenko <ivan.ivashchenko@7bits.it>
2016-08-10 17:04:43 +0600

    tests for ConnectionOptionsPlugin have been added

--- PluginConnectionOptions/src/test/java/info/smart_tools/smartactors/plugin/connection_options/ConnectionOptionsPluginTest.java
+++ PluginConnectionOptions/src/test/java/info/smart_tools/smartactors/plugin/connection_options/ConnectionOptionsPluginTest.java
@@ -1,7 +1,5 @@
-package info.smart_tools.smartactors.plugin.change_password_actor;
+package info.smart_tools.smartactors.plugin.connection_options;
 
-import info.smart_tools.smartactors.actor.change_password.ChangePasswordActor;
-import info.smart_tools.smartactors.actor.change_password.wrapper.ChangePasswordConfig;
 import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
 import info.smart_tools.smartactors.core.iaction.IPoorAction;
 import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
@@ -11,6 +9,7 @@ import info.smart_tools.smartactors.core.iobject.IObject;
 import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
+import info.smart_tools.smartactors.core.postgres_connection.wrapper.ConnectionOptions;
 import info.smart_tools.smartactors.strategy.apply_function_to_arguments.ApplyFunctionToArgumentsStrategy;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,6 +18,7 @@ import org.mockito.ArgumentCaptor;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
@@ -29,11 +29,11 @@ import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@PrepareForTest({IOC.class, Keys.class, ChangePasswordActorPlugin.class, ApplyFunctionToArgumentsStrategy.class})
+@PrepareForTest({IOC.class, Keys.class, ConnectionOptionsPlugin.class, ApplyFunctionToArgumentsStrategy.class})
 @RunWith(PowerMockRunner.class)
-public class ChangePasswordActorPluginTest {
+public class ConnectionOptionsPluginTest {
 
-    private ChangePasswordActorPlugin plugin;
+    private ConnectionOptionsPlugin plugin;
     private IBootstrap bootstrap;
 
     @Before
@@ -43,7 +43,7 @@ public class ChangePasswordActorPluginTest {
         mockStatic(Keys.class);
 
         bootstrap = mock(IBootstrap.class);
-        plugin = new ChangePasswordActorPlugin(bootstrap);
+        plugin = new ConnectionOptionsPlugin(bootstrap);
     }
 
 
@@ -51,14 +51,14 @@ public class ChangePasswordActorPluginTest {
     public void ShouldCorrectLoadPlugin() throws Exception {
 
         BootstrapItem bootstrapItem = mock(BootstrapItem.class);
-        whenNew(BootstrapItem.class).withArguments("ChangePasswordActorPlugin").thenReturn(bootstrapItem);
+        whenNew(BootstrapItem.class).withArguments("ConnectionOptionsPlugin").thenReturn(bootstrapItem);
 
         when(bootstrapItem.after(anyString())).thenReturn(bootstrapItem);
         when(bootstrapItem.before(anyString())).thenReturn(bootstrapItem);
 
         plugin.load();
 
-        verifyNew(BootstrapItem.class).withArguments("ChangePasswordActorPlugin");
+        verifyNew(BootstrapItem.class).withArguments("ConnectionOptionsPlugin");
 
         ArgumentCaptor<IPoorAction> actionArgumentCaptor = ArgumentCaptor.forClass(IPoorAction.class);
 
@@ -67,40 +67,30 @@ public class ChangePasswordActorPluginTest {
         verify(bootstrapItem).process(actionArgumentCaptor.capture());
         verify(bootstrap).add(bootstrapItem);
 
-        IKey changePasswordActorKey = mock(IKey.class);
-        when(Keys.getOrAdd(ChangePasswordActor.class.getCanonicalName())).thenReturn(changePasswordActorKey);
+        IKey connectionOptionsKey = mock(IKey.class);
+        when(Keys.getOrAdd("PostgresConnectionOptions")).thenReturn(connectionOptionsKey);
 
         actionArgumentCaptor.getValue().execute();
 
         verifyStatic();
-        Keys.getOrAdd(ChangePasswordConfig.class.getCanonicalName());
-
-        verifyStatic();
-        Keys.getOrAdd(ChangePasswordActor.class.getCanonicalName());
+        Keys.getOrAdd("PostgresConnectionOptions");
 
         ArgumentCaptor<ApplyFunctionToArgumentsStrategy> argumentCaptor = ArgumentCaptor.forClass(ApplyFunctionToArgumentsStrategy.class);
 
         verifyStatic();
-        IOC.register(eq(changePasswordActorKey), argumentCaptor.capture());
+        IOC.register(eq(connectionOptionsKey), argumentCaptor.capture());
 
         IObject configObj = mock(IObject.class);
-        IKey configKey = mock(IKey.class);
-        when(Keys.getOrAdd(ChangePasswordConfig.class.getCanonicalName())).thenReturn(configKey);
-        ChangePasswordConfig config = mock(ChangePasswordConfig.class);
-        when(IOC.resolve(configKey, configObj)).thenReturn(config);
 
-        ChangePasswordActor actor = mock(ChangePasswordActor.class);
-        whenNew(ChangePasswordActor.class).withArguments(config).thenReturn(actor);
+        ConnectionOptions options = argumentCaptor.getValue().resolve(configObj);
+        assertNotNull(options);
 
-        argumentCaptor.getValue().resolve(configObj);
-
-        verifyNew(ChangePasswordActor.class);
     }
 
     @Test(expected = PluginException.class)
     public void ShouldThrowException_When_InternalExceptionIsThrown() throws Exception {
 
-        whenNew(BootstrapItem.class).withArguments("ChangePasswordActorPlugin").thenThrow(new InvalidArgumentException(""));
+        whenNew(BootstrapItem.class).withArguments("ConnectionOptionsPlugin").thenThrow(new InvalidArgumentException(""));
         plugin.load();
     }
 }
