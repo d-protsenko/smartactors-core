f1432a104a8ef8a312378af09536b678683ef275
Denis Nelubin <dnelubin@gmail.com>
2016-08-26 19:47:10 +0600

    Fixed PostgresConnnectionPoolPlugin to return the same Pool when the same ConnectionOptions are passed to resolve(), to avoid instantiation of pools and leak of Postgres connections.

--- PluginPostgresConnectionPool/src/main/java/info/smart_tools/smartactors/plugin/postgres_connection_pool/PostgresConnectionPoolPlugin.java
+++ PluginPostgresConnectionPool/src/main/java/info/smart_tools/smartactors/plugin/postgres_connection_pool/PostgresConnectionPoolPlugin.java
@@ -4,7 +4,6 @@ import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
 import info.smart_tools.smartactors.core.iaction.exception.ActionExecuteException;
 import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
 import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
-import info.smart_tools.smartactors.core.iioccontainer.exception.RegistrationException;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
 import info.smart_tools.smartactors.core.ikey.IKey;
 import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
@@ -12,12 +11,14 @@ import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.iplugin.IPlugin;
 import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
+import info.smart_tools.smartactors.core.ipool.IPool;
 import info.smart_tools.smartactors.core.iresolve_dependency_strategy.IResolveDependencyStrategy;
 import info.smart_tools.smartactors.core.istorage_connection.exception.StorageException;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import info.smart_tools.smartactors.core.pool.Pool;
 import info.smart_tools.smartactors.core.postgres_connection.PostgresConnection;
 import info.smart_tools.smartactors.core.postgres_connection.wrapper.ConnectionOptions;
+import info.smart_tools.smartactors.core.singleton_strategy.SingletonStrategy;
 import info.smart_tools.smartactors.strategy.apply_function_to_arguments.ApplyFunctionToArgumentsStrategy;
 
 /**
@@ -46,45 +47,50 @@ public class PostgresConnectionPoolPlugin implements IPlugin {
     @Override
     public void load() throws PluginException {
         try {
-            IBootstrapItem<String> item = new BootstrapItem("PostgresConnectionPoolPlugin");
+            BootstrapItem item = new BootstrapItem("PostgresConnectionPoolPlugin");
             item
                 .after("IOC")
+                .after("ioc_keys")
                 .before("CreateCachedCollectionPlugin")
                 .process(() -> {
                     try {
                         IResolveDependencyStrategy poolStrategy = new ApplyFunctionToArgumentsStrategy(
                                 (args) -> {
                                     ConnectionOptions connectionOptions = (ConnectionOptions) args[0];
-                                    if (connectionOptions == null) {
-                                        throw new RuntimeException("Can't resolve connection pool: connectionOptions is null");
+
+                                    final IKey poolKey;
+                                    try {
+                                        poolKey = getPoolKey(connectionOptions);
+                                    } catch (Exception e) {
+                                        throw new RuntimeException("Can't build the pool key: connectionOptions = " + connectionOptions, e);
                                     }
 
                                     try {
-                                        return new Pool(connectionOptions.getMaxConnections(), () -> {
-                                            try {
-                                                return new PostgresConnection(connectionOptions);
-                                            } catch (StorageException e) {
-                                                throw new RuntimeException(e);
-                                            }
-                                        });
-                                    } catch (ReadValueException e) {
-                                        throw new RuntimeException("Can't create PostgresConnectionPool", e);
+                                        return IOC.resolve(poolKey);
+                                    } catch (ResolutionException re) {      // pool not found
+                                        try {
+                                            IPool pool = new Pool(connectionOptions.getMaxConnections(), () -> {
+                                                try {
+                                                    return new PostgresConnection(connectionOptions);
+                                                } catch (StorageException se) {
+                                                    throw new RuntimeException(
+                                                            "Cannot create PostgresConnection: poolKey = " + poolKey, se);
+                                                }
+                                            });
+                                            IOC.register(poolKey, new SingletonStrategy(pool));
+                                            return pool;
+                                        } catch (Exception e) {
+                                            throw new RuntimeException("Can't create PostgresConnectionPool: poolKey = " + poolKey, e);
+                                        }
                                     }
-
                                 });
                         IKey postgresConnectionPoolKey = Keys.getOrAdd("PostgresConnectionPool");
                         IKey databaseConnectionPoolKey = Keys.getOrAdd("DatabaseConnectionPool");
                         IOC.register(postgresConnectionPoolKey, poolStrategy);
                         IOC.register(databaseConnectionPoolKey, poolStrategy);
-                    } catch (ResolutionException e) {
+                    } catch (Exception e) {
                         throw new ActionExecuteException(
-                                "PostgresConnectionPool plugin can't load: can't get PostgresConnectionPool key", e);
-                    } catch (InvalidArgumentException e) {
-                        throw new ActionExecuteException(
-                                "PostgresConnectionPool plugin can't load: can't create strategy", e);
-                    } catch (RegistrationException e) {
-                        throw new ActionExecuteException(
-                                "PostgresConnectionPool plugin can't load: can't register new strategy", e);
+                                "PostgresConnectionPool plugin can't load", e);
                     }
             });
             bootstrap.add(item);
@@ -92,4 +98,17 @@ public class PostgresConnectionPoolPlugin implements IPlugin {
             throw new PluginException("Can't load postgres connection pool plugin", e);
         }
     }
+
+    /**
+     * Calculates the pool key from the connection options
+     * to resolve the same pool when it's requested.
+     * Note, the password is not used to construct the key.
+     * @param options connection options to construct the key from
+     * @return the IOC key to store the pool in
+     */
+    private IKey getPoolKey(final ConnectionOptions options) throws ReadValueException, ResolutionException {
+        return Keys.getOrAdd(String.format("postgres_connection_%s_%s_%d",
+                options.getUrl(), options.getUsername(), options.getMaxConnections()));
+    }
+
 }
