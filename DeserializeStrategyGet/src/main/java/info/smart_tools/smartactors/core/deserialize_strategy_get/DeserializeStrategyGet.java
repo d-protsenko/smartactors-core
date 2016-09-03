1c64a731ffd379897e7b9bc00ce5e24b14d46aef
ValchukDmitry <dart.fk712@gmail.com>
2016-09-03 17:26:18 +0600

    rewrite deserialize strategy for get to parse tree

--- DeserializeStrategyGet/src/main/java/info/smart_tools/smartactors/core/deserialize_strategy_get/DeserializeStrategyGet.java
+++ DeserializeStrategyGet/src/main/java/info/smart_tools/smartactors/core/deserialize_strategy_get/DeserializeStrategyGet.java
@@ -1,6 +1,7 @@
 package info.smart_tools.smartactors.core.deserialize_strategy_get;
 
 import info.smart_tools.smartactors.core.IDeserializeStrategy;
+import info.smart_tools.smartactors.core.deserialize_strategy_get.parse_tree.IParseTree;
 import info.smart_tools.smartactors.core.exceptions.DeserializationException;
 import info.smart_tools.smartactors.core.ifield_name.IFieldName;
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
@@ -12,67 +13,54 @@ import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.QueryStringDecoder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sevenbits on 31.08.16.
  */
 public class DeserializeStrategyGet implements IDeserializeStrategy {
+    IParseTree tree;
 
-    IFieldName messageMapIdFieldName = IOC.resolve(Keys.getOrAdd(IFieldName.class.getCanonicalName()), "messageMapId");
-    IFieldName argsFieldName = IOC.resolve(Keys.getOrAdd(IFieldName.class.getCanonicalName()), "args");
-    List<List<String>> templates = new ArrayList<>();
-
-    public DeserializeStrategyGet(List<String> templates) throws ResolutionException {
+    public DeserializeStrategyGet(final List<String> templates) throws ResolutionException {
+        this.tree = IOC.resolve(Keys.getOrAdd(IParseTree.class.getCanonicalName()));
         for (String template : templates) {
-            this.templates.add(Arrays.asList(template.split("/")));
+            tree.addTemplate(template);
         }
     }
 
     @Override
-    public IObject deserialize(FullHttpRequest request) throws DeserializationException {
+    public IObject deserialize(final FullHttpRequest request) throws DeserializationException {
         try {
-            QueryStringDecoder decoder = new QueryStringDecoder(request.uri());
-            IObject resultIObject = null;//IOC.resolve(Keys.getOrAdd(DSObject.));
-            //add to resultIObject
-            decoder.parameters();
-            String[] uriElems = decoder.path().split("/");
-            int score[] = new int[templates.size()];
-            for (int i = 0; i < templates.size(); i++) {
-                List<String> template = templates.get(i);
-                for (String templateElem : template) {
-                    if (templateElem.startsWith(":")) {
-                        score[i] += 1;
-                        continue;
+            if (!request.uri().contains("/")) {
+                return IOC.resolve(Keys.getOrAdd("EmptyIObject"));
+            }
+            QueryStringDecoder decoder = IOC.resolve(Keys.getOrAdd(QueryStringDecoder.class.getCanonicalName()),
+                    request.uri().substring(request.uri().indexOf("/")));
+            IObject resultIObject = IOC.resolve(Keys.getOrAdd("EmptyIObject"));
+            decoder.parameters().forEach(
+                    (k, v) -> {
+                        try {
+                            IFieldName fieldName = IOC.resolve(Keys.getOrAdd(IFieldName.class.getCanonicalName()), k);
+                            resultIObject.setValue(fieldName, v);
+                        } catch (ResolutionException | ChangeValueException | InvalidArgumentException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
-                    if (templateElem.equals(uriElems[i])) {
-                        score[i] += 2;
+            );
+            Map<String, String> parameters = tree.match(decoder.uri());
+            parameters.forEach(
+                    (k, v) -> {
+                        try {
+                            IFieldName fieldName = IOC.resolve(Keys.getOrAdd(IFieldName.class.getCanonicalName()), k);
+                            resultIObject.setValue(fieldName, v);
+                        } catch (ResolutionException | ChangeValueException | InvalidArgumentException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
-                }
-            }
-            String uri = request.uri();
-            String[] parts = uri.split("\\?");
-
-            List<String> restApi = Arrays.asList(parts[0].split("/"));
-            if (parts[0].contains("/")) {
-                String messageMapId = restApi.get(1);
-                resultIObject.setValue(messageMapIdFieldName, messageMapId);
-            }
-            if (parts.length > 1) {
-                String args[] = parts[1].split("&");
-                for (String arg : args) {
-                    String keyValue[] = arg.split("=");
-                    IFieldName fieldName = IOC.resolve(Keys.getOrAdd(IFieldName.class.getCanonicalName()), keyValue[0]);
-                    resultIObject.setValue(fieldName, keyValue[1]);
-                }
-            }
-            resultIObject.setValue(argsFieldName, Arrays.copyOfRange(restApi.toArray(), 2, restApi.size()));
+            );
             return resultIObject;
-        } catch (ResolutionException | ChangeValueException | InvalidArgumentException e) {
+        } catch (ResolutionException e) {
             throw new DeserializationException(e);
         }
     }
--- DeserializeStrategyGet/src/test/java/info/smart_tools/smartactors/core/deserialize_strategy_get/DeserializeStrategyGetTest.java
+++ DeserializeStrategyGet/src/test/java/info/smart_tools/smartactors/core/deserialize_strategy_get/DeserializeStrategyGetTest.java
@@ -1,6 +1,7 @@
 package info.smart_tools.smartactors.core.deserialize_strategy_get;
 
 import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
+import info.smart_tools.smartactors.core.deserialize_strategy_get.parse_tree.IParseTree;
 import info.smart_tools.smartactors.core.ds_object.DSObject;
 import info.smart_tools.smartactors.core.exceptions.DeserializationException;
 import info.smart_tools.smartactors.core.field_name.FieldName;
@@ -20,8 +21,15 @@ import info.smart_tools.smartactors.core.scope_provider.ScopeProvider;
 import info.smart_tools.smartactors.core.singleton_strategy.SingletonStrategy;
 import info.smart_tools.smartactors.core.strategy_container.StrategyContainer;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.QueryStringDecoder;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -33,9 +41,12 @@ public class DeserializeStrategyGetTest {
 
     FullHttpRequest httpRequest;
     IObject emptyIObject;
+    IParseTree parseTree;
+    QueryStringDecoder decoder;
 
     @Before
     public void setUp() throws ScopeProviderException, RegistrationException, ResolutionException, InvalidArgumentException {
+
         emptyIObject = mock(IObject.class);
         ScopeProvider.subscribeOnCreationNewScope(
                 scope -> {
@@ -67,77 +78,83 @@ public class DeserializeStrategyGetTest {
                 }
         ));
 
+        decoder = mock(QueryStringDecoder.class);
+        IOC.register(Keys.getOrAdd(QueryStringDecoder.class.getCanonicalName()), new SingletonStrategy(
+                        decoder
+                )
+        );
+
+        parseTree = mock(IParseTree.class);
+        IOC.register(Keys.getOrAdd(IParseTree.class.getCanonicalName()), new SingletonStrategy(
+                        parseTree
+                )
+        );
+        IOC.register(Keys.getOrAdd("EmptyIObject"), new CreateNewInstanceStrategy(
+                        args -> new DSObject()
+                )
+        );
 
     }
 
-  /*  @Test
+    @Test
     public void testUriWithEmptyArgs() throws DeserializationException, ResolutionException, InvalidArgumentException, RegistrationException, ChangeValueException {
         IOC.register(Keys.getOrAdd("EmptyIObject"), new SingletonStrategy(
                         emptyIObject
                 )
         );
+
         IFieldName fieldName = new FieldName("messageMapId");
-        IOC.register(Keys.getOrAdd(IFieldName.class.getCanonicalName()), new SingletonStrategy(
-                        fieldName
-                )
-        );
+
+        String arg = "/hello";
+        when(decoder.uri()).thenReturn(arg);
+        when(decoder.parameters()).thenReturn(new HashMap<>());
+
+        Map<String, String> outputMap = new HashMap<>();
+        outputMap.put("messageMapId", "hello");
+        when(parseTree.match(arg)).thenReturn(outputMap);
+
         String testUri = "www.www.ru/hello";
         httpRequest = mock(FullHttpRequest.class);
         when(httpRequest.uri()).thenReturn(testUri);
-        DeserializeStrategyGet deserializeStrategyGet = new DeserializeStrategyGet();
+        DeserializeStrategyGet deserializeStrategyGet = new DeserializeStrategyGet(new ArrayList<>());
         IObject iObject = deserializeStrategyGet.deserialize(httpRequest);
-        verify(emptyIObject).setValue(fieldName, "hello");
         verify(emptyIObject).setValue(fieldName, "hello");
     }
 
     @Test
     public void testUriWithArgs() throws DeserializationException, InvalidArgumentException, ReadValueException, ResolutionException, RegistrationException {
-        IOC.register(Keys.getOrAdd("EmptyIObject"), new CreateNewInstanceStrategy(
-                        args -> new DSObject()
 
-                )
-        );
         String testUri = "www.www.ru/hello?hello=world";
         httpRequest = mock(FullHttpRequest.class);
         when(httpRequest.uri()).thenReturn(testUri);
-        DeserializeStrategyGet deserializeStrategyGet = new DeserializeStrategyGet();
+
+        String arg = "/hello";
+        when(decoder.uri()).thenReturn(arg);
+        Map<String, List<String>> parameters = new HashMap<>();
+        parameters.put("hello", Collections.singletonList("world"));
+        when(decoder.parameters()).thenReturn(parameters);
+
+        Map<String, String> outputMap = new HashMap<>();
+        outputMap.put("messageMapId", "hello");
+        when(parseTree.match(arg)).thenReturn(outputMap);
+
+        DeserializeStrategyGet deserializeStrategyGet = new DeserializeStrategyGet(new ArrayList<>());
         IObject iObject = deserializeStrategyGet.deserialize(httpRequest);
-        assertEquals(iObject.getValue(new FieldName("hello")), "world");
+        assertEquals(((List<String>) iObject.getValue(new FieldName("hello"))).get(0), "world");
         assertEquals(iObject.getValue(new FieldName("messageMapId")), "hello");
     }
 
     @Test
-    public void testArgsAtUrl() throws DeserializationException, InvalidArgumentException, ReadValueException, ResolutionException, RegistrationException {
-        IOC.register(Keys.getOrAdd("EmptyIObject"), new CreateNewInstanceStrategy(
-                        args -> new DSObject()
+    public void testUriWithoutArgs() throws DeserializationException, InvalidArgumentException, ReadValueException, ResolutionException, RegistrationException {
+        IOC.register(Keys.getOrAdd("EmptyIObject"), new SingletonStrategy(
+                        emptyIObject
                 )
         );
-        String testUri = "www.www.ru/hello/world";
+        String testUri = "www.www.ru";
         httpRequest = mock(FullHttpRequest.class);
         when(httpRequest.uri()).thenReturn(testUri);
-        DeserializeStrategyGet deserializeStrategyGet = new DeserializeStrategyGet();
+        DeserializeStrategyGet deserializeStrategyGet = new DeserializeStrategyGet(new ArrayList<>());
         IObject iObject = deserializeStrategyGet.deserialize(httpRequest);
-        String[] args = (String[]) iObject.getValue(new FieldName("args"));
-        assertEquals(args.length, 1);
-        assertEquals(args[0], "world");
+        assertEquals(iObject, this.emptyIObject);
     }
-
-    @Test
-    public void testMoreArgsAtUrl() throws DeserializationException, InvalidArgumentException, ReadValueException, ResolutionException, RegistrationException {
-        IOC.register(Keys.getOrAdd("EmptyIObject"), new CreateNewInstanceStrategy(
-                        args -> new DSObject()
-                )
-        );
-        String testUri = "www.www.ru/hello/world/foo/bar";
-        httpRequest = mock(FullHttpRequest.class);
-        when(httpRequest.uri()).thenReturn(testUri);
-        DeserializeStrategyGet deserializeStrategyGet = new DeserializeStrategyGet();
-        IObject iObject = deserializeStrategyGet.deserialize(httpRequest);
-        String[] args = (String[]) iObject.getValue(new FieldName("args"));
-        assertEquals(args.length, 3);
-        assertEquals(args[0], "world");
-        assertEquals(args[1], "foo");
-        assertEquals(args[2], "bar");
-    }
-*/
 }
