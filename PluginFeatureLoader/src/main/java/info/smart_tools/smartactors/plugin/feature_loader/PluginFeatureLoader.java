fdedbc7e3e50f12c8329e79d505554baf910468d
AlexeyBond <alexey.bond.94.55@gmail.com>
2016-09-19 18:34:36 +0600

    Use BootstrapPlugin for PluginFeatureLoader.

--- PluginFeatureLoader/src/main/java/info/smart_tools/smartactors/plugin/feature_loader/PluginFeatureLoader.java
+++ PluginFeatureLoader/src/main/java/info/smart_tools/smartactors/plugin/feature_loader/PluginFeatureLoader.java
@@ -1,12 +1,10 @@
 package info.smart_tools.smartactors.plugin.feature_loader;
 
-import info.smart_tools.smartactors.core.bootstrap_item.BootstrapItem;
 import info.smart_tools.smartactors.core.create_new_instance_strategy.CreateNewInstanceStrategy;
 import info.smart_tools.smartactors.core.feature_loader.FeatureLoader;
 import info.smart_tools.smartactors.core.feature_loader.FeatureStatusImpl;
 import info.smart_tools.smartactors.core.iaction.IAction;
 import info.smart_tools.smartactors.core.iaction.IBiAction;
-import info.smart_tools.smartactors.core.iaction.exception.ActionExecuteException;
 import info.smart_tools.smartactors.core.ibootstrap.IBootstrap;
 import info.smart_tools.smartactors.core.ibootstrap_item.IBootstrapItem;
 import info.smart_tools.smartactors.core.ifeature_loader.GlobalFeatureLoader;
@@ -15,74 +13,56 @@ import info.smart_tools.smartactors.core.iioccontainer.exception.RegistrationExc
 import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
 import info.smart_tools.smartactors.core.invalid_argument_exception.InvalidArgumentException;
 import info.smart_tools.smartactors.core.ioc.IOC;
-import info.smart_tools.smartactors.core.iplugin.IPlugin;
-import info.smart_tools.smartactors.core.iplugin.exception.PluginException;
 import info.smart_tools.smartactors.core.iplugin_loader_visitor.IPluginLoaderVisitor;
 import info.smart_tools.smartactors.core.named_keys_storage.Keys;
 import info.smart_tools.smartactors.core.plugin_creator.PluginCreator;
 import info.smart_tools.smartactors.core.plugin_loader_from_jar.PluginLoader;
 import info.smart_tools.smartactors.core.plugin_loader_visitor_empty_implementation.PluginLoaderVisitor;
 import info.smart_tools.smartactors.core.singleton_strategy.SingletonStrategy;
+import info.smart_tools.smartactors.plugin.base.bootstrap_plugin.BootstrapPlugin;
 
 /**
  *
  */
-public class PluginFeatureLoader implements IPlugin {
-    private IBootstrap<IBootstrapItem<String>> bootstrap;
-
+public class PluginFeatureLoader extends BootstrapPlugin {
     /**
-     * The constructor
-     *
+     * The constructor.
      * @param bootstrap    the bootstrap
      */
     public PluginFeatureLoader(final IBootstrap<IBootstrapItem<String>> bootstrap) {
-        this.bootstrap = bootstrap;
+        super(bootstrap);
     }
 
-    @Override
-    public void load() throws PluginException {
-        try {
-            IBootstrapItem<String> loaderItem = new BootstrapItem("feature_loader");
+    /**
+     * Item registering the feature loader and some related components.
+     * @throws ResolutionException if cannot resolve the keys
+     * @throws RegistrationException if cannot register a strategy
+     * @throws InvalidArgumentException if some of called methods throws
+     */
+    @Item("feature_loader")
+    @After({"IOC", "configuration_manager", "config_sections:done", "IFieldNamePlugin", "filesystem_facade", "ConfigurationObject"})
+    public void registerFeatureLoader()
+            throws ResolutionException, RegistrationException, InvalidArgumentException {
+        IOC.register(Keys.getOrAdd(FeatureStatusImpl.class.getCanonicalName()), new CreateNewInstanceStrategy(args -> {
+            try {
+                return new FeatureStatusImpl((String) args[0], (IBiAction) args[1]);
+            } catch (InvalidArgumentException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+        IOC.register(Keys.getOrAdd("plugin creator"), new SingletonStrategy(new PluginCreator()));
+        IOC.register(Keys.getOrAdd("plugin loader visitor"), new SingletonStrategy(new PluginLoaderVisitor<String>()));
+        IOC.register(Keys.getOrAdd("plugin loader"), new CreateNewInstanceStrategy(args -> {
+            try {
+                return new PluginLoader(
+                        (ClassLoader) args[0], (IAction<Class>) args[1], (IPluginLoaderVisitor) args[2]);
+            } catch (InvalidArgumentException e) {
+                throw new RuntimeException(e);
+            }
+        }));
 
-            loaderItem
-                    .after("IOC")
-                    .after("configuration_manager")
-                    .after("config_sections:done")
-                    .after("IFieldNamePlugin")
-                    .after("filesystem_facade")
-                    .after("ConfigurationObject")
-                    .process(() -> {
-                        try {
-                            IOC.register(Keys.getOrAdd(FeatureStatusImpl.class.getCanonicalName()), new CreateNewInstanceStrategy(args -> {
-                                try {
-                                    return new FeatureStatusImpl((String) args[0], (IBiAction) args[1]);
-                                } catch (InvalidArgumentException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }));
-                            IOC.register(Keys.getOrAdd("plugin creator"), new SingletonStrategy(new PluginCreator()));
-                            IOC.register(Keys.getOrAdd("plugin loader visitor"), new SingletonStrategy(new PluginLoaderVisitor<String>()));
-                            IOC.register(Keys.getOrAdd("plugin loader"), new CreateNewInstanceStrategy(args -> {
-                                try {
-                                    return new PluginLoader(
-                                            (ClassLoader) args[0], (IAction<Class>) args[1], (IPluginLoaderVisitor) args[2]);
-                                } catch (InvalidArgumentException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }));
-
-                            IFeatureLoader featureLoader = new FeatureLoader();
-                            GlobalFeatureLoader.set(featureLoader);
-                            IOC.register(Keys.getOrAdd("feature loader"), new SingletonStrategy(featureLoader));
-                        } catch (ResolutionException | RegistrationException | InvalidArgumentException e) {
-                            throw new ActionExecuteException(e);
-                        }
-                    });
-
-            bootstrap.add(loaderItem);
-
-        } catch (InvalidArgumentException e) {
-            throw new PluginException(e);
-        }
+        IFeatureLoader featureLoader = new FeatureLoader();
+        GlobalFeatureLoader.set(featureLoader);
+        IOC.register(Keys.getOrAdd("feature loader"), new SingletonStrategy(featureLoader));
     }
 }
