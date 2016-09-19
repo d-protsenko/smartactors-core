b788a1613cb7772e88b42157846c830d1b5ddefa
Andrey Kutalev <akutalev@gmail.com>
2016-09-19 14:36:38 +0600

    Added new feature for testing deploy process.

---
+++ CoreFeatures/UtilityTools/IClassGenerator/src/main/java/info/smart_tools/smartactors/utility_tools/interfaces/iclass_generator/exception/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package contains exceptions for {@link info.smart_tools.smartactors.utility_tools.interfaces.iclass_generator.IClassGenerator} methods
+ */
+package info.smart_tools.smartactors.utility_tools.interfaces.iclass_generator.exception;
