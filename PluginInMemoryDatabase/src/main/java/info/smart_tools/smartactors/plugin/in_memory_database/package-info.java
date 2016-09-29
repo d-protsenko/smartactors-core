d1b7d9b5e18768440621c29cd0308a988c2d210c
Andrey Kutalev <akutalev@gmail.com>
2016-09-29 21:15:19 +0600

    Aggrigated new plugin-pack - Database-in-memory-plugins. Refactoring.

--- PluginInMemoryDatabase/src/main/java/info/smart_tools/smartactors/plugin/in_memory_database/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package contains plugin for in memory database
- */
-package info.smart_tools.smartactors.plugin.in_memory_database;
--- examples/src/main/java/info/smart_tools/smartactors/core/examples/db_collection/InMemoryDBCollectionServer.java
+++ examples/src/main/java/info/smart_tools/smartactors/core/examples/db_collection/InMemoryDBCollectionServer.java
@@ -14,8 +14,8 @@ import info.smart_tools.smartactors.database_postgresql.postgres_connection.wrap
 import info.smart_tools.smartactors.iobject_plugins.dsobject_plugin.PluginDSObject;
 import info.smart_tools.smartactors.field_plugins.ifield_plugin.IFieldPlugin;
 import info.smart_tools.smartactors.iobject_plugins.ifieldname_plugin.IFieldNamePlugin;
-import info.smart_tools.smartactors.plugin.in_memory_database.PluginInMemoryDatabase;
-import info.smart_tools.smartactors.plugin.in_memory_db_tasks.PluginInMemoryDBTasks;
+import info.smart_tools.smartactors.database_in_memory_plugins.in_memory_database_plugin.PluginInMemoryDatabase;
+import info.smart_tools.smartactors.database_in_memory_plugins.in_memory_db_tasks_plugin.PluginInMemoryDBTasks;
 import info.smart_tools.smartactors.ioc_plugins.ioc_keys_plugin.PluginIOCKeys;
 import info.smart_tools.smartactors.ioc_plugins.ioc_simple_container_plugin.PluginIOCSimpleContainer;
 import info.smart_tools.smartactors.plugin.null_connection_pool.NullConnectionPoolPlugin;
