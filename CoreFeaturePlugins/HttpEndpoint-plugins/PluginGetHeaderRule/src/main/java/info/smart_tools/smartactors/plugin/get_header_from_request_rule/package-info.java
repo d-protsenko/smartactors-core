c6cd116036c709c5ef0fc9ca8cfc30bb15b8d912
Andrey Kutalev <akutalev@gmail.com>
2016-10-03 21:17:20 +0600

    Aggrigated new plugin-packs - HttpEndpoint-plugins and HttpsEndpoint-plugins. Refactoring.

--- CoreFeaturePlugins/HttpEndpoint-plugins/PluginGetHeaderRule/src/main/java/info/smart_tools/smartactors/plugin/get_header_from_request_rule/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contain plugin for registering {@link info.smart_tools.smartactors.http_endpoint.strategy.get_header_from_request.GetHeaderFromRequestRule}
- */
-package info.smart_tools.smartactors.plugin.get_header_from_request_rule;
---
+++ CoreFeaturePlugins/HttpsEndpoint-plugins/PluginHttpsEndpoint/src/main/java/info/smart_tools/smartactors/https_endpoint_plugins/https_endpoint_plugin/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package contains plugin for https endpoint
+ */
+package info.smart_tools.smartactors.https_endpoint_plugins.https_endpoint_plugin;
