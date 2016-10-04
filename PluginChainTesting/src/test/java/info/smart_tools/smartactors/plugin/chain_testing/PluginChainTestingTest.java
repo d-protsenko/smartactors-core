529aaed55a02141ed5a5d529be749154da98397a
Andrey Kutalev <akutalev@gmail.com>
2016-10-04 13:57:17 +0600

    Aggrigated new plugin-pack - Testing-plugins. Refactoring.

--- PluginChainTesting/src/test/java/info/smart_tools/smartactors/plugin/chain_testing/PluginChainTestingTest.java
+++
@@ -1,7 +0,0 @@
-package info.smart_tools.smartactors.plugin.chain_testing;
-
-/**
- * Tests for {@link PluginChainTesting}.
- */
-public class PluginChainTestingTest {
-}
--- PluginChainTestingAssertions/src/test/java/info/smart_tools/smartactors/plugin/chain_testing/assertions/Checker.java
+++
@@ -1,7 +0,0 @@
-package info.smart_tools.smartactors.plugin.chain_testing.assertions;
-
-import info.smart_tools.smartactors.feature_loading_system.interfaces.ibootstrap_item.IBootstrapItem;
-
-class Checker {
-    public IBootstrapItem<String> item;
-}
