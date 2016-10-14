cd7d7a4cc47ff86aaa245870bdf928789532108f
Vitaly <stels95@list.ru>
2016-10-14 17:24:52 +0600

    fix dependencies

--- PluginCheckValidityAsyncOperationActor/src/main/java/info/smart_tools/smartactors/plugin/check_validity_async_operation_actor/package-info.java
+++ PluginCheckValidityAsyncOperationActor/src/main/java/info/smart_tools/smartactors/plugin/check_validity_async_operation_actor/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Package contains plugin for {@link info.smart_tools.smartactors.core.actor.CreateSessionActor}
+ * Package contains plugin for {@link info.smart_tools.smartactors.core.actors.check_validity_async_operation.CheckValidityAsyncOperationActor}
  */
 package info.smart_tools.smartactors.plugin.check_validity_async_operation_actor;
