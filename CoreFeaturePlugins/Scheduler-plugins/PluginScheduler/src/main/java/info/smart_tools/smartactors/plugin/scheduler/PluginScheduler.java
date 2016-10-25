2c18bce118d2f13133a5159bd82d17e58fcf4b31
AlexeyBond <alexey.bond.94.55@gmail.com>
2016-10-25 20:48:05 +0600

    Make scheduler execute a ISchedulerAction instead of just sending a message.

--- CoreFeaturePlugins/Scheduler-plugins/PluginScheduler/src/main/java/info/smart_tools/smartactors/plugin/scheduler/PluginScheduler.java
+++ CoreFeaturePlugins/Scheduler-plugins/PluginScheduler/src/main/java/info/smart_tools/smartactors/plugin/scheduler/PluginScheduler.java
@@ -4,6 +4,7 @@ import info.smart_tools.smartactors.base.exception.invalid_argument_exception.In
 import info.smart_tools.smartactors.base.interfaces.iaction.exception.FunctionExecutionException;
 import info.smart_tools.smartactors.base.interfaces.ipool.IPool;
 import info.smart_tools.smartactors.base.strategy.apply_function_to_arguments.ApplyFunctionToArgumentsStrategy;
+import info.smart_tools.smartactors.base.strategy.singleton_strategy.SingletonStrategy;
 import info.smart_tools.smartactors.feature_loading_system.bootstrap_plugin.BootstrapPlugin;
 import info.smart_tools.smartactors.feature_loading_system.interfaces.ibootstrap.IBootstrap;
 import info.smart_tools.smartactors.iobject.iobject.IObject;
@@ -12,6 +13,7 @@ import info.smart_tools.smartactors.ioc.iioccontainer.exception.ResolutionExcept
 import info.smart_tools.smartactors.ioc.ioc.IOC;
 import info.smart_tools.smartactors.ioc.named_keys_storage.Keys;
 import info.smart_tools.smartactors.scheduler.actor.SchedulerActor;
+import info.smart_tools.smartactors.scheduler.actor.impl.DefaultSchedulerAction;
 import info.smart_tools.smartactors.scheduler.actor.impl.EntryImpl;
 import info.smart_tools.smartactors.scheduler.actor.impl.EntryStorage;
 import info.smart_tools.smartactors.scheduler.interfaces.ISchedulerEntryStorage;
@@ -27,6 +29,22 @@ public class PluginScheduler extends BootstrapPlugin {
      */
     public PluginScheduler(final IBootstrap bootstrap) {
         super(bootstrap);
+    }
+
+    /**
+     * Register default action to be executed by scheduler when entry fires.
+     *
+     * @throws ResolutionException if error occurs resolving key
+     * @throws RegistrationException if error occurs registering strategy
+     * @throws InvalidArgumentException if {@link ApplyFunctionToArgumentsStrategy} does not like our function
+     */
+    @Item("scheduler_default_action")
+    public void registerDefaultAction()
+            throws ResolutionException, RegistrationException, InvalidArgumentException {
+        IOC.register(
+                Keys.getOrAdd("default scheduler action"),
+                new SingletonStrategy(new DefaultSchedulerAction())
+        );
     }
 
     /**
@@ -72,8 +90,13 @@ public class PluginScheduler extends BootstrapPlugin {
     @After({"scheduler_entry_strategies"})
     public void registerStorage()
             throws ResolutionException, RegistrationException, InvalidArgumentException {
-        IOC.register(Keys.getOrAdd(ISchedulerEntryStorage.class.getCanonicalName()), new ApplyFunctionToArgumentsStrategy(args ->
-            new EntryStorage((IPool) args[0], (String) args[1])));
+        IOC.register(Keys.getOrAdd(ISchedulerEntryStorage.class.getCanonicalName()), new ApplyFunctionToArgumentsStrategy(args -> {
+            try {
+                return new EntryStorage((IPool) args[0], (String) args[1]);
+            } catch (ResolutionException e) {
+                throw new FunctionExecutionException(e);
+            }
+        }));
     }
 
     /**
---
+++ CoreFeatures/Scheduler/SchedulerActor/src/main/java/info/smart_tools/smartactors/scheduler/actor/impl/DefaultSchedulerAction.java
@@ -0,0 +1,67 @@
+package info.smart_tools.smartactors.scheduler.actor.impl;
+
+import info.smart_tools.smartactors.base.exception.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.iobject.ifield_name.IFieldName;
+import info.smart_tools.smartactors.iobject.iobject.IObject;
+import info.smart_tools.smartactors.iobject.iobject.exception.ChangeValueException;
+import info.smart_tools.smartactors.iobject.iobject.exception.ReadValueException;
+import info.smart_tools.smartactors.iobject.iobject.exception.SerializeException;
+import info.smart_tools.smartactors.ioc.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.ioc.ioc.IOC;
+import info.smart_tools.smartactors.ioc.named_keys_storage.Keys;
+import info.smart_tools.smartactors.message_bus.interfaces.imessage_bus_container.exception.SendingMessageException;
+import info.smart_tools.smartactors.message_bus.message_bus.MessageBus;
+import info.smart_tools.smartactors.scheduler.interfaces.ISchedulerAction;
+import info.smart_tools.smartactors.scheduler.interfaces.ISchedulerEntry;
+import info.smart_tools.smartactors.scheduler.interfaces.exceptions.SchedulerActionExecutionException;
+import info.smart_tools.smartactors.scheduler.interfaces.exceptions.SchedulerActionInitializationException;
+
+/**
+ * Default scheduler action - send message stored in "message" field of arguments to {@link
+ * info.smart_tools.smartactors.message_bus.message_bus.MessageBus message bus}.
+ */
+public class DefaultSchedulerAction implements ISchedulerAction {
+    private final IFieldName messageFieldName;
+
+    /**
+     * The constructor.
+     *
+     * @throws ResolutionException if cannot resolve any dependency
+     */
+    public DefaultSchedulerAction()
+            throws ResolutionException {
+        messageFieldName = IOC.resolve(Keys.getOrAdd(IFieldName.class.getCanonicalName()), "message");
+    }
+
+    @Override
+    public void init(final ISchedulerEntry entry, final IObject args)
+            throws SchedulerActionInitializationException {
+        try {
+            Object message = args.getValue(messageFieldName);
+
+            if (message == null || !(message instanceof IObject)) {
+                throw new SchedulerActionInitializationException("\"message\" field of arguments should contain a message object", null);
+            }
+
+            entry.getState().setValue(messageFieldName, message);
+        } catch (ReadValueException | ChangeValueException | InvalidArgumentException e) {
+            throw new SchedulerActionInitializationException("Error occurred copying message from arguments to entry state.", e);
+        }
+    }
+
+    @Override
+    public void execute(final ISchedulerEntry entry)
+            throws SchedulerActionExecutionException {
+        try {
+            IObject message = (IObject) entry.getState().getValue(messageFieldName);
+
+            String serialized = message.serialize();
+
+            message = IOC.resolve(Keys.getOrAdd(IObject.class.getCanonicalName()), serialized);
+
+            MessageBus.send(message);
+        } catch (ReadValueException | InvalidArgumentException | SendingMessageException | SerializeException | ResolutionException e) {
+            throw new SchedulerActionExecutionException("Error occurred sending the message.", e);
+        }
+    }
+}
