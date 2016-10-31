f2a298d7571c532d7d1ba8f9b48fa4ff69100508
Andrey Kutalev <akutalev@gmail.com>
2016-10-31 21:56:10 +0600

    Added EmailPlugins, SyncOperationsPlugins to the CommonFeaturePlugins.

---
+++ CommonFeaturePlugins/AsyncOperations-plugins/PluginCheckValidityAsyncOperationActor/src/main/java/info/smart_tools/smartactors/async_operations_plugins/check_validity_async_operation_actor_plugin/CheckValidityAsyncOperationPlugin.java
@@ -0,0 +1,63 @@
+package info.smart_tools.smartactors.async_operations_plugins.check_validity_async_operation_actor_plugin;
+
+import info.smart_tools.smartactors.async_operations.check_validity_async_operation.CheckValidityAsyncOperationActor;
+import info.smart_tools.smartactors.feature_loading_system.bootstrap_item.BootstrapItem;
+import info.smart_tools.smartactors.base.interfaces.iaction.exception.ActionExecuteException;
+import info.smart_tools.smartactors.feature_loading_system.interfaces.ibootstrap.IBootstrap;
+import info.smart_tools.smartactors.feature_loading_system.interfaces.ibootstrap_item.IBootstrapItem;
+import info.smart_tools.smartactors.ioc.iioccontainer.exception.RegistrationException;
+import info.smart_tools.smartactors.ioc.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.ioc.ikey.IKey;
+import info.smart_tools.smartactors.base.exception.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.iobject.iobject.IObject;
+import info.smart_tools.smartactors.ioc.ioc.IOC;
+import info.smart_tools.smartactors.feature_loading_system.interfaces.iplugin.IPlugin;
+import info.smart_tools.smartactors.feature_loading_system.interfaces.iplugin.exception.PluginException;
+import info.smart_tools.smartactors.ioc.named_keys_storage.Keys;
+import info.smart_tools.smartactors.base.strategy.apply_function_to_arguments.ApplyFunctionToArgumentsStrategy;
+
+/**
+ * Plugin for load IOC-strategy for check validity asynchronous operation actor
+ */
+public class CheckValidityAsyncOperationPlugin implements IPlugin {
+
+    private final IBootstrap<IBootstrapItem<String>> bootstrap;
+
+    /**
+     * Constructor for create instance of CheckValidityAsyncOperationPlugin
+     * @param bootstrap the bootstrap
+     */
+    public CheckValidityAsyncOperationPlugin(final IBootstrap<IBootstrapItem<String>> bootstrap) {
+        this.bootstrap = bootstrap;
+    }
+
+    @Override
+    public void load() throws PluginException {
+        try {
+            IBootstrapItem<String> item = new BootstrapItem("CreateCheckValidityAsyncOperationActor");
+
+            item
+                    .after("IOC")
+                    .before("starter")
+                    .process(() -> {
+                try {
+                    IKey operationKey = Keys.getOrAdd(CheckValidityAsyncOperationActor.class.getCanonicalName());
+                    IOC.register(operationKey, new ApplyFunctionToArgumentsStrategy(
+                            (args) -> {
+                                return new CheckValidityAsyncOperationActor((IObject) args[0]);
+                        }
+                    ));
+                } catch (ResolutionException e) {
+                    throw new ActionExecuteException("CheckValidityAsyncOperationActor plugin can't load: can't get CheckValidityAsyncOperationActor key", e);
+                } catch (InvalidArgumentException e) {
+                    throw new ActionExecuteException("CheckValidityAsyncOperationActor plugin can't load: can't create strategy", e);
+                } catch (RegistrationException e) {
+                    throw new ActionExecuteException("CheckValidityAsyncOperationActor plugin can't load: can't register new strategy", e);
+                }
+            });
+            bootstrap.add(item);
+        } catch (InvalidArgumentException e) {
+            throw new PluginException("Can't load CheckValidityAsyncOperation plugin", e);
+        }
+    }
+}
---
+++ CommonFeaturePlugins/AsyncOperations-plugins/PluginCheckValidityAsyncOperationActor/src/main/java/info/smart_tools/smartactors/async_operations_plugins/check_validity_async_operation_actor_plugin/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package contains plugin for {@link info.smart_tools.smartactors.core.actors.check_validity_async_operation.CheckValidityAsyncOperationActor}
+ */
+package info.smart_tools.smartactors.async_operations_plugins.check_validity_async_operation_actor_plugin;
