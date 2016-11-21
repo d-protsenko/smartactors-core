41a329dfc97f0ba4fda64e8230000c13d720aa05
AlexeyBond <alexey.bond.94.55@gmail.com>
2016-11-21 17:17:36 +0600

    Add plugins for debugger actor and related components.

---
+++ CoreFeaturePlugins/Debugger-plugins/PluginDebuggerSequence/src/main/java/info/smart_tools/smartactors/debugger_plugins/debugger_sequence/PluginDebuggerSequence.java
@@ -0,0 +1,49 @@
+package info.smart_tools.smartactors.debugger_plugins.debugger_sequence;
+
+import info.smart_tools.smartactors.base.exception.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.base.interfaces.iaction.exception.FunctionExecutionException;
+import info.smart_tools.smartactors.base.strategy.apply_function_to_arguments.ApplyFunctionToArgumentsStrategy;
+import info.smart_tools.smartactors.debugger.sequence_impl.DebuggerSequenceImpl;
+import info.smart_tools.smartactors.feature_loading_system.bootstrap_plugin.BootstrapPlugin;
+import info.smart_tools.smartactors.feature_loading_system.interfaces.ibootstrap.IBootstrap;
+import info.smart_tools.smartactors.ioc.iioccontainer.exception.RegistrationException;
+import info.smart_tools.smartactors.ioc.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.ioc.ioc.IOC;
+import info.smart_tools.smartactors.ioc.named_keys_storage.Keys;
+import info.smart_tools.smartactors.message_processing_interfaces.message_processing.IMessageProcessingSequence;
+
+/**
+ *
+ */
+public class PluginDebuggerSequence extends BootstrapPlugin {
+    /**
+     * The constructor.
+     *
+     * @param bootstrap the bootstrap
+     */
+    protected PluginDebuggerSequence(final IBootstrap bootstrap) {
+        super(bootstrap);
+    }
+
+    /**
+     * Register strategy of creation of debugger sequence.
+     *
+     * @throws ResolutionException if error occurs resolving a key
+     * @throws RegistrationException if error occurs registering a strategy
+     * @throws InvalidArgumentException if strategy does not accept the function
+     */
+    @Item("debugger:sequence")
+    public void registerSequence()
+            throws ResolutionException, RegistrationException, InvalidArgumentException {
+        IOC.register(Keys.getOrAdd("new debugger sequence"), new ApplyFunctionToArgumentsStrategy(args -> {
+            IMessageProcessingSequence innerSeq = (IMessageProcessingSequence) args[0];
+            Object debuggerAddress = args[1];
+
+            try {
+                return new DebuggerSequenceImpl(innerSeq, debuggerAddress);
+            } catch (ResolutionException e) {
+                throw new FunctionExecutionException(e);
+            }
+        }));
+    }
+}
---
+++ CoreFeaturePlugins/Debugger-plugins/PluginDebuggerSession/src/main/java/info/smart_tools/smartactors/debugger_plugins/session_plugin/PluginDebuggerSession.java
@@ -0,0 +1,49 @@
+package info.smart_tools.smartactors.debugger_plugins.session_plugin;
+
+import info.smart_tools.smartactors.base.exception.invalid_argument_exception.InvalidArgumentException;
+import info.smart_tools.smartactors.base.interfaces.iaction.exception.FunctionExecutionException;
+import info.smart_tools.smartactors.base.strategy.apply_function_to_arguments.ApplyFunctionToArgumentsStrategy;
+import info.smart_tools.smartactors.debugger.session_impl.DebuggerSessionImpl;
+import info.smart_tools.smartactors.feature_loading_system.bootstrap_plugin.BootstrapPlugin;
+import info.smart_tools.smartactors.feature_loading_system.interfaces.ibootstrap.IBootstrap;
+import info.smart_tools.smartactors.ioc.iioccontainer.exception.RegistrationException;
+import info.smart_tools.smartactors.ioc.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.ioc.ioc.IOC;
+import info.smart_tools.smartactors.ioc.named_keys_storage.Keys;
+
+/**
+ *
+ */
+public class PluginDebuggerSession extends BootstrapPlugin {
+    /**
+     * The constructor.
+     *
+     * @param bootstrap the bootstrap
+     */
+    protected PluginDebuggerSession(final IBootstrap bootstrap) {
+        super(bootstrap);
+    }
+
+    /**
+     * Register strategy of creation of a debugger session.
+     *
+     * @throws ResolutionException if error occurs resolving a key
+     * @throws RegistrationException if error occurs registering a strategy
+     * @throws InvalidArgumentException if strategy does not accept the function
+     */
+    @Item("debugger:session")
+    @After({"debugger:sequence"})
+    public void registerSessionStrategy()
+            throws ResolutionException, RegistrationException, InvalidArgumentException {
+        IOC.register(Keys.getOrAdd("debugger session"), new ApplyFunctionToArgumentsStrategy(args -> {
+            String id = (String) args[0];
+            Object addr = args[1];
+
+            try {
+                return new DebuggerSessionImpl(id, addr);
+            } catch (ResolutionException e) {
+                throw new FunctionExecutionException(e);
+            }
+        }));
+    }
+}
