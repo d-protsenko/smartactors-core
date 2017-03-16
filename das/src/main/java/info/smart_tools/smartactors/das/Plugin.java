204b1e672aa8eb30ea3a62cc9888dc125b3ceccc
Andrey Kutalev <ak@7bits.it>
2017-03-16 13:05:53 +0600

    Fixed das bugs.

--- das/src/main/java/info/smart_tools/smartactors/das/Plugin.java
+++ das/src/main/java/info/smart_tools/smartactors/das/Plugin.java
@@ -37,8 +37,8 @@ public class Plugin {
     private static final String DEPLOY_REPOSITORY_ID_TOKEN = "${upload.repository.id}";
     private static final String DEPLOY_REPOSITORY_URL_TOKEN = "${upload.repository.url}";
 
-    private static final String SRC_MAIN_JAVA = "scr/main/java";
-    private static final String SRC_TEST_JAVA = "scr/test/java";
+    private static final String SRC_MAIN_JAVA = "src/main/java";
+    private static final String SRC_TEST_JAVA = "src/test/java";
 
     private static final String CLASS_FILE_EXTENSION = ".java";
 
@@ -73,7 +73,7 @@ public class Plugin {
             this.rawName = name + PLUGIN_POSTFIX;
         }
         this.ownerFeature = feature;
-        this.artifactId = ParameterResolver.getArtifactId(name);
+        this.artifactId = ParameterResolver.getArtifactId(this.rawName);
         if (null == version || version.isEmpty()) {
             this.version = feature.getVersion();
         } else {
