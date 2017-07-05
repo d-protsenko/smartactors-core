7c787bc9be7336498895ef152103031a356b2ea7
aslesarev <aslesarev@hwdtech.ru>
2017-07-05 18:25:28 +0600

    Add IMessageProcessor#resetEnironment(), fix ActorCollectionReceiver.

--- SystemActors/SystemActorPack/ActorCollectionReceiver/src/test/java/info/smart_tools/smartactors/system_actors_pack/actor_collection_receiver/ActorCollectionReceiverTest.java
+++ SystemActors/SystemActorPack/ActorCollectionReceiver/src/test/java/info/smart_tools/smartactors/system_actors_pack/actor_collection_receiver/ActorCollectionReceiverTest.java
@@ -180,4 +180,21 @@ public class ActorCollectionReceiverTest extends PluginsLoadingTestBase {
         IMessageReceiver receiver = new ActorCollectionReceiver(new HashMap<>());
         receiver.receive(processorMocks[0]);
     }
+
+    @Test
+    public void Should_resetMessageProcessorEnvironmentBeforeReceiverCall()
+            throws Exception {
+        doAnswer(invocation -> {
+            verify(processorMocks[0]).resetEnvironment();
+            return null;
+        }).when(childReceiverMocks[0]).receive(same(processorMocks[0]));
+
+        IMessageReceiver receiver = new ActorCollectionReceiver(new HashMap<>());
+
+        receiver.receive(processorMocks[0]);
+
+        verify(childReceiverMocks[0], times(1)).receive(same(processorMocks[0]));
+
+        verify(creatorMocks[0], times(1)).create(any(), any(), any());
+    }
 }
