c51fea11912cce0463f0e75327218026831353e5
Ilya Siganov <ilya.blan4@gmail.com>
2017-07-11 11:40:58 +0600

    add java docs and Options to the CreateCollectionActor

--- CommonFeatures/DatabasePostgresqlCreateCollectionIfNotExists/CreateCollectionActor/src/main/java/info/smart_tools/smartactors/database_postgresql_create_collection_if_not_exists/create_collection_actor/CreateCollectionActor.java
+++ CommonFeatures/DatabasePostgresqlCreateCollectionIfNotExists/CreateCollectionActor/src/main/java/info/smart_tools/smartactors/database_postgresql_create_collection_if_not_exists/create_collection_actor/CreateCollectionActor.java
@@ -3,9 +3,9 @@ package info.smart_tools.smartactors.database_postgresql_create_collection_if_no
 import info.smart_tools.smartactors.base.interfaces.ipool.IPool;
 import info.smart_tools.smartactors.base.pool_guard.PoolGuard;
 import info.smart_tools.smartactors.base.pool_guard.exception.PoolGuardException;
+import info.smart_tools.smartactors.database_postgresql.postgres_connection.wrapper.ConnectionOptions;
 import info.smart_tools.smartactors.database_postgresql_create_collection_if_not_exists.create_collection_actor.exception.CreateCollectionActorException;
 import info.smart_tools.smartactors.database_postgresql_create_collection_if_not_exists.create_collection_actor.wrapper.CreateCollectionWrapper;
-import info.smart_tools.smartactors.database_postgresql.postgres_connection.wrapper.ConnectionOptions;
 import info.smart_tools.smartactors.iobject.iobject.exception.ReadValueException;
 import info.smart_tools.smartactors.ioc.iioccontainer.exception.ResolutionException;
 import info.smart_tools.smartactors.ioc.ioc.IOC;
@@ -13,6 +13,12 @@ import info.smart_tools.smartactors.ioc.named_keys_storage.Keys;
 import info.smart_tools.smartactors.task.interfaces.itask.ITask;
 import info.smart_tools.smartactors.task.interfaces.itask.exception.TaskExecutionException;
 
+/**
+ * This actor gets a {@link ConnectionOptions} object from the IOC by the key from this actor's wrapper.
+ * Then it gets an {@link IPool} object from the IOC by hard-coded name `PostgresConnectionPool`.
+ * Then using collection name field from the wrapper it calls `db.collection.create-if-not-exists` {@link ITask}
+ * so creates collection if it is not exist yet.
+ */
 public class CreateCollectionActor {
     public void createTable(CreateCollectionWrapper message) throws CreateCollectionActorException {
         try {
@@ -24,7 +30,8 @@ public class CreateCollectionActor {
                 ITask task = IOC.resolve(
                         Keys.getOrAdd("db.collection.create-if-not-exists"),
                         guard.getObject(),
-                        collectionName
+                        collectionName,
+                        message.getOptions()
                 );
                 task.execute();
             } catch (TaskExecutionException | ResolutionException | PoolGuardException e) {
