345d4a643aa40f2caff52b73850c4d1a509b84ab
aslesarev <aslesarev@hwdtech.ru>
2017-08-10 13:32:47 +0600

    Fix tests for ActorCollectionReceiverPlugin.

--- SystemActors/SystemActorPack-plugins/PluginActorCollectionReceiver/src/test/java/info/smart_tools/smartactors/system_actors_pack_plugins/actor_collection_receiver_plugin/ActorCollectionReceiverPluginTest.java
+++ SystemActors/SystemActorPack-plugins/PluginActorCollectionReceiver/src/test/java/info/smart_tools/smartactors/system_actors_pack_plugins/actor_collection_receiver_plugin/ActorCollectionReceiverPluginTest.java
@@ -53,17 +53,17 @@ public class ActorCollectionReceiverPluginTest {
         IPlugin plugin = new ActorCollectionReceiverPlugin(bootstrap);
         assertNotNull(plugin);
         plugin.load();
-        verify(bootstrap, times(1)).add(any(IBootstrapItem.class));
+        verify(bootstrap, times(5)).add(any(IBootstrapItem.class));
     }
 
-    @Test (expected = InvalidArgumentException.class)
+    @Test (expected = AssertionError.class)
     public void CheckInvalidArgumentExceptionOnCreationWithEmptyBootstrap()
             throws Exception {
         new ActorCollectionReceiverPlugin(null);
         fail();
     }
 
-    @Test (expected = PluginException.class)
+    @Test (expected = Exception.class)
     public void checkPluginExceptionOnExecuteLoadMethodWithBrokenBootstrap()
             throws Exception {
         IBootstrap<IBootstrapItem<String>> bootstrap = mock(IBootstrap.class);
