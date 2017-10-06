5b6fe583d074215db5830c8cd48d53e93ccb9894
aslesarev <aslesarev@hwdtech.ru>
2017-10-06 17:22:44 +0600

    Add some tests for DefaultEndpointPipelineImplementation, add javadoc's.

--- CoreFeatures/EndpointComponentsGeneric/DefaultEndpointPipelineImplementation/src/main/java/info/smart_tools/smartactors/endpoint_components_generic/default_endpoint_pipeline_implementation/DefaultEndpointPipelineImplementation.java
+++ CoreFeatures/EndpointComponentsGeneric/DefaultEndpointPipelineImplementation/src/main/java/info/smart_tools/smartactors/endpoint_components_generic/default_endpoint_pipeline_implementation/DefaultEndpointPipelineImplementation.java
@@ -10,11 +10,20 @@ import info.smart_tools.smartactors.endpoint_interfaces.imessage_handler.excepti
 import java.util.List;
 import java.util.ListIterator;
 
+/**
+ * Implementation of {@link IEndpointPipeline}
+ */
 public class DefaultEndpointPipelineImplementation implements IEndpointPipeline {
     private final List<IMessageHandler> handlers;
     private final IFunction0<IMessageContext> contextFactory;
     private IMessageHandlerCallback<IMessageContext> callback;
 
+    /**
+     * The constructor.
+     *
+     * @param handlers          list of handlers
+     * @param contextFactory    function creating message contexts
+     */
     public DefaultEndpointPipelineImplementation(
             final List<IMessageHandler> handlers,
             final IFunction0<IMessageContext> contextFactory) {
@@ -24,7 +33,7 @@ public class DefaultEndpointPipelineImplementation implements IEndpointPipeline 
         this.contextFactory = contextFactory;
         this.handlers = handlers;
         ListIterator li = handlers.listIterator(handlers.size());
-        while(li.hasPrevious()) {
+        while (li.hasPrevious()) {
             IMessageHandler handler = (IMessageHandler) li.previous();
             this.callback = (ctx -> {
                 handler.handle(callback, ctx);
--- CoreFeatures/EndpointComponentsGeneric/DefaultEndpointPipelineImplementation/src/test/java/info/smart_tools/smartactors/endpoint_components_generic/default_endpoint_pipeline_implementation/DefaultEndpointPipelineImplementationTest.java
+++ CoreFeatures/EndpointComponentsGeneric/DefaultEndpointPipelineImplementation/src/test/java/info/smart_tools/smartactors/endpoint_components_generic/default_endpoint_pipeline_implementation/DefaultEndpointPipelineImplementationTest.java
@@ -1,29 +1,26 @@
 package info.smart_tools.smartactors.endpoint_components_generic.default_endpoint_pipeline_implementation;
 
 import info.smart_tools.smartactors.base.interfaces.iaction.IFunction0;
-import info.smart_tools.smartactors.endpoint_components_generic.asynchronous_unordered_message_handler.AsynchronousUnorderedMessageHandler;
-import info.smart_tools.smartactors.endpoint_components_generic.create_environment_message_handler.CreateEnvironmentMessageHandler;
-import info.smart_tools.smartactors.endpoint_components_generic.scope_setter_message_handler.ScopeSetterMessageHandler;
-import info.smart_tools.smartactors.endpoint_components_generic.send_internal_message_handler.SendInternalMessageHandler;
 import info.smart_tools.smartactors.endpoint_interfaces.iendpoint_pipeline.IEndpointPipeline;
+import info.smart_tools.smartactors.endpoint_interfaces.imessage_handler.IMessageContext;
 import info.smart_tools.smartactors.endpoint_interfaces.imessage_handler.IMessageHandler;
+import info.smart_tools.smartactors.endpoint_interfaces.imessage_handler.exception.MessageHandlerException;
 import info.smart_tools.smartactors.field_plugins.ifield_plugin.IFieldPlugin;
 import info.smart_tools.smartactors.helpers.plugins_loading_test_base.PluginsLoadingTestBase;
 import info.smart_tools.smartactors.iobject_plugins.dsobject_plugin.PluginDSObject;
 import info.smart_tools.smartactors.iobject_plugins.ifieldname_plugin.IFieldNamePlugin;
 import info.smart_tools.smartactors.ioc_plugins.ioc_keys_plugin.PluginIOCKeys;
-import info.smart_tools.smartactors.message_processing_interfaces.message_processing.IReceiverChain;
-import info.smart_tools.smartactors.scope.iscope.IScope;
 import info.smart_tools.smartactors.scope_plugins.scope_provider_plugin.PluginScopeProvider;
 import info.smart_tools.smartactors.scope_plugins.scoped_ioc_plugin.ScopedIOCPlugin;
-import info.smart_tools.smartactors.task.interfaces.iqueue.IQueue;
-import info.smart_tools.smartactors.task.interfaces.itask.ITask;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
 
 public class DefaultEndpointPipelineImplementationTest extends PluginsLoadingTestBase {
     @Override
@@ -42,18 +39,32 @@ public class DefaultEndpointPipelineImplementationTest extends PluginsLoadingTes
     @Test
     public void Should_buildPipeline()
             throws Exception {
-        int stackDepth = 5;
-        IQueue<ITask> taskQueueMock = mock(IQueue.class);
+        IMessageContext ctx = mock(IMessageContext.class);
         IFunction0 ctxFactory = mock(IFunction0.class);
-        IReceiverChain receiverChainMock = mock(IReceiverChain.class);
-        IScope scopeMock = mock(IScope.class);
+        when(ctxFactory.execute()).thenReturn(ctx);
+
+        IMessageHandler handler1 = mock(IMessageHandler.class);
+        IMessageHandler handler2 = mock(IMessageHandler.class);
+
         List<IMessageHandler> handlers = new ArrayList<IMessageHandler>() {{
-            add(new SendInternalMessageHandler<>(stackDepth, taskQueueMock, receiverChainMock));
-            add(new CreateEnvironmentMessageHandler<>());
-            add(new ScopeSetterMessageHandler<>(scopeMock));
-            add(new AsynchronousUnorderedMessageHandler<>(taskQueueMock));
+            add(handler1);
+            add(handler2);
         }};
 
         IEndpointPipeline pipeline = new DefaultEndpointPipelineImplementation(handlers, ctxFactory);
+        pipeline.getInputCallback().handle(ctx);
+        verify(handler1).handle(anyObject(), eq(ctx));
+
+        assertSame(pipeline.getHandlers(), handlers);
+        assertSame(pipeline.getContextFactory(), ctxFactory);
+    }
+
+    @Test(expected = MessageHandlerException.class)
+    public void Should_thowErrorWhenNoHandlers() throws Exception {
+        IMessageContext ctx = mock(IMessageContext.class);
+        IFunction0 ctxFactory = mock(IFunction0.class);
+        when(ctxFactory.execute()).thenReturn(ctx);
+        IEndpointPipeline pipeline = new DefaultEndpointPipelineImplementation(new ArrayList<>(), ctxFactory);
+        pipeline.getInputCallback().handle(ctx);
     }
 }
