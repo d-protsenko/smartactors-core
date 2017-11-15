54e30bc59e1dee1726421a5aa493233f515e85da
AlexeyBond <alexey.bond.94.55@gmail.com>
2017-11-15 17:34:11 +0600

    Rewrite DefaultTcpClientConnectionPool using channel pool provided by Netty.

--- CoreFeatures/EndpointComponentsNetty/DefaultTcpClientConnectionPool/src/main/java/info/smart_tools/smartactors/endpoint_components_netty/default_tcp_client_connection_pool/DefaultTcpClientConnectionPool.java
+++ CoreFeatures/EndpointComponentsNetty/DefaultTcpClientConnectionPool/src/main/java/info/smart_tools/smartactors/endpoint_components_netty/default_tcp_client_connection_pool/DefaultTcpClientConnectionPool.java
@@ -1,154 +1,95 @@
 package info.smart_tools.smartactors.endpoint_components_netty.default_tcp_client_connection_pool;
 
-import info.smart_tools.smartactors.base.exception.invalid_argument_exception.InvalidArgumentException;
 import info.smart_tools.smartactors.base.interfaces.iaction.IAction;
-import info.smart_tools.smartactors.base.interfaces.iaction.exception.ActionExecuteException;
+import info.smart_tools.smartactors.base.interfaces.iaction.IBiFunction;
 import info.smart_tools.smartactors.endpoint_components_netty.isocket_connection_pool.ISocketConnectionPool;
 import info.smart_tools.smartactors.endpoint_components_netty.isocket_connection_pool.exception.SocketConnectionPoolException;
-import io.netty.channel.ChannelFactory;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.pool.*;
 import io.netty.channel.socket.SocketChannel;
 
 import java.net.InetSocketAddress;
-import java.util.Map;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Implementation of {@link ISocketConnectionPool} for TCP sockets.
+ *
+ * <p>
+ *  This implementation uses Netty's {@link ChannelPool} and {@link ChannelPoolMap}.
+ * </p>
  */
 public class DefaultTcpClientConnectionPool implements ISocketConnectionPool<SocketChannel, InetSocketAddress> {
     private final ChannelFactory<? extends SocketChannel> channelFactory;
-    private final Map<InetSocketAddress, ChannelCollection> channelCollections;
     private final IAction<SocketChannel> channelSetupCallback;
+    private final EventLoopGroup eventLoopGroup;
+    private final IBiFunction<Bootstrap, ChannelPoolHandler, ChannelPool> poolFactory;
+
+    private final ChannelPoolHandler channelPoolHandler = new ChannelPoolHandler() {
+        @Override
+        public void channelReleased(final Channel ch) throws Exception {
+        }
+
+        @Override
+        public void channelAcquired(final Channel ch) throws Exception {
+        }
+
+        @Override
+        public void channelCreated(final Channel ch) throws Exception {
+            channelSetupCallback.execute((SocketChannel) ch);
+        }
+    };
+
+    private final ChannelPoolMap<InetSocketAddress, ChannelPool> pools = new AbstractChannelPoolMap<InetSocketAddress, ChannelPool>() {
+        @Override
+        protected ChannelPool newPool(final InetSocketAddress key) {
+            Bootstrap bootstrap = new Bootstrap()
+                    .channelFactory(channelFactory)
+                    .remoteAddress(key)
+                    .group(eventLoopGroup);
+
+            try {
+                return poolFactory.execute(bootstrap, channelPoolHandler);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
 
     /**
      * The constructor.
      *
      * @param channelFactory       channel factory that creates channels
      * @param channelSetupCallback action that configures channels (sets up the pipeline)
+     * @param eventLoopGroup       event loop group to be used by pooled channels
+     * @param poolFactory          factory that creates a {@link ChannelPool} for given bootstrap and {@link ChannelPoolHandler}
      */
     public DefaultTcpClientConnectionPool(
             final ChannelFactory<? extends SocketChannel> channelFactory,
-            final IAction<SocketChannel> channelSetupCallback) {
+            final IAction<SocketChannel> channelSetupCallback,
+            final EventLoopGroup eventLoopGroup,
+            final IBiFunction<Bootstrap, ChannelPoolHandler, ChannelPool> poolFactory) {
         this.channelFactory = channelFactory;
         this.channelSetupCallback = channelSetupCallback;
-        channelCollections = new ConcurrentHashMap<>();
+        this.eventLoopGroup = eventLoopGroup;
+        this.poolFactory = poolFactory;
     }
-
-    /**
-     * {@link ChannelFutureListener} that removes a channel from pool when channel gets closed.
-     */
-    private class ChannelCloseListener implements ChannelFutureListener {
-        @Override
-        public void operationComplete(final ChannelFuture future)
-                throws Exception {
-            SocketChannel channel = (SocketChannel) future.channel();
-
-            ChannelCollection collection = channelCollections.get(channel.remoteAddress());
-
-            collection.closed(channel);
-        }
-    }
-
-    /**
-     * Collection of channels connected to the same address.
-     */
-    private class ChannelCollection {
-        private boolean terminated = false;
-        private Queue<SocketChannel> channels = new ConcurrentLinkedQueue<>();
-
-        void recycle(final SocketChannel channel) {
-            if (terminated) {
-                channel.close();
-                processTerminated();
-            } else {
-                channels.add(channel);
-            }
-        }
-
-        void closed(final SocketChannel channel) {
-            channels.remove(channel);
-
-            if (channels.isEmpty()) {
-                channelCollections.remove(channel.remoteAddress(), this);
-                terminated = true;
-                processTerminated();
-            }
-        }
-
-        SocketChannel get() {
-            SocketChannel channel;
-
-            while (null != (channel = channels.poll())) {
-                if (channel.isOpen()) {
-                    return channel;
-                }
-            }
-
-            return null;
-        }
-
-        void processTerminated() {
-            SocketChannel channel;
-
-            while (null != (channel = channels.poll())) {
-                channel.close();
-            }
-        }
-    }
-
-    private final ChannelFutureListener closeFutureListener = new ChannelCloseListener();
 
     @Override
     public SocketChannel getChannel(final InetSocketAddress address)
             throws SocketConnectionPoolException {
-        SocketChannel channel = tryGetPooled(address);
-
-        if (null != channel) {
-            return channel;
+        try {
+            return (SocketChannel) pools.get(address).acquire().await().getNow();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new SocketConnectionPoolException(e);
+        } catch (Exception e) {
+            throw new SocketConnectionPoolException(e);
         }
-
-        return createChannel(address);
     }
 
     @Override
     public void recycleChannel(final SocketChannel channel)
             throws SocketConnectionPoolException {
-        if (channel.isOpen()) {
-            ChannelCollection collection = channelCollections.computeIfAbsent(
-                    channel.remoteAddress(),
-                    addr -> new ChannelCollection());
-            collection.recycle(channel);
-        }
-    }
-
-    private SocketChannel tryGetPooled(final InetSocketAddress address) {
-        ChannelCollection collection = channelCollections.get(address);
-
-        if (null == collection) {
-            return null;
-        }
-
-        return collection.get();
-    }
-
-    private SocketChannel createChannel(final InetSocketAddress address)
-            throws SocketConnectionPoolException {
-        SocketChannel channel = channelFactory.newChannel();
-
-        channel.closeFuture().addListener(closeFutureListener);
-
-        try {
-            channelSetupCallback.execute(channel);
-        } catch (ActionExecuteException | InvalidArgumentException e) {
-            throw new SocketConnectionPoolException("Error occurred configuring client channel.", e);
-        }
-
-        channel.connect(address);
-
-        return channel;
+        pools.get(channel.remoteAddress()).release(channel);
     }
 }
