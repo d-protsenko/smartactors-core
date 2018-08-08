7bfb238b5e5e908d24eafead662002bc6f304d70
Alexey Kutalev <alexey.kutalev@7bits.it>
2018-08-08 16:00:42 +0600

    Bootstrap process refactored to supportmulti load/revert procedures, configuration.json fixed with new functionality.

--- CoreFeatures/FeatureLoadingSystem/Bootstrap/src/test/java/info/smart_tools/smartactors/feature_loading_system/bootstrap/BootstrapTest.java
+++ CoreFeatures/FeatureLoadingSystem/Bootstrap/src/test/java/info/smart_tools/smartactors/feature_loading_system/bootstrap/BootstrapTest.java
@@ -10,11 +10,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
  * Tests for Bootstrap
@@ -29,25 +25,54 @@ public class BootstrapTest {
         IBootstrapItem item1 = mock(IBootstrapItem.class);
         IBootstrapItem item2 = mock(IBootstrapItem.class);
         IBootstrapItem item3 = mock(IBootstrapItem.class);
+
         doNothing().when(item1).executeProcess();
-        doNothing().when(item1).executeRevertProcess();
         doThrow(ProcessExecutionException.class).doNothing().when(item2).executeProcess();
-        doNothing().when(item2).executeRevertProcess();
         doThrow(ProcessExecutionException.class).doThrow(ProcessExecutionException.class).doNothing().when(item3).executeProcess();
-        doNothing().when(item3).executeRevertProcess();
+
+        doNothing().when(item1).executeRevertProcess();
+        doNothing().when(item2).executeRevertProcess();
+        doNothing().doNothing().doThrow(ProcessExecutionException.class).doNothing().when(item3).executeRevertProcess();
+
+        doReturn(item1).when(item1).process(any());
+        doReturn(item2).when(item2).process(any());
+        doReturn(item3).when(item3).process(any());
+
         bootstrap.add(item1);
         bootstrap.add(item2);
         bootstrap.add(item3);
+
         List<IBootstrapItem<String>> items = bootstrap.start();
         verify(item1, times(1)).executeProcess();
         verify(item2, times(2)).executeProcess();
-        verify(item2, times(1)).executeRevertProcess();
         verify(item3, times(3)).executeProcess();
+        verify(item1, times(0)).executeRevertProcess();
+        verify(item2, times(1)).executeRevertProcess();
         verify(item3, times(2)).executeRevertProcess();
+
+        try {
+            bootstrap.start();
+            fail();
+        } catch (ProcessExecutionException e) { }
+
+        IBootstrap<IBootstrapItem<String>> nextBootstrap = new Bootstrap(items);
+        nextBootstrap.start();
+        verify(item1, times(2)).executeProcess();
+        verify(item2, times(3)).executeProcess();
+        verify(item3, times(4)).executeProcess();
+
+        try {
+            bootstrap.revert();
+            fail();
+        } catch(RevertProcessExecutionException e) { }
+        verify(item1, times(1)).executeRevertProcess();
+        verify(item2, times(2)).executeRevertProcess();
+        verify(item3, times(3)).executeRevertProcess();
+
         bootstrap.revert();
         verify(item1, times(1)).executeRevertProcess();
-        IBootstrap<IBootstrapItem<String>> nextBootstrap = new Bootstrap(items);
-        verify(item1, times(1)).executeProcess();
+        verify(item2, times(2)).executeRevertProcess();
+        verify(item3, times(4)).executeRevertProcess();
     }
 
     @Test (expected = ProcessExecutionException.class)
