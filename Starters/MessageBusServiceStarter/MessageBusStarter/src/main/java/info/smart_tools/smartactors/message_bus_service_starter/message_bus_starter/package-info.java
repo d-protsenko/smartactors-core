bbf3c10081c1839cdd375d42f6da5ed8437ec414
Alexey Kutalev <alexey.kutalev@7bits.it>
2018-09-06 15:45:49 +0600

    All features from Starters moved to CoreFeatures

--- Starters/MessageBusServiceStarter/MessageBusStarter/src/main/java/info/smart_tools/smartactors/message_bus_service_starter/message_bus_starter/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Contains implementations of {@link info.smart_tools.smartactors.configuration_manager.interfaces.iconfiguration_manager.ISectionStrategy} for configuration sections:
- * - "objects"
- * - "maps"
- * - "endpoints"
- */
-package info.smart_tools.smartactors.messagebus_service_starter.messagebus_starter;
