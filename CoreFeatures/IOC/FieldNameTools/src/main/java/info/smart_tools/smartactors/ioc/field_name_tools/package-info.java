9bec99d15fa70307f9190e50d1037b31e7dc7dd1
Alexey Kutalev <alexey.kutalev@7bits.it>
2019-06-16 00:33:25 +0300

    Removed FieldNameTools

--- CoreFeatures/IOC/FieldNameTools/src/main/java/info/smart_tools/smartactors/ioc/field_name_tools/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package contains service locator class for storing named instances of {@link info.smart_tools.smartactors.ioc.ikey.IKey}
- */
-package info.smart_tools.smartactors.ioc.field_name_tools;
